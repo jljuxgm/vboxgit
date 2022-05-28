6df60af5d6d40ba3a9af6f624b93363ade6fbc81
qzuser <395143360@qq.com>
2022-05-28 18:55:15 +0800

    commit_1273 ------------------------------------------------------------------------ r1273 | vboxsync | 2007-03-06 21:41:17 +0800 (Tue, 06 Mar 2007) | 3 lines
    Keep stats for forced SVM exits.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/HWACCMInternal.h
+++ vbox/src/VBox/VMM/HWACCMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: HWACCMInternal.h 1272 2007-03-06 13:32:13Z vboxsync $ */
+/* $Id: HWACCMInternal.h 1273 2007-03-06 13:41:17Z vboxsync $ */
 /** @file
  * HWACCM - Internal header file.
  */
@@ -278,6 +278,7 @@ typedef struct HWACCM
     STAMCOUNTER             StatExitIrqWindow;
     STAMCOUNTER             StatIntReinject;
     STAMCOUNTER             StatPendingHostIrq;
+    STAMCOUNTER             StatExitForced;
 
     STAMCOUNTER             StatSwitchGuestIrq;
     STAMCOUNTER             StatSwitchToR3;
