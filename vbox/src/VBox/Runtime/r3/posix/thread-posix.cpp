2469b00525552a0e900134276571b9e6152f6639
qzuser <395143360@qq.com>
2022-05-28 15:57:26 +0800

    commit_244 ------------------------------------------------------------------------ r244 | vboxsync | 2007-01-24 00:49:40 +0800 (Wed, 24 Jan 2007) | 3 lines
    Added RTThreadExit for win32 & os2. Posix & l4 version not implemented
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/posix/thread-posix.cpp
+++ vbox/src/VBox/Runtime/r3/posix/thread-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: thread-posix.cpp 1  vboxsync $ */
+/* $Id: thread-posix.cpp 244 2007-01-23 16:49:40Z vboxsync $ */
 /** @file
  * InnoTek Portable Runtime - Threads, POSIX.
  */
@@ -257,3 +257,8 @@ RTR3DECL(int) RTThreadSetAffinity(uint64_t u64Mask)
     return VINF_SUCCESS;
 }
 
+
+RTR3DECL(void) RTThreadExit(void)
+{
+    AssertMsgFailed(("Unimplemented\n"));
+}
