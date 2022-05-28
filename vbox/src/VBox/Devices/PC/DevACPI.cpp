e6e76365aa2d1cb11b92b17d071a5eb8abb24b3e
qzuser <395143360@qq.com>
2022-05-29 04:43:59 +0800

    commit_4382 ------------------------------------------------------------------------ r4382 | vboxsync | 2007-08-27 19:28:55 +0800 (Mon, 27 Aug 2007) | 1 line
    Extended the ROMRegister DevHlp interface to include a fShadow flag. Added a interface intented for write proecting shadow ROM after the POST is over.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/PC/DevACPI.cpp
+++ vbox/src/VBox/Devices/PC/DevACPI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevACPI.cpp 4279 2007-08-22 12:45:11Z vboxsync $ */
+/* $Id: DevACPI.cpp 4382 2007-08-27 11:28:55Z vboxsync $ */
 /** @file
  * Advanced Configuration and Power Interface (ACPI) Device.
  */
@@ -1560,7 +1560,7 @@ static DECLCALLBACK(int) acpiConstruct (PPDMDEVINS pDevIns, int iInstance, PCFGM
     if (VBOX_FAILURE (rc))
         return rc;
 
-    rc = PDMDevHlpROMRegister (pDevIns, rsdp_addr, 0x1000, s->au8RSDPPage, "ACPI RSDP");
+    rc = PDMDevHlpROMRegister (pDevIns, rsdp_addr, 0x1000, s->au8RSDPPage, false /* fShadow */, "ACPI RSDP");
     if (VBOX_FAILURE (rc))
         return rc;
 
