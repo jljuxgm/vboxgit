f66878332a2403b2973ad2faeb0da35459be0517
qzuser <395143360@qq.com>
2022-05-28 22:38:16 +0800

    commit_2381 ------------------------------------------------------------------------ r2381 | vboxsync | 2007-04-27 14:38:19 +0800 (Fri, 27 Apr 2007) | 1 line
    VCC8 is very picky about char* vs const char *
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Storage/VmdkHDDCore.cpp
+++ vbox/src/VBox/Devices/Storage/VmdkHDDCore.cpp
@@ -1,4 +1,4 @@
-/** $Id: VmdkHDDCore.cpp 2375 2007-04-27 00:32:46Z vboxsync $ */
+/** $Id: VmdkHDDCore.cpp 2381 2007-04-27 06:38:19Z vboxsync $ */
 /** @file
  * VMDK Disk image, Core Code.
  */
@@ -497,7 +497,7 @@ static int vmdkStringUnquote(PVMDKIMAGE pImage, const char *pszStr, char **ppszU
     if (*pszStr++ != '"')
         return vmdkError(pImage, VERR_VDI_INVALID_HEADER, RT_SRC_POS, N_("VMDK: incorrectly quoted value in descriptor"));
 
-    pszQ = strchr(pszStr, '"');
+    pszQ = (char*)strchr(pszStr, '"');
     if (pszQ == NULL)
         return vmdkError(pImage, VERR_VDI_INVALID_HEADER, RT_SRC_POS, N_("VMDK: incorrectly quoted value in descriptor"));
     pszUnquoted = (char *)RTMemTmpAlloc(pszQ - pszStr + 1);
