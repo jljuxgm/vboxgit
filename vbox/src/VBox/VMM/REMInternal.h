36574178fde8c5cf5c5970843e6ee684f16b632e
qzuser <395143360@qq.com>
2022-05-28 21:02:07 +0800

    commit_1945 ------------------------------------------------------------------------ r1945 | vboxsync | 2007-04-05 17:02:32 +0800 (Thu, 05 Apr 2007) | 3 lines
    Increased SSM version of REM.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/REMInternal.h
+++ vbox/src/VBox/VMM/REMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: REMInternal.h 983 2007-02-19 14:57:38Z vboxsync $ */
+/* $Id: REMInternal.h 1945 2007-04-05 09:02:32Z vboxsync $ */
 /** @file
  * REM - Internal header file.
  */
@@ -43,7 +43,7 @@
  */
 
 /** The saved state version number. */
-#define REM_SAVED_STATE_VERSION     4
+#define REM_SAVED_STATE_VERSION     5
 
 
 /** @def REM_MONITOR_CODE_PAGES
