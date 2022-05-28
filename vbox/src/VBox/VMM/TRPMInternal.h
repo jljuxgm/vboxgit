45fd5346411b9b1805e44ab3ac886a9bffef6f8f
qzuser <395143360@qq.com>
2022-05-28 17:10:46 +0800

    commit_624 ------------------------------------------------------------------------ r624 | vboxsync | 2007-02-05 18:03:11 +0800 (Mon, 05 Feb 2007) | 3 lines
    More statistics.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/TRPMInternal.h
+++ vbox/src/VBox/VMM/TRPMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: TRPMInternal.h 415 2007-01-29 18:04:13Z vboxsync $ */
+/* $Id: TRPMInternal.h 624 2007-02-05 10:03:11Z vboxsync $ */
 /** @file
  * TRPM - Internal header file.
  */
@@ -155,6 +155,9 @@ typedef struct TRPM
     STAMCOUNTER     StatForwardFailPatchAddr;
     STAMCOUNTER     StatForwardFailGC;
     STAMCOUNTER     StatForwardFailHC;
+
+    STAMPROFILEADV  StatForwardProfGC;
+    STAMPROFILEADV  StatForwardProfHC;
 
     /* R3: Statistics for interrupt handlers (allocated on the hypervisor heap). */
     R3PTRTYPE(PSTAMCOUNTER) paStatForwardedIRQR3;
