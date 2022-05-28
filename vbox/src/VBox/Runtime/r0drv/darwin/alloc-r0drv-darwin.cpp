9ad12488190b736f853fb5ba15b939ddfb193b63
qzuser <395143360@qq.com>
2022-05-28 16:27:54 +0800

    commit_395 ------------------------------------------------------------------------ r395 | vboxsync | 2007-01-28 08:03:10 +0800 (Sun, 28 Jan 2007) | 1 line
    Log allocation failures.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r0drv/darwin/alloc-r0drv-darwin.cpp
+++ vbox/src/VBox/Runtime/r0drv/darwin/alloc-r0drv-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: alloc-r0drv-darwin.cpp 1  vboxsync $ */
+/* $Id: alloc-r0drv-darwin.cpp 395 2007-01-28 00:03:10Z vboxsync $ */
 /** @file
  * InnoTek Portable Runtime - Memory Allocation, Ring-0 Driver, Darwin.
  */
@@ -44,6 +44,8 @@ PRTMEMHDR rtMemAlloc(size_t cb, uint32_t fFlags)
         pHdr->cb        = cb;
         pHdr->u32Padding= 0;
     }
+    else
+        printf("rmMemAlloc(%#x, %#x) failed\n", cb + sizeof(*pHdr), fFlags);
     return pHdr;
 }
 
