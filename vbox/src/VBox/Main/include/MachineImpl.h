ecbea2bd35b52ec229710dd2d78405a5fd94d7b0
qzuser <395143360@qq.com>
2022-05-30 04:22:47 +0800

    commit_10595 ------------------------------------------------------------------------ r10595 | vboxsync | 2008-07-14 20:23:57 +0800 (Mon, 14 Jul 2008) | 1 line
    Main: Performance: Typos, docs, cosmetics.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/MachineImpl.h
+++ vbox/src/VBox/Main/include/MachineImpl.h
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.h 10544 2008-07-11 18:39:28Z vboxsync $ */
+/* $Id: MachineImpl.h 10595 2008-07-14 12:23:57Z vboxsync $ */
 
 /** @file
  *
@@ -711,8 +711,8 @@ protected:
     void copyFrom (Machine *aThat);
 
 #ifdef VBOX_WITH_RESOURCE_USAGE_API
-    void registerMetrics(PerformanceCollector *collector);
-    void unregisterMetrics(PerformanceCollector *collector);
+    void registerMetrics (PerformanceCollector *aCollector);
+    void unregisterMetrics (PerformanceCollector *aCollector);
 #endif /* VBOX_WITH_RESOURCE_USAGE_API */
 
     const InstanceType mType;
