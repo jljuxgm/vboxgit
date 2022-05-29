af8b6b22bd69cd1c662a130ce8d81bd363c4aaf3
qzuser <395143360@qq.com>
2022-05-30 03:42:29 +0800

    commit_10401 ------------------------------------------------------------------------ r10401 | vboxsync | 2008-07-09 19:14:02 +0800 (Wed, 09 Jul 2008) | 3 lines
    Detect page reuse by rep movsq.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ vbox/src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPool.cpp 10397 2008-07-09 09:05:42Z vboxsync $ */
+/* $Id: PGMAllPool.cpp 10401 2008-07-09 11:14:02Z vboxsync $ */
 /** @file
  * PGM Shadow Page Pool.
  */
@@ -720,6 +720,7 @@ DECLINLINE(bool) pgmPoolMonitorIsReused(PPGMPOOLPAGE pPage, PCPUMCTXCORE pRegFra
         case OP_MOVNTDQ:    /* solaris - hwblkclr & hwblkpagecopy */
             Log4(("pgmPoolMonitorIsReused: MOVNTDQ\n"));
             return true;
+        case OP_MOVSWD:
         case OP_STOSWD:
             if (    pRegFrame
                 &&  pCpu->prefix == PREFIX_REP
