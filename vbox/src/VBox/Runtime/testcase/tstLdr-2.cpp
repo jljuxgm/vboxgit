9349982a2062e251fa6fcfb33c4ed7d2a98fbeb2
qzuser <395143360@qq.com>
2022-05-29 12:43:51 +0800

    commit_7016 ------------------------------------------------------------------------ r7016 | vboxsync | 2008-02-19 21:32:39 +0800 (Tue, 19 Feb 2008) | 2 lines
    Compile fixes
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/testcase/tstLdr-2.cpp
+++ vbox/src/VBox/Runtime/testcase/tstLdr-2.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstLdr-2.cpp 5999 2007-12-07 15:05:06Z vboxsync $ */
+/* $Id: tstLdr-2.cpp 7016 2008-02-19 13:32:39Z vboxsync $ */
 /** @file
  * innotek Portable Runtime - Testcase for parts of RTLdr*, manual inspection.
  */
@@ -117,7 +117,9 @@ static int testLdrOne(const char *pszFilename)
                     unsigned off = Value - Addr;
                     if (off < cb)
                     {
-                        DISCPUSTATE Cpu = {0};
+                        DISCPUSTATE Cpu;
+
+                        memset(&Cpu, 0, sizeof(Cpu));
                         Cpu.mode = CPUMODE_32BIT;
                         if (MyDisBlock(&Cpu, (RTUINTPTR)pvBits + off, 200, Addr - (uintptr_t)pvBits))
                         {
