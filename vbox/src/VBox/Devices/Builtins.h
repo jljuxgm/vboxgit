b8d9765549a0a9470de6fbc62280c5f71a335b96
qzuser <395143360@qq.com>
2022-05-29 16:19:03 +0800

    commit_7759 ------------------------------------------------------------------------ r7759 | vboxsync | 2008-04-05 19:36:02 +0800 (Sat, 05 Apr 2008) | 3 lines
    The BIOS logo stuff moved to VGA device. Added 24bpp bitmaps support.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Builtins.h
+++ vbox/src/VBox/Devices/Builtins.h
@@ -1,4 +1,4 @@
-/* $Id: Builtins.h 6597 2008-01-30 12:55:54Z vboxsync $ */
+/* $Id: Builtins.h 7759 2008-04-05 11:36:02Z vboxsync $ */
 /** @file
  * Built-in drivers & devices (part 1) header.
  */
@@ -23,9 +23,9 @@
 __BEGIN_DECLS
 
 /** The default BIOS logo data. */
-extern const unsigned char  g_abPcDefBiosLogo[];
+extern const unsigned char  g_abVgaDefBiosLogo[];
 /** The size of the default BIOS logo data. */
-extern const unsigned       g_cbPcDefBiosLogo;
+extern const unsigned       g_cbVgaDefBiosLogo;
 
 extern const PDMDEVREG g_DevicePCI;
 extern const PDMDEVREG g_DevicePcArch;
