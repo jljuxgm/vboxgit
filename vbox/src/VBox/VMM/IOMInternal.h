71d6b07bbe47bdcaff857cd1e325ff830d888369
qzuser <395143360@qq.com>
2022-05-30 03:58:32 +0800

    commit_10473 ------------------------------------------------------------------------ r10473 | vboxsync | 2008-07-10 23:02:53 +0800 (Thu, 10 Jul 2008) | 3 lines
    MMIO instruction emulation for OR, BT and XOR added.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/IOMInternal.h
+++ vbox/src/VBox/VMM/IOMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: IOMInternal.h 9776 2008-06-17 14:58:11Z vboxsync $ */
+/* $Id: IOMInternal.h 10473 2008-07-10 15:02:53Z vboxsync $ */
 /** @file
  * IOM - Internal header file.
  */
@@ -389,6 +389,9 @@ typedef struct IOM
     STAMPROFILE             StatGCInstMov;
     STAMPROFILE             StatGCInstCmp;
     STAMPROFILE             StatGCInstAnd;
+    STAMPROFILE             StatGCInstOr;
+    STAMPROFILE             StatGCInstXor;
+    STAMPROFILE             StatGCInstBt;
     STAMPROFILE             StatGCInstTest;
     STAMPROFILE             StatGCInstXchg;
     STAMPROFILE             StatGCInstStos;
