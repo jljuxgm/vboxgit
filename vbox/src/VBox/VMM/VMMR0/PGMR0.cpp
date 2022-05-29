a1806b76ac5378a368d9db952f9308cc10b6b391
qzuser <395143360@qq.com>
2022-05-30 02:37:43 +0800

    commit_10064 ------------------------------------------------------------------------ r10064 | vboxsync | 2008-07-01 18:02:18 +0800 (Tue, 01 Jul 2008) | 2 lines
    Missing log group
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR0/PGMR0.cpp
+++ vbox/src/VBox/VMM/VMMR0/PGMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMR0.cpp 10019 2008-06-30 14:36:34Z vboxsync $ */
+/* $Id: PGMR0.cpp 10064 2008-07-01 10:02:18Z vboxsync $ */
 /** @file
  * PGM - Page Manager and Monitor, Ring-0.
  */
@@ -22,6 +22,7 @@
 /*******************************************************************************
 *   Header Files                                                               *
 *******************************************************************************/
+#define LOG_GROUP LOG_GROUP_PGM
 #include <VBox/pgm.h>
 #include "PGMInternal.h"
 #include <VBox/vm.h>
