956e3eae710f1d1d4b107f2b0133a62ffdd3bc05
qzuser <395143360@qq.com>
2022-05-29 23:16:56 +0800

    commit_9357 ------------------------------------------------------------------------ r9357 | vboxsync | 2008-06-03 23:39:36 +0800 (Tue, 03 Jun 2008) | 2 lines
    #include <limits.h>; this should drag in NAME_MAX whether even if it's in syslimits.h...
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/dir.cpp
+++ vbox/src/VBox/Runtime/r3/dir.cpp
@@ -1,4 +1,4 @@
-/* $Id: dir.cpp 9355 2008-06-03 15:32:05Z vboxsync $ */
+/* $Id: dir.cpp 9357 2008-06-03 15:39:36Z vboxsync $ */
 /** @file
  * IPRT - Directory Manipulation.
  */
@@ -38,6 +38,7 @@
 #else
 # include <dirent.h>
 # include <unistd.h>
+# include <limits.h>
 #endif
 
 #include <iprt/dir.h>
