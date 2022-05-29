7ef2af02f72d2ededcf101856f2c7d0e10761753
qzuser <395143360@qq.com>
2022-05-30 04:51:52 +0800

    commit_10748 ------------------------------------------------------------------------ r10748 | vboxsync | 2008-07-18 21:53:03 +0800 (Fri, 18 Jul 2008) | 1 line
    SUPR0MemAlloc isn't clearing the memory it returns on darwin, work around this.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Network/SrvIntNetR0.cpp
+++ vbox/src/VBox/Devices/Network/SrvIntNetR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: SrvIntNetR0.cpp 10745 2008-07-18 12:18:55Z vboxsync $ */
+/* $Id: SrvIntNetR0.cpp 10748 2008-07-18 13:53:03Z vboxsync $ */
 /** @file
  * Internal networking - The ring 0 service.
  */
@@ -1674,6 +1674,8 @@ static int intnetR0NetworkCreateIf(PINTNETNETWORK pNetwork, PSUPDRVSESSION pSess
         rc = SUPR0MemAlloc(pIf->pSession, cbBuf, (PRTR0PTR)&pIf->pIntBufDefault, (PRTR3PTR)&pIf->pIntBufDefaultR3);
         if (RT_SUCCESS(rc))
         {
+            ASMMemZero32(pIf->pIntBufDefault, cbBuf); /** @todo I thought I specified these buggers as clearing the memory... */
+
             pIf->pIntBuf = pIf->pIntBufDefault;
             pIf->pIntBufR3 = pIf->pIntBufDefaultR3;
             pIf->pIntBuf->cbBuf = cbBuf;
