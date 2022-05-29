1ff29f699e35d12ed189ccd3d7b2ffcbb632f027
qzuser <395143360@qq.com>
2022-05-29 15:37:01 +0800

    commit_7616 ------------------------------------------------------------------------ r7616 | vboxsync | 2008-03-28 16:47:21 +0800 (Fri, 28 Mar 2008) | 2 lines
    cosmetics
    
    ------------------------------------------------------------------------

--- vbox/src/recompiler/VBoxRecompiler.c
+++ vbox/src/recompiler/VBoxRecompiler.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxRecompiler.c 7613 2008-03-28 08:25:54Z vboxsync $ */
+/* $Id: VBoxRecompiler.c 7616 2008-03-28 08:47:21Z vboxsync $ */
 /** @file
  * VBox Recompiler - QEMU.
  */
@@ -2557,7 +2557,7 @@ void remR3GrowDynRange(unsigned long physaddr)
     PVM pVM = cpu_single_env->pVM;
 
     Log(("remR3GrowDynRange %VGp\n", physaddr));
-    RTGCPHYS GCPhys = physaddr;
+    const RTGCPHYS GCPhys = physaddr;
     rc = PGM3PhysGrowRange(pVM, &GCPhys);
     if (VBOX_SUCCESS(rc))
         return;
