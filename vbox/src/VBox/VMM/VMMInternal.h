47d1d47b1c17318195618055bf557a76d471a92a
qzuser <395143360@qq.com>
2022-05-30 15:50:36 +0800

    commit_13798 ------------------------------------------------------------------------ r13798 | vboxsync | 2008-11-05 02:57:19 +0800 (Wed, 05 Nov 2008) | 1 line
    VMM: Split out the switcher code from VMM.cpp and into VMMSwitcher.cpp.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMInternal.h
+++ vbox/src/VBox/VMM/VMMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VMMInternal.h 13796 2008-11-04 18:37:33Z vboxsync $ */
+/* $Id: VMMInternal.h 13798 2008-11-04 18:57:19Z vboxsync $ */
 /** @file
  * VMM - Internal header file.
  */
@@ -422,6 +422,10 @@ typedef enum VMMGCOPERATION
 
 __BEGIN_DECLS
 
+#ifdef IN_RING3
+int  vmmR3SwitcherInit(PVM pVM);
+void vmmR3SwitcherRelocate(PVM pVM, RTGCINTPTR offDelta);
+#endif /* IN_RING3 */
 
 #ifdef IN_RING0
 /**
