484ff30a81f2bf09d30684aa4a96a6d72d8494df
qzuser <395143360@qq.com>
2022-05-30 10:21:34 +0800

    commit_12459 ------------------------------------------------------------------------ r12459 | vboxsync | 2008-09-15 20:07:41 +0800 (Mon, 15 Sep 2008) | 3 lines
    More comments would be nice.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/win/PerformanceWin.cpp
+++ vbox/src/VBox/Main/win/PerformanceWin.cpp
@@ -1,4 +1,4 @@
-/* $Id: PerformanceWin.cpp 12457 2008-09-15 11:14:35Z vboxsync $ */
+/* $Id: PerformanceWin.cpp 12459 2008-09-15 12:07:41Z vboxsync $ */
 
 /** @file
  *
@@ -165,7 +165,7 @@ CollectorWin::CollectorWin() : mRefresher(0), mNameSpace(0), mEnumProcessor(0), 
         return;
     }
 
-    // Retrieve property handles
+    // Retrieve property handles (Returned handles work across all instances of a class!)
 
     if (FAILED (hr = mRefresher->Refresh(0L)))
     {
