25b906aeac6ea5408299e36066c40f2672930f14
qzuser <395143360@qq.com>
2022-05-31 00:10:19 +0800

    commit_15740 ------------------------------------------------------------------------ r15740 | vboxsync | 2008-12-24 18:16:41 +0800 (Wed, 24 Dec 2008) | 2 lines
    removed bogus assert when trying to wait on non-waitable thread
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/misc/thread.cpp
+++ vbox/src/VBox/Runtime/common/misc/thread.cpp
@@ -1,4 +1,4 @@
-/* $Id: thread.cpp 14298 2008-11-18 12:47:26Z vboxsync $ */
+/* $Id: thread.cpp 15740 2008-12-24 10:16:41Z vboxsync $ */
 /** @file
  * IPRT - Threads, common routines.
  */
@@ -1011,7 +1011,6 @@ static int rtThreadWait(RTTHREAD Thread, unsigned cMillies, int *prc, bool fAuto
             else
             {
                 rc = VERR_THREAD_NOT_WAITABLE;
-                AssertRC(rc);
             }
             rtThreadRelease(pThread);
         }
