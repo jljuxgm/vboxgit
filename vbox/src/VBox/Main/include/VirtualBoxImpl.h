af8dedeb18664b5232752fd853a7460b82fcf08b
qzuser <395143360@qq.com>
2022-05-31 02:17:42 +0800

    commit_16191 ------------------------------------------------------------------------ r16191 | vboxsync | 2009-01-23 05:23:10 +0800 (Fri, 23 Jan 2009) | 1 line
    VirtualBoxImpl.h: forward declaration of IAppliance doesn't work on windows and shouldn't be necessary.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/VirtualBoxImpl.h
+++ vbox/src/VBox/Main/include/VirtualBoxImpl.h
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxImpl.h 16188 2009-01-22 19:58:31Z vboxsync $ */
+/* $Id: VirtualBoxImpl.h 16191 2009-01-22 21:23:10Z vboxsync $ */
 
 /** @file
  *
@@ -54,7 +54,6 @@ class Progress;
 class ProgressCollection;
 class Host;
 class SystemProperties;
-class IAppliance;
 
 #ifdef RT_OS_WINDOWS
 class SVCHlpClient;
