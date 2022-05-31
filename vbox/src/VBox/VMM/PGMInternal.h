8f2ed246bf7bbb6d117a06775bbf7619d211a340
qzuser <395143360@qq.com>
2022-05-31 16:49:06 +0800

    commit_20158 ------------------------------------------------------------------------ r20158 | vboxsync | 2009-05-29 23:25:31 +0800 (Fri, 29 May 2009) | 2 lines
    Extra VCPU checks. Attempt to fix alignment issue
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/PGMInternal.h
+++ vbox/src/VBox/VMM/PGMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: PGMInternal.h 20151 2009-05-29 12:40:31Z vboxsync $ */
+/* $Id: PGMInternal.h 20158 2009-05-29 15:25:31Z vboxsync $ */
 /** @file
  * PGM - Internal header file.
  */
@@ -2221,7 +2221,7 @@ typedef struct PGM
      */
     bool                            fRamPreAlloc;
     /** Alignment padding. */
-    bool                            afAlignment0[7];
+    bool                            afAlignment0[11];
 
     /*
      * This will be redefined at least two more times before we're done, I'm sure.
