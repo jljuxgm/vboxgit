bc36dc1f5f1e6288b45f97a0b5ce642b90824589
qzuser <395143360@qq.com>
2022-06-01 01:38:45 +0800

    commit_22052 ------------------------------------------------------------------------ r22052 | vboxsync | 2009-08-07 17:45:48 +0800 (Fri, 07 Aug 2009) | 1 line
    IPRT: RT_MORE_STRICT for r0rdv and r0drv/darwin.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r0drv/darwin/threadpreempt-r0drv-darwin.cpp
+++ vbox/src/VBox/Runtime/r0drv/darwin/threadpreempt-r0drv-darwin.cpp
@@ -1,4 +1,4 @@
-/* $Id: threadpreempt-r0drv-darwin.cpp 21536 2009-07-13 14:49:39Z vboxsync $ */
+/* $Id: threadpreempt-r0drv-darwin.cpp 22052 2009-08-07 09:45:48Z vboxsync $ */
 /** @file
  * IPRT - Thread Preemption, Ring-0 Driver, Darwin.
  */
@@ -33,6 +33,7 @@
 *   Header Files                                                               *
 *******************************************************************************/
 #include "the-darwin-kernel.h"
+#include "internal/iprt.h"
 #include <iprt/thread.h>
 
 #include <iprt/asm.h>
@@ -190,7 +191,7 @@ RTDECL(void) RTThreadPreemptRestore(PRTTHREADPREEMPTSTATE pState)
 RTDECL(bool) RTThreadIsInInterrupt(RTTHREAD hThread)
 {
     Assert(hThread == NIL_RTTHREAD); NOREF(hThread);
-    /** @todo Solaris: Implement RTThreadIsInInterrupt. Required for guest
+    /** @todo Darwin: Implement RTThreadIsInInterrupt. Required for guest
      *        additions! */
     return !ASMIntAreEnabled();
 }
