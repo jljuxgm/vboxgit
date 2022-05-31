52c76d92155424a5ee4d2b9646359a5bc76f77e6
qzuser <395143360@qq.com>
2022-05-31 08:18:45 +0800

    commit_18096 ------------------------------------------------------------------------ r18096 | vboxsync | 2009-03-20 00:25:41 +0800 (Fri, 20 Mar 2009) | 1 line
    spaces, sigh.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/string/string.cpp
+++ vbox/src/VBox/Runtime/common/string/string.cpp
@@ -1,4 +1,4 @@
-/* $Id: string.cpp 18055 2009-03-18 12:01:51Z vboxsync $ */
+/* $Id: string.cpp 18096 2009-03-19 16:25:41Z vboxsync $ */
 /** @file
  * IPRT - String Manipulation.
  */
@@ -37,6 +37,7 @@
 #include <iprt/assert.h>
 #include <iprt/err.h>
 #include "internal/string.h"
+
 
 /**
  * Free string allocated by any of the non-UCS-2 string functions.
