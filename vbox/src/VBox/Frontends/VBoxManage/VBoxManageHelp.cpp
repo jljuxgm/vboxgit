c117700e6ee56294868e085e98e3b8247fe14e78
qzuser <395143360@qq.com>
2022-05-31 17:21:01 +0800

    commit_20313 ------------------------------------------------------------------------ r20313 | vboxsync | 2009-06-05 19:00:32 +0800 (Fri, 05 Jun 2009) | 2 lines
    Frontends/VBoxManage, Main/Machine: implement new session type "headless" which suppresses any output. Based on the contribution by Josh Wright.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ vbox/src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageHelp.cpp 20054 2009-05-27 06:57:32Z vboxsync $ */
+/* $Id: VBoxManageHelp.cpp 20313 2009-06-05 11:00:32Z vboxsync $ */
 /** @file
  * VBoxManage - help and other message output.
  */
@@ -258,8 +258,10 @@ void printUsage(USAGECATEGORY u64Cmd)
     if (u64Cmd & USAGE_STARTVM)
     {
         RTPrintf("VBoxManage startvm          <uuid>|<name>\n");
+        RTPrintf("                            [--type gui");
         if (fVRDP)
-            RTPrintf("                            [--type gui|vrdp]\n");
+            RTPrintf(                         "|vrdp");
+        RTPrintf(                             "|headless]\n");
         RTPrintf("\n");
     }
 
