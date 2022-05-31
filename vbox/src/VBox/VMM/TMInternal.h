28f6135785347deb2dd53d6639e9406a7f569a42
qzuser <395143360@qq.com>
2022-05-31 15:27:07 +0800

    commit_19810 ------------------------------------------------------------------------ r19810 | vboxsync | 2009-05-19 17:59:20 +0800 (Tue, 19 May 2009) | 1 line
    TM: TMTimerPoll hacking.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/TMInternal.h
+++ vbox/src/VBox/VMM/TMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: TMInternal.h 19803 2009-05-19 08:33:18Z vboxsync $ */
+/* $Id: TMInternal.h 19810 2009-05-19 09:59:20Z vboxsync $ */
 /** @file
  * TM - Internal header file.
  */
@@ -469,10 +469,12 @@ typedef struct TM
      * @{ */
     STAMCOUNTER                 StatPoll;
     STAMCOUNTER                 StatPollAlreadySet;
-    STAMCOUNTER                 StatPollVirtual;
-    STAMCOUNTER                 StatPollVirtualSync;
+    STAMCOUNTER                 StatPollELoop;
     STAMCOUNTER                 StatPollMiss;
     STAMCOUNTER                 StatPollRunning;
+    STAMCOUNTER                 StatPollSimple;
+    STAMCOUNTER                 StatPollVirtual;
+    STAMCOUNTER                 StatPollVirtualSync;
     /** @} */
     /** TMTimerPollGIP
      * @{ */
