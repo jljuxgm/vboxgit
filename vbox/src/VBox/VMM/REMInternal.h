b8954facc753cc57ad03935cc22ccc8bbc753e95
qzuser <395143360@qq.com>
2022-05-31 18:46:07 +0800

    commit_20749 ------------------------------------------------------------------------ r20749 | vboxsync | 2009-06-22 04:57:37 +0800 (Mon, 22 Jun 2009) | 1 line
    REMNotifyInvalidatePage: Don't overflow aGCPtrInvalidatedPages. 2xtodo
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/REMInternal.h
+++ vbox/src/VBox/VMM/REMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: REMInternal.h 20747 2009-06-21 20:23:22Z vboxsync $ */
+/* $Id: REMInternal.h 20749 2009-06-21 20:57:37Z vboxsync $ */
 /** @file
  * REM - Internal header file.
  */
@@ -164,7 +164,7 @@ typedef struct REM
     uint32_t                u32PendingInterrupt;
 
     /** Number of recorded invlpg instructions. */
-    uint32_t                cInvalidatedPages;
+    uint32_t volatile       cInvalidatedPages;
 #if HC_ARCH_BITS == 32
     uint32_t                uPadding2;
 #endif
