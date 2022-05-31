4a1aaf492adf0a8266f604ac38a819e56638dcf2
qzuser <395143360@qq.com>
2022-05-31 08:40:09 +0800

    commit_18208 ------------------------------------------------------------------------ r18208 | vboxsync | 2009-03-25 01:01:32 +0800 (Wed, 25 Mar 2009) | 1 line
    #3569: DHCP Server is now started from VBoxSVC and terminates with it.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/DHCPServerRunner.cpp
+++ vbox/src/VBox/Main/DHCPServerRunner.cpp
@@ -1,4 +1,4 @@
-/* $Id: DHCPServerRunner.cpp 18023 2009-03-17 13:48:59Z vboxsync $ */
+/* $Id: DHCPServerRunner.cpp 18208 2009-03-24 17:01:32Z vboxsync $ */
 /** @file
  * VirtualBox Main - interface for VBox DHCP server
  */
