f908a5707b234050655e69839d1e286e791b2fea
qzuser <395143360@qq.com>
2022-05-31 11:56:21 +0800

    commit_19107 ------------------------------------------------------------------------ r19107 | vboxsync | 2009-04-22 19:24:55 +0800 (Wed, 22 Apr 2009) | 2 lines
    Cbinding: removed the unwanted header
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/cbinding/VBoxXPCOMC.cpp
+++ vbox/src/VBox/Main/cbinding/VBoxXPCOMC.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxXPCOMC.cpp 19106 2009-04-22 11:23:11Z vboxsync $ */
+/* $Id: VBoxXPCOMC.cpp 19107 2009-04-22 11:24:55Z vboxsync $ */
 /** @file VBoxXPCOMC.cpp
  * Utility functions to use with the C binding for XPCOM.
  */
@@ -26,7 +26,6 @@
 
 #include <iprt/string.h>
 #include <iprt/env.h>
-#include <iprt/uuid.h>
 #include <VBox/log.h>
 
 #include "VBoxCAPI_v2_5.h"
