356b25d279b2fb062bb038a38b39586102e75ed9
qzuser <395143360@qq.com>
2022-05-31 11:49:07 +0800

    commit_19070 ------------------------------------------------------------------------ r19070 | vboxsync | 2009-04-21 20:29:36 +0800 (Tue, 21 Apr 2009) | 1 line
    Main,VBoxManage: Implemented IHost::OperatingSystem and IHost::OSVersion using RTSystemQueryOSInfo. TODO: review error reporting.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxManage/VBoxManageList.cpp
+++ vbox/src/VBox/Frontends/VBoxManage/VBoxManageList.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageList.cpp 19068 2009-04-21 11:47:42Z vboxsync $ */
+/* $Id: VBoxManageList.cpp 19070 2009-04-21 12:29:36Z vboxsync $ */
 /** @file
  * VBoxManage - The 'list' command.
  */
@@ -416,7 +416,6 @@ int handleList(HandlerArg *a)
             CHECK_ERROR (Host, COMGETTER(MemoryAvailable)(&memoryAvailable));
             RTPrintf("Memory available: %lu MByte\n", memoryAvailable);
 
-#if 0 /* not yet implemented in Main */
             Bstr operatingSystem;
             CHECK_ERROR (Host, COMGETTER(OperatingSystem)(operatingSystem.asOutParam()));
             RTPrintf("Operating system: %lS\n", operatingSystem.raw());
@@ -424,7 +423,6 @@ int handleList(HandlerArg *a)
             Bstr oSVersion;
             CHECK_ERROR (Host, COMGETTER(OSVersion)(oSVersion.asOutParam()));
             RTPrintf("Operating system version: %lS\n", oSVersion.raw());
-#endif
         }
         break;
 
