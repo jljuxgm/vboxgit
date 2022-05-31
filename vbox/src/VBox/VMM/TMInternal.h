a099c1476dfe4639f7541dcefdb315b3ca5e3474
qzuser <395143360@qq.com>
2022-05-31 18:31:55 +0800

    commit_20678 ------------------------------------------------------------------------ r20678 | vboxsync | 2009-06-18 17:22:30 +0800 (Thu, 18 Jun 2009) | 1 line
    TM: Count calls to TMCpuTickSet and take the VM handle as an argument.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/TMInternal.h
+++ vbox/src/VBox/VMM/TMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: TMInternal.h 20374 2009-06-08 00:43:21Z vboxsync $ */
+/* $Id: TMInternal.h 20678 2009-06-18 09:22:30Z vboxsync $ */
 /** @file
  * TM - Internal header file.
  */
@@ -506,6 +506,9 @@ typedef struct TM
     STAMPROFILE                 StatVirtualSyncFF;
     /** The timer callback. */
     STAMCOUNTER                 StatTimerCallbackSetFF;
+
+    /** Calls to TMCpuTickSet. */
+    STAMCOUNTER                 StatTSCSet;
 
     /** @name Reasons for refusing TSC offsetting in TMCpuTickCanUseRealTSC.
      * @{ */
