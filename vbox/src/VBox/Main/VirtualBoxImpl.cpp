9594500a1c811f72c4645b1cc4430004381b4d76
qzuser <395143360@qq.com>
2022-06-01 02:27:10 +0800

    commit_22304 ------------------------------------------------------------------------ r22304 | vboxsync | 2009-08-17 23:55:30 +0800 (Mon, 17 Aug 2009) | 1 line
    Main: fix broken deletion of extradata
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/VirtualBoxImpl.cpp
+++ vbox/src/VBox/Main/VirtualBoxImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxImpl.cpp 22295 2009-08-17 13:10:16Z vboxsync $ */
+/* $Id: VirtualBoxImpl.cpp 22304 2009-08-17 15:55:30Z vboxsync $ */
 
 /** @file
  * Implementation of IVirtualBox in VBoxSVC.
@@ -1560,7 +1560,10 @@ STDMETHODIMP VirtualBox::SetExtraData(IN_BSTR aKey,
 
         AutoWriteLock alock(this);
 
-        m_pMainConfigFile->mapExtraDataItems[strKey] = strValue;
+        if (strValue.isEmpty())
+            m_pMainConfigFile->mapExtraDataItems.erase(strKey);
+        else
+            m_pMainConfigFile->mapExtraDataItems[strKey] = strValue;
                 // creates a new key if needed
 
         /* save settings on success */
