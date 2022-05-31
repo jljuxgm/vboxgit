3b8cb2ffdca8ebdbeaec394429fd49eba63d9af0
qzuser <395143360@qq.com>
2022-06-01 04:22:10 +0800

    commit_22890 ------------------------------------------------------------------------ r22890 | vboxsync | 2009-09-10 07:11:31 +0800 (Thu, 10 Sep 2009) | 1 line
    VM::cCPUs -> VM::cCpus so it matches all the other cCpus and aCpus members.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMAll/REMAll.cpp
+++ vbox/src/VBox/VMM/VMMAll/REMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: REMAll.cpp 22707 2009-09-02 11:02:48Z vboxsync $ */
+/* $Id: REMAll.cpp 22890 2009-09-09 23:11:31Z vboxsync $ */
 /** @file
  * REM - Recompiled Execution Monitor, all Contexts part.
  */
@@ -210,7 +210,7 @@ VMMDECL(void) REMNotifyHandlerPhysicalModify(PVM pVM, PGMPHYSHANDLERTYPE enmType
  */
 VMMDECL(void) REMNotifyHandlerPhysicalFlushIfAlmostFull(PVM pVM, PVMCPU pVCpu)
 {
-    Assert(pVM->cCPUs == 1);
+    Assert(pVM->cCpus == 1);
 
     /*
      * Less than 48 items means we should flush.
