6835bbd803a95bd02daefc843a809f3c2955065e
qzuser <395143360@qq.com>
2022-06-01 00:00:22 +0800

    commit_21883

--- vbox/src/VBox/Devices/PC/DevPcBios.cpp
+++ vbox/src/VBox/Devices/PC/DevPcBios.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevPcBios.cpp 21874 2009-07-30 12:12:23Z vboxsync $ */
+/* $Id: DevPcBios.cpp 21883 2009-07-30 13:10:21Z vboxsync $ */
 /** @file
  * PC BIOS Device.
  */
@@ -1533,7 +1533,6 @@ static DECLCALLBACK(int)  pcbiosConstruct(PPDMDEVINS pDevIns, int iInstance, PCF
                               "DmiChassisVersion\0"
                               "DmiChassisSerial\0"
                               "DmiChassisAssetTag\0"
-                              "DmiSystemVersion\0"
                               "DmiOEMVBoxVer\0"
                               "DmiOEMVBoxRev\0"))
         return PDMDEV_SET_ERROR(pDevIns, VERR_PDM_DEVINS_UNKNOWN_CFG_VALUES,
