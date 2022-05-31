e9ad889d93695752af15f9026f5a6e9e23c6ff11
qzuser <395143360@qq.com>
2022-06-01 05:54:44 +0800

    commit_23355 ------------------------------------------------------------------------ r23355 | vboxsync | 2009-09-28 15:45:59 +0800 (Mon, 28 Sep 2009) | 1 line
    LsiLogic: Fix misaligned critical section
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
+++ vbox/src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevLsiLogicSCSI.cpp 22793 2009-09-05 01:29:24Z vboxsync $ */
+/* $Id: DevLsiLogicSCSI.cpp 23355 2009-09-28 07:45:59Z vboxsync $ */
 /** @file
  *
  * VBox storage devices:
@@ -2066,14 +2066,14 @@ typedef struct LSILOGICSCSI
     /** Number entries allocated for the outstanding request queue. */
     uint32_t              cRequestQueueEntries;
 
+#if HC_ARCH_BITS == 64
+    uint32_t              Alignment2;
+#endif
+
     /** Critical section protecting the reply post queue. */
     PDMCRITSECT           ReplyPostQueueCritSect;
     /** Critical section protecting the reply free queue. */
     PDMCRITSECT           ReplyFreeQueueCritSect;
-
-#if HC_ARCH_BITS == 64
-    uint32_t              Alignment2;
-#endif
 
     /** Pointer to the start of the reply free queue - R3. */
     R3PTRTYPE(volatile uint32_t *) pReplyFreeQueueBaseR3;
