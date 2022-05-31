777098668f0939bc26c8e49dde29e200784c1aff
qzuser <395143360@qq.com>
2022-05-31 11:31:47 +0800

    commit_18984 ------------------------------------------------------------------------ r18984 | vboxsync | 2009-04-17 17:00:22 +0800 (Fri, 17 Apr 2009) | 2 lines
    Autoset does belong in PGMCPU
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ vbox/src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HWVMXR0.cpp 18976 2009-04-17 07:22:59Z vboxsync $ */
+/* $Id: HWVMXR0.cpp 18984 2009-04-17 09:00:22Z vboxsync $ */
 /** @file
  * HWACCM VMX - Host Context Ring 0.
  */
@@ -2135,7 +2135,7 @@ ResumeExecution:
     }
 #endif
 #ifdef VBOX_WITH_2X_4GB_ADDR_SPACE_IN_R0
-    PGMDynMapFlushAutoSet(pVM);
+    PGMDynMapFlushAutoSet(pVCpu);
 #endif
 
     /*
