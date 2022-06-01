f9a25162aa05d8e8185ee0219cd1867bc973039e
qzuser <395143360@qq.com>
2022-06-01 18:16:40 +0800

    commit_26856 ------------------------------------------------------------------------ r26856 | vboxsync | 2010-02-26 22:33:01 +0800 (Fri, 26 Feb 2010) | 1 line
    DevVGA: Some small changes to make it work without having HW acceleration (like VBoxBFE).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Graphics/DevVGA.cpp
+++ vbox/src/VBox/Devices/Graphics/DevVGA.cpp
@@ -1,5 +1,5 @@
 #ifdef VBOX
-/* $Id: DevVGA.cpp 26173 2010-02-02 21:11:09Z vboxsync $ */
+/* $Id: DevVGA.cpp 26856 2010-02-26 14:33:01Z vboxsync $ */
 /** @file
  * DevVGA - VBox VGA/VESA device.
  */
@@ -5745,7 +5745,8 @@ static DECLCALLBACK(int)  vgaAttach(PPDMDEVINS pDevIns, unsigned iLUN, uint32_t 
                     if(rc == VINF_SUCCESS)
                     {
                         rc = vbvaVHWAConstruct(pThis);
-                        AssertRC(rc);
+                        if (rc != VERR_NOT_IMPLEMENTED)
+                            AssertRC(rc);
                     }
 #endif
                 }
