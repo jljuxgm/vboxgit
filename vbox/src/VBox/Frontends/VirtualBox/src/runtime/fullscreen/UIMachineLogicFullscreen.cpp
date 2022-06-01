96cecbfa1b3e5028f2a8f87b15ed3067971353ec
qzuser <395143360@qq.com>
2022-06-01 19:50:37 +0800

    commit_27300 ------------------------------------------------------------------------ r27300 | vboxsync | 2010-03-12 03:20:32 +0800 (Fri, 12 Mar 2010) | 1 line
    FE/Qt4: New running VM core: support for pre-loading extended modes, seamless pre-loading support.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/fullscreen/UIMachineLogicFullscreen.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/fullscreen/UIMachineLogicFullscreen.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineLogicFullscreen.cpp 27263 2010-03-11 02:01:19Z vboxsync $ */
+/* $Id: UIMachineLogicFullscreen.cpp 27300 2010-03-11 19:20:32Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -131,6 +131,9 @@ void UIMachineLogicFullscreen::initialize()
     /* If required features are ready: */
     if (!isPreventAutoStart())
     {
+        /* Prepare common connections: */
+        prepareConnections();
+
         /* Prepare console connections: */
         prepareConsoleConnections();
 
