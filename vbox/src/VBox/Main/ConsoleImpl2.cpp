1011c08f27d60dfd881109f912fa7a1c7a34e959
qzuser <395143360@qq.com>
2022-06-01 20:17:07 +0800

    commit_27423 ------------------------------------------------------------------------ r27423 | vboxsync | 2010-03-17 02:51:35 +0800 (Wed, 17 Mar 2010) | 2 lines
    RTC is _not_ enabled by default
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/ConsoleImpl2.cpp
+++ vbox/src/VBox/Main/ConsoleImpl2.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl2.cpp 27422 2010-03-16 18:20:53Z vboxsync $ */
+/* $Id: ConsoleImpl2.cpp 27423 2010-03-16 18:51:35Z vboxsync $ */
 /** @file
  * VBox Console COM Class implementation
  *
@@ -2172,7 +2172,7 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
         rc = CFGMR3InsertInteger(pCfg,  "FdcEnabled", fFdcEnabled);                 RC_CHECK();
         rc = CFGMR3InsertInteger(pCfg,  "HpetEnabled", fHpetEnabled);               RC_CHECK();
         rc = CFGMR3InsertInteger(pCfg,  "SmcEnabled", fSmcEnabled);                 RC_CHECK();
-        rc = CFGMR3InsertInteger(pCfg,  "ShowRtc",    true);                        RC_CHECK();
+        rc = CFGMR3InsertInteger(pCfg,  "ShowRtc",    fExtProfile);                 RC_CHECK();
 
         rc = CFGMR3InsertInteger(pCfg,  "ShowCpu", fShowCpu);                       RC_CHECK();
         rc = CFGMR3InsertInteger(pCfg,  "CpuHotPlug", fCpuHotPlug);                 RC_CHECK();
