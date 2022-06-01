8e127bd9eb7504b9052e6a973ead4871310bc5f6
qzuser <395143360@qq.com>
2022-06-01 10:12:52 +0800

    commit_24678 ------------------------------------------------------------------------ r24678 | vboxsync | 2009-11-16 00:07:51 +0800 (Mon, 16 Nov 2009) | 1 line
    Misc: Shut up annoying gcc warnings on darwin.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Storage/DrvSCSI.cpp
+++ vbox/src/VBox/Devices/Storage/DrvSCSI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvSCSI.cpp 24245 2009-11-02 10:26:46Z vboxsync $ */
+/* $Id: DrvSCSI.cpp 24678 2009-11-15 16:07:51Z vboxsync $ */
 /** @file
  *
  * VBox storage drivers:
@@ -609,9 +609,9 @@ static int drvscsiProcessRequestOne(PDRVSCSI pThis, PPDMSCSIREQUEST pRequest)
     int rc = VINF_SUCCESS;
     int iTxDir;
     int rcCompletion;
-    uint64_t uOffset;
-    uint32_t cbToTransfer;
-    uint32_t cSegmentsLeft;
+    uint64_t uOffset       = UINT64_MAX;    /* initialized to shut up gcc warnings. */
+    uint32_t cbToTransfer  = UINT32_MAX;    /* ditto */
+    uint32_t cSegmentsLeft = UINT32_MAX;    /* ditto */
 
     LogFlowFunc(("Entered\n"));
 
