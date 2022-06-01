e9e17a361b26877ae572cabb4b057613d9663787
qzuser <395143360@qq.com>
2022-06-02 04:07:05 +0800

    commit_29312 ------------------------------------------------------------------------ r29312 | vboxsync | 2010-05-11 10:59:56 +0800 (Tue, 11 May 2010) | 2 lines
    ConsoleVRDPServer.cpp: Don't assert when seeing VERR_NET_ADDRESS_IN_USE upon Lunch()!
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/ConsoleVRDPServer.cpp
+++ vbox/src/VBox/Main/ConsoleVRDPServer.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleVRDPServer.cpp 28802 2010-04-27 09:23:16Z vboxsync $ */
+/* $Id: ConsoleVRDPServer.cpp 29312 2010-05-11 02:59:56Z vboxsync $ */
 
 /** @file
  *
@@ -1236,7 +1236,7 @@ int ConsoleVRDPServer::Launch (void)
                 remoteUSBThreadStart ();
 #endif /* VBOX_WITH_USB */
             }
-            else
+            else if (rc != VERR_NET_ADDRESS_IN_USE)
                 AssertMsgFailed(("Could not start VRDP server: rc = %Rrc\n", rc));
         }
         else
