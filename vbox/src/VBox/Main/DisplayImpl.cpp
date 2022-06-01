8ce350c7179a04e1def52f4d7c7d6475156b3a60
qzuser <395143360@qq.com>
2022-06-01 08:33:56 +0800

    commit_24166 ------------------------------------------------------------------------ r24166 | vboxsync | 2009-10-30 00:09:22 +0800 (Fri, 30 Oct 2009) | 1 line
    2d accel: fix the typo; some dbg assertions
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/DisplayImpl.cpp
+++ vbox/src/VBox/Main/DisplayImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: DisplayImpl.cpp 24165 2009-10-29 15:46:38Z vboxsync $ */
+/* $Id: DisplayImpl.cpp 24166 2009-10-29 16:09:22Z vboxsync $ */
 
 /** @file
  *
@@ -2339,7 +2339,7 @@ void Display::handleVHWACommandProcess(PPDMIDISPLAYCONNECTOR pInterface, PVBOXVH
     {
         IFramebuffer *pFramebuffer = maFramebuffers[id].pFramebuffer;
 
-        if (pFramebuffer == NULL)
+        if (pFramebuffer != NULL)
         {
             pFramebuffer->Lock();
 
