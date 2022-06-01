6d5953c7a4812fab07fb94356d8958e93cfd8df9
qzuser <395143360@qq.com>
2022-06-01 12:32:22 +0800

    commit_25345 ------------------------------------------------------------------------ r25345 | vboxsync | 2009-12-13 23:56:59 +0800 (Sun, 13 Dec 2009) | 1 line
    iprt/lock.h -> iprt/cpp/lock.h
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/testcase/tstRTCritSect.cpp
+++ vbox/src/VBox/Runtime/testcase/tstRTCritSect.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstRTCritSect.cpp 20606 2009-06-15 23:49:07Z vboxsync $ */
+/* $Id: tstRTCritSect.cpp 25345 2009-12-13 15:56:59Z vboxsync $ */
 /** @file
  * IPRT Testcase - Critical Sections.
  */
@@ -42,7 +42,7 @@
 #include <iprt/err.h>
 #include <iprt/initterm.h>
 #include <iprt/getopt.h>
-#include <iprt/lock.h>
+#include <iprt/cpp/lock.h>
 #include <iprt/log.h>
 #include <iprt/mem.h>
 #include <iprt/semaphore.h>
