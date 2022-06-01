9ff80b4b2d0e3653bb03fcb39512f833cbb79c6c
qzuser <395143360@qq.com>
2022-06-01 12:02:19 +0800

    commit_25200 ------------------------------------------------------------------------ r25200 | vboxsync | 2009-12-05 02:19:05 +0800 (Sat, 05 Dec 2009) | 1 line
    Main: make MediumAttachment instance data private
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/HostImpl.cpp
+++ vbox/src/VBox/Main/HostImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: HostImpl.cpp 25151 2009-12-02 15:23:18Z vboxsync $ */
+/* $Id: HostImpl.cpp 25200 2009-12-04 18:19:05Z vboxsync $ */
 /** @file
  * VirtualBox COM class implementation: Host
  */
@@ -29,6 +29,7 @@
 # include "VirtualBoxImpl.h"
 #endif // VBOX_WITH_USB
 
+#include "MediumImpl.h"
 #include "HostPower.h"
 
 #if defined(RT_OS_LINUX) || defined(RT_OS_FREEBSD)
