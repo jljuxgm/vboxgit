15e5160154eeae079b1e76695daf339af46b1aa5
qzuser <395143360@qq.com>
2022-06-01 13:05:44 +0800

    commit_25474 ------------------------------------------------------------------------ r25474 | vboxsync | 2009-12-18 05:56:29 +0800 (Fri, 18 Dec 2009) | 1 line
    Oops
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/VBox/log-vbox.cpp
+++ vbox/src/VBox/Runtime/VBox/log-vbox.cpp
@@ -1,4 +1,4 @@
-/* $Id: log-vbox.cpp 25472 2009-12-17 21:48:33Z vboxsync $ */
+/* $Id: log-vbox.cpp 25474 2009-12-17 21:56:29Z vboxsync $ */
 /** @file
  * VirtualBox Runtime - Logging configuration.
  */
@@ -393,7 +393,7 @@ RTDECL(PRTLOGGER) RTLogDefaultInit(void)
             if (pszArgFileBuf)
             {
                 /* Retrieve the argument list */
-                rcBSD = sysctl(aiName, RT_ELEMENTS(aiName), NULL, &cchArgs, NULL, 0);
+                rcBSD = sysctl(aiName, RT_ELEMENTS(aiName), pszArgFileBuf, &cchArgs, NULL, 0);
                 if (!rcBSD)
                 {
                     /*
