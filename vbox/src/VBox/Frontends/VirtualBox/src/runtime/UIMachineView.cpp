7062c573e955271022f741f4cb8ad50763d21a88
qzuser <395143360@qq.com>
2022-06-01 18:17:47 +0800

    commit_26862 ------------------------------------------------------------------------ r26862 | vboxsync | 2010-02-26 23:20:27 +0800 (Fri, 26 Feb 2010) | 2 lines
    FE/Qt4: new core: QGL mode working again
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineView.cpp 26859 2010-02-26 14:49:29Z vboxsync $ */
+/* $Id: UIMachineView.cpp 26862 2010-02-26 15:20:27Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -369,11 +369,9 @@ void UIMachineView::prepareFrameBuffer()
     QWidget *pViewport;
     switch (mode())
     {
-#if 0 // TODO: Create Open GL viewport!
         case VBoxDefs::QGLMode:
-            pViewport = new VBoxGLWidget(this, this, NULL);
+            pViewport = new VBoxGLWidget(session().GetConsole(), this, NULL);
             break;
-#endif
         default:
             pViewport = new VBoxViewport(this);
     }
