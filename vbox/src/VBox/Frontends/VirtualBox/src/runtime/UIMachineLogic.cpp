91da25367767567bac58a3d9bb2bc0024df63cb1
qzuser <395143360@qq.com>
2022-06-01 18:25:22 +0800

    commit_26896 ------------------------------------------------------------------------ r26896 | vboxsync | 2010-03-01 02:03:10 +0800 (Mon, 01 Mar 2010) | 1 line
    FE/Qt4: New running VM core: fixed bug of closing VM through VBoxManage.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIMachineLogic.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIMachineLogic.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineLogic.cpp 26894 2010-02-28 17:48:51Z vboxsync $ */
+/* $Id: UIMachineLogic.cpp 26896 2010-02-28 18:03:10Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -644,7 +644,7 @@ void UIMachineLogic::sltMachineStateChanged()
     {
         /* VM has been powered off or saved or aborted, no matter internally or externally.
          * We must *safely* close the console window unless auto closure is disabled: */
-        sltClose();
+        QTimer::singleShot(0, uisession(), SLOT(sltCloseVirtualSession()));
         return;
     }
 
