a90b0aba95993e09c3966624ee9ba27a986c78ac
qzuser <395143360@qq.com>
2022-06-01 13:23:06 +0800

    commit_25547 ------------------------------------------------------------------------ r25547 | vboxsync | 2009-12-22 00:38:13 +0800 (Tue, 22 Dec 2009) | 2 lines
    pgmPoolAccessHandlerFlush: tlb flush on necessary for raw mode
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ vbox/src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMAllPool.cpp 25540 2009-12-21 14:02:28Z vboxsync $ */
+/* $Id: PGMAllPool.cpp 25547 2009-12-21 16:38:13Z vboxsync $ */
 /** @file
  * PGM Shadow Page Pool.
  */
@@ -907,8 +907,10 @@ static int pgmPoolAccessHandlerFlush(PVM pVM, PVMCPU pVCpu, PPGMPOOL pPool, PPGM
     else
         rc = rc2;
 
+#ifdef IN_RC
     /* See use in pgmPoolAccessHandlerSimple(). */
     PGM_INVL_VCPU_TLBS(pVCpu);
+#endif
     LogFlow(("pgmPoolAccessHandlerPT: returns %Rrc (flushed)\n", rc));
     return rc;
 }
