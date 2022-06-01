1b7c7254fa14d81a918f65cb7de3133cc76257f8
qzuser <395143360@qq.com>
2022-06-02 01:51:52 +0800

    commit_28597 ------------------------------------------------------------------------ r28597 | vboxsync | 2010-04-22 22:07:51 +0800 (Thu, 22 Apr 2010) | 1 line
    FE/Qt: New running VM core: correct update of USB, Shared folders and Virtualization LEDs.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineWindowNormal.cpp 28389 2010-04-15 20:13:32Z vboxsync $ */
+/* $Id: UIMachineWindowNormal.cpp 28597 2010-04-22 14:07:51Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -107,6 +107,7 @@ UIMachineWindowNormal::~UIMachineWindowNormal()
 void UIMachineWindowNormal::sltMachineStateChanged()
 {
     UIMachineWindow::sltMachineStateChanged();
+    updateAppearanceOf(UIVisualElement_VirtualizationStuff);
 }
 
 void UIMachineWindowNormal::sltMediumChange(const CMediumAttachment &attachment)
