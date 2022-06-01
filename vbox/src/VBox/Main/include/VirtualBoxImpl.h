dbfbb24f2be6489a7ac9d2e4048f5bfac9322472
qzuser <395143360@qq.com>
2022-06-02 07:18:50 +0800

    commit_30207 ------------------------------------------------------------------------ r30207 | vboxsync | 2010-06-15 23:49:27 +0800 (Tue, 15 Jun 2010) | 2 lines
    Main: console COM events, cleanup, locking
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/VirtualBoxImpl.h
+++ vbox/src/VBox/Main/include/VirtualBoxImpl.h
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxImpl.h 29925 2010-05-31 18:33:15Z vboxsync $ */
+/* $Id: VirtualBoxImpl.h 30207 2010-06-15 15:49:27Z vboxsync $ */
 
 /** @file
  *
@@ -95,6 +95,8 @@ public:
     BEGIN_CONNECTION_POINT_MAP(VirtualBox)
          CONNECTION_POINT_ENTRY(IID_IVirtualBoxCallback)
     END_CONNECTION_POINT_MAP()
+   
+    typedef CComDynamicUnkArray EventListenersList;
 #endif
 
     // to postpone generation of the default ctor/dtor
