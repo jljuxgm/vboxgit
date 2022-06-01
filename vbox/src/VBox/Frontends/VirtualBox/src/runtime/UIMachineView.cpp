1d5c9e3b5b2f5bd1a12af39a4a0ef1aa94ce0658
qzuser <395143360@qq.com>
2022-06-02 01:12:06 +0800

    commit_28407 ------------------------------------------------------------------------ r28407 | vboxsync | 2010-04-16 19:38:06 +0800 (Fri, 16 Apr 2010) | 1 line
    FE/Qt: New running VM core: remove one superfluous assertion.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineView.cpp 28291 2010-04-14 10:47:39Z vboxsync $ */
+/* $Id: UIMachineView.cpp 28407 2010-04-16 11:38:06Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -1918,7 +1918,6 @@ void UIMachineView::paintEvent(QPaintEvent *pPaintEvent)
 
 LRESULT CALLBACK UIMachineView::lowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    Assert(gView);
     if (gView && nCode == HC_ACTION && gView->winLowKeyboardEvent(wParam, *(KBDLLHOOKSTRUCT *)lParam))
         return 1;
 
