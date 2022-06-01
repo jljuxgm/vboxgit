f8ae7cb467606cdfe2e14985b430414d0412c0f7
qzuser <395143360@qq.com>
2022-06-01 16:26:56 +0800

    commit_26318 ------------------------------------------------------------------------ r26318 | vboxsync | 2010-02-08 05:37:44 +0800 (Mon, 08 Feb 2010) | 2 lines
    build fix
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/posix/process-posix.cpp
+++ vbox/src/VBox/Runtime/r3/posix/process-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: process-posix.cpp 26317 2010-02-07 21:35:45Z vboxsync $ */
+/* $Id: process-posix.cpp 26318 2010-02-07 21:37:44Z vboxsync $ */
 /** @file
  * IPRT - Process, POSIX.
  */
@@ -381,6 +381,6 @@ RTR3DECL(int)   RTProcDaemonize(bool fNoChDir, bool fNoClose, const char *pszPid
         exit(0);
     }
 
-    return rc;
+    return VINF_SUCCESS;
 }
 
