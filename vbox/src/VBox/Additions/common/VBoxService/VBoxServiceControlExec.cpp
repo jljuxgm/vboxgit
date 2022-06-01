eef034f0201e737c87131c1e2f89e510d99fc0f7
qzuser <395143360@qq.com>
2022-06-02 02:01:41 +0800

    commit_28649 ------------------------------------------------------------------------ r28649 | vboxsync | 2010-04-23 22:14:47 +0800 (Fri, 23 Apr 2010) | 1 line
    Guest Control/VBoxService: Fixed assertion.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxService/VBoxServiceControlExec.cpp
+++ vbox/src/VBox/Additions/common/VBoxService/VBoxServiceControlExec.cpp
@@ -1,5 +1,5 @@
 
-/* $Id: VBoxServiceControlExec.cpp 28647 2010-04-23 13:43:45Z vboxsync $ */
+/* $Id: VBoxServiceControlExec.cpp 28649 2010-04-23 14:14:47Z vboxsync $ */
 /** @file
  * VBoxServiceControlExec - Utility functions for process execution.
  */
@@ -625,8 +625,8 @@ int VBoxServiceControlExecAllocateThreadData(PVBOXSERVICECTRLTHREAD pThread,
         RTStrFree(pszArgsTemp);
     }
 
-    /* Did we get the same result? Also count in the added argv[0] above! */
-    Assert((uNumArgs + 1) == pData->uNumArgs);
+    /* Did we get at least as much arguments as told? */
+    Assert(pData->uNumArgs >= uNumArgs);
 
     if (RT_SUCCESS(rc))
     {
