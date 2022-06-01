adb944c741e759987a1d216a35b7a4cc235502cd
qzuser <395143360@qq.com>
2022-06-02 07:31:13 +0800

    commit_30265 ------------------------------------------------------------------------ r30265 | vboxsync | 2010-06-17 14:22:11 +0800 (Thu, 17 Jun 2010) | 2 lines
    OSE fix
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/testcase/tstHostHardwareLinux.cpp
+++ vbox/src/VBox/Main/testcase/tstHostHardwareLinux.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstHostHardwareLinux.cpp 28882 2010-04-28 23:17:52Z vboxsync $ */
+/* $Id: tstHostHardwareLinux.cpp 30265 2010-06-17 06:22:11Z vboxsync $ */
 /** @file
  *
  * Test executable for quickly excercising/debugging the Linux host hardware
