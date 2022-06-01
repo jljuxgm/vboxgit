7d250e1e92072e3c882406373818a1bdccf7dd63
qzuser <395143360@qq.com>
2022-06-02 02:49:08 +0800

    commit_28901 ------------------------------------------------------------------------ r28901 | vboxsync | 2010-04-29 22:43:50 +0800 (Thu, 29 Apr 2010) | 2 lines
    RTPathReal: fixed freeing of rtPathToNative result.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/posix/path-posix.cpp
+++ vbox/src/VBox/Runtime/r3/posix/path-posix.cpp
@@ -1,4 +1,4 @@
-/* $Id: path-posix.cpp 28877 2010-04-28 19:10:47Z vboxsync $ */
+/* $Id: path-posix.cpp 28901 2010-04-29 14:43:50Z vboxsync $ */
 /** @file
  * IPRT - Path Manipulation, POSIX.
  */
@@ -90,7 +90,7 @@ RTDECL(int) RTPathReal(const char *pszPath, char *pszRealPath, size_t cchRealPat
         }
         else
             rc = RTErrConvertFromErrno(errno);
-        RTStrFree(pszNativePath);
+        rtPathFreeNative(pszNativePath, pszPath);
     }
 
     LogFlow(("RTPathReal(%p:{%s}, %p:{%s}, %u): returns %Rrc\n", pszPath, pszPath,
