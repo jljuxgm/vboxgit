2e5fe51b8e1ad33614473972ab5a5510477e4343
qzuser <395143360@qq.com>
2022-06-02 01:02:39 +0800

    commit_28368 ------------------------------------------------------------------------ r28368 | vboxsync | 2010-04-15 22:46:06 +0800 (Thu, 15 Apr 2010) | 1 line
    Main/Display: seamless for multimonitor. (xTracker 4655)
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/DisplayImpl.h
+++ vbox/src/VBox/Main/include/DisplayImpl.h
@@ -1,4 +1,4 @@
-/* $Id: DisplayImpl.h 28245 2010-04-13 12:25:12Z vboxsync $ */
+/* $Id: DisplayImpl.h 28368 2010-04-15 14:46:06Z vboxsync $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -136,6 +136,10 @@ public:
     {
         return maFramebuffers[VBOX_VIDEO_PRIMARY_SCREEN].pFramebuffer;
     }
+#ifdef MMSEAMLESS
+    int handleSetVisibleRegion(uint32_t cRect, PRTRECT pRect);
+    int handleQueryVisibleRegion(uint32_t *pcRect, PRTRECT pRect);
+#endif
 
     int VideoAccelEnable (bool fEnable, VBVAMEMORY *pVbvaMemory);
     void VideoAccelFlush (void);
