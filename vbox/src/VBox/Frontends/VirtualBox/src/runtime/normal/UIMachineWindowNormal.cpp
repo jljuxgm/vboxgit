c0122716e3cccfcd554739fe7a16e34483081f9e
qzuser <395143360@qq.com>
2022-06-02 04:12:27 +0800

    commit_29339 ------------------------------------------------------------------------ r29339 | vboxsync | 2010-05-11 18:52:38 +0800 (Tue, 11 May 2010) | 1 line
    FE/Qt: New running VM core: cosmetics.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineWindowNormal.cpp 29337 2010-05-11 10:50:21Z vboxsync $ */
+/* $Id: UIMachineWindowNormal.cpp 29339 2010-05-11 10:52:38Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -567,9 +567,9 @@ void UIMachineWindowNormal::loadWindowSettings()
         if (machineView())
 #if defined(Q_WS_MAC) || defined(Q_WS_WIN)
             machineView()->normalizeGeometry(true);
-#else /* Q_WS_MAC */
+#else /* Q_WS_MAC || Q_WS_WIN */
             QTimer::singleShot(0, machineView(), SLOT(sltNormalizeGeometry()));
-#endif /* Q_WS_MAC */
+#endif /* Q_WS_MAC || Q_WS_WIN */
     }
 
     /* Load availability settings: */
