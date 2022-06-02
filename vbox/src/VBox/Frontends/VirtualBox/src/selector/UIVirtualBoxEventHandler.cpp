334dd7536393d5b5b0cf799cb84f9b8f0f3e41ba
qzuser <395143360@qq.com>
2022-06-02 09:42:15 +0800

    commit_30879 ------------------------------------------------------------------------ r30879 | vboxsync | 2010-07-16 20:48:47 +0800 (Fri, 16 Jul 2010) | 2 lines
    FE/Qt: to much
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/selector/UIVirtualBoxEventHandler.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/selector/UIVirtualBoxEventHandler.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIVirtualBoxEventHandler.cpp 30872 2010-07-16 10:31:31Z vboxsync $ */
+/* $Id: UIVirtualBoxEventHandler.cpp 30879 2010-07-16 12:48:47Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -72,8 +72,8 @@ UIVirtualBoxEventHandler::UIVirtualBoxEventHandler()
             this, SIGNAL(sigMachineDataChange(QString)),
             Qt::QueuedConnection);
 
-    connect(pListener, SIGNAL(sigMachineRegistere(QString, bool)),
-            this, SIGNAL(sigMachineRegistere(QString, bool)),
+    connect(pListener, SIGNAL(sigMachineRegistered(QString, bool)),
+            this, SIGNAL(sigMachineRegistered(QString, bool)),
             Qt::QueuedConnection);
 
     connect(pListener, SIGNAL(sigSessionStateChange(QString, KSessionState)),
