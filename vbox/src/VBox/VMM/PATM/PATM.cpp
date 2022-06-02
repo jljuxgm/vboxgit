b86ab7d16376e51c015666da825fe1c763014d5a
qzuser <395143360@qq.com>
2022-06-02 12:04:15 +0800

    commit_31438 ------------------------------------------------------------------------ r31438 | vboxsync | 2010-08-06 20:19:13 +0800 (Fri, 06 Aug 2010) | 1 line
    patmR3HandleDirtyInstr: wrong variable.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/PATM/PATM.cpp
+++ vbox/src/VBox/VMM/PATM/PATM.cpp
@@ -1,4 +1,4 @@
-/* $Id: PATM.cpp 31437 2010-08-06 12:01:55Z vboxsync $ */
+/* $Id: PATM.cpp 31438 2010-08-06 12:19:13Z vboxsync $ */
 /** @file
  * PATM - Dynamic Guest OS Patching Manager
  *
@@ -5899,7 +5899,7 @@ static int patmR3HandleDirtyInstr(PVM pVM, PCPUMCTX pCtx, PPATMPATCHREC pPatch, 
 
         /* Remove old lookup record. */
         patmr3RemoveP2GLookupRecord(pVM, &pPatch->patch, pCurPatchInstrGC);
-        pCurPatchInstrGC = NULL;
+        pPatchToGuestRec = NULL;
 
         pCurPatchInstrGC += CpuOld.opsize;
         cbDirty          += CpuOld.opsize;
