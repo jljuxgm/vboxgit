1918f37d9cb0c0a25452b71b324375a299a3a797
qzuser <395143360@qq.com>
2022-06-03 05:08:08 +0800

    commit_35152 ------------------------------------------------------------------------ r35152 | vboxsync | 2010-12-16 00:45:42 +0800 (Thu, 16 Dec 2010) | 2 lines
    *: added fFlags parameter to ''SUPR3HardenedLdrLoadAppPriv()'', ''SUPR3HardenedLdrLoad()'' and ''RTLdrLoadEx()''. VBoxSVC: slurp in VBoxVMM because it is required by the extension packs
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGlobal.cpp 35114 2010-12-15 09:36:27Z vboxsync $ */
+/* $Id: VBoxGlobal.cpp 35152 2010-12-15 16:45:42Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -5114,7 +5114,7 @@ void VBoxGlobal::init()
     {
         char szErr[8192];
         szErr[0] = '\0';
-        int vrc = SUPR3HardenedLdrLoadAppPriv("VBoxDbg", &mhVBoxDbg, szErr, sizeof(szErr));
+        int vrc = SUPR3HardenedLdrLoadAppPriv("VBoxDbg", &mhVBoxDbg, 0 /*=fFlags*/, szErr, sizeof(szErr));
         if (RT_FAILURE(vrc))
         {
             mhVBoxDbg = NIL_RTLDRMOD;
