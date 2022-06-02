73f7c3df33902c65fc7519e8d799d6f120a98148
qzuser <395143360@qq.com>
2022-06-02 16:04:10 +0800

    commit_32340 ------------------------------------------------------------------------ r32340 | vboxsync | 2010-09-09 19:51:09 +0800 (Thu, 09 Sep 2010) | 1 line
    DBGFCoreWrite: split out the core related stuff into a separate header, adjusted the first note header.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/DBGFInternal.h
+++ vbox/src/VBox/VMM/DBGFInternal.h
@@ -1,4 +1,4 @@
-/* $Id: DBGFInternal.h 32253 2010-09-06 15:14:24Z vboxsync $ */
+/* $Id: DBGFInternal.h 32340 2010-09-09 11:51:09Z vboxsync $ */
 /** @file
  * DBGF - Internal header file.
  */
@@ -308,27 +308,6 @@ typedef struct DBGFCPU
 } DBGFCPU;
 /** Pointer to DBGFCPU data. */
 typedef DBGFCPU *PDBGFCPU;
-
-
-/** DBGCORECOREDESCRIPTOR::u32Magic. */
-#define DBGFCORE_MAGIC      0xc01ac0de
-
-/**
- * The DBGF Core descriptor.
- */
-typedef struct DBGFCOREDESCRIPTOR
-{
-    /** The core file magic (DBGFCORE_MAGIC) */
-    uint32_t                u32Magic;
-    /** VirtualBox version. */
-    uint64_t                VBoxVersion;
-    /** VirtualBox revision. */
-    uint64_t                VBoxRevision;
-    /** Number of CPUs. */
-    uint32_t                cCpus;
-} DBGFCOREDESCRIPTOR;
-/** Pointer to DBGFCOREDESCRIPTOR data. */
-typedef DBGFCOREDESCRIPTOR  *PDBGFCOREDESCRIPTOR;
 
 
 int  dbgfR3AsInit(PVM pVM);
