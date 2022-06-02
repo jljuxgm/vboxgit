1332d8f881421667d1a78dd776f671eea7cdfbac
qzuser <395143360@qq.com>
2022-06-03 04:34:15 +0800

    commit_34983 ------------------------------------------------------------------------ r34983 | vboxsync | 2010-12-13 18:14:08 +0800 (Mon, 13 Dec 2010) | 2 lines
    FE/Qt4: more progress images
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UINewVMWzd.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UINewVMWzd.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINewVMWzd.cpp 34907 2010-12-09 16:39:41Z vboxsync $ */
+/* $Id: UINewVMWzd.cpp 34983 2010-12-13 10:14:08Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -496,7 +496,7 @@ void UINewVMWzdPage4::ensureNewHardDiskDeleted()
     CProgress progress = m_HardDisk.DeleteStorage();
     if (m_HardDisk.isOk())
     {
-        vboxProblem().showModalProgressDialog(progress, windowTitle(), ":/progress_delete_90px.png", this, true);
+        vboxProblem().showModalProgressDialog(progress, windowTitle(), ":/progress_media_delete_90px.png", this, true);
         if (progress.isOk() && progress.GetResultCode() == S_OK)
             success = true;
     }
