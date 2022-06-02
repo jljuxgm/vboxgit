6e7a5b9334fe52e07f0963fe17dc484a08c35ab5
qzuser <395143360@qq.com>
2022-06-02 18:10:36 +0800

    commit_32900 ------------------------------------------------------------------------ r32900 | vboxsync | 2010-10-05 18:00:18 +0800 (Tue, 05 Oct 2010) | 1 line
    Main: typo
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/SnapshotImpl.cpp
+++ vbox/src/VBox/Main/SnapshotImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: SnapshotImpl.cpp 32780 2010-09-27 19:00:22Z vboxsync $ */
+/* $Id: SnapshotImpl.cpp 32900 2010-10-05 10:00:18Z vboxsync $ */
 
 /** @file
  *
@@ -3065,7 +3065,7 @@ void SessionMachine::cancelDeleteSnapshotMedium(const ComObjPtr<Medium> &aHD,
 
         if (aHD->getParent().isNull())
         {
-            HRESULT rc = aHD->UnlockWrite(NULL);;
+            HRESULT rc = aHD->UnlockWrite(NULL);
             AssertComRC(rc);
         }
         else
