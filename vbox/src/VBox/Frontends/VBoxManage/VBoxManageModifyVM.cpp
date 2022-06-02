1703743ba306cd814ea34abe2600fe2988a84413
qzuser <395143360@qq.com>
2022-06-02 11:18:26 +0800

    commit_31298 ------------------------------------------------------------------------ r31298 | vboxsync | 2010-08-02 21:16:21 +0800 (Mon, 02 Aug 2010) | 2 lines
    gcc warning
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
+++ vbox/src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageModifyVM.cpp 31291 2010-08-02 12:29:35Z vboxsync $ */
+/* $Id: VBoxManageModifyVM.cpp 31298 2010-08-02 13:16:21Z vboxsync $ */
 /** @file
  * VBoxManage - Implementation of modifyvm command.
  */
@@ -155,7 +155,7 @@ enum
     MODIFYVM_HARDWARE_UUID,
     MODIFYVM_HPET,
     MODIFYVM_IOCACHE,
-    MODIFYVM_IOCACHESIZE,
+    MODIFYVM_IOCACHESIZE
 };
 
 static const RTGETOPTDEF g_aModifyVMOptions[] =
