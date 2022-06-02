a8676b5d8f1bce4921cb98a3e4c1c6e322ba016a
qzuser <395143360@qq.com>
2022-06-02 23:00:18 +0800

    commit_33895 ------------------------------------------------------------------------ r33895 | vboxsync | 2010-11-09 20:41:28 +0800 (Tue, 09 Nov 2010) | 1 line
    Build fix.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo-win.cpp
+++ vbox/src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceVMInfo-win.cpp 33891 2010-11-09 12:09:25Z vboxsync $ */
+/* $Id: VBoxServiceVMInfo-win.cpp 33895 2010-11-09 12:41:28Z vboxsync $ */
 /** @file
  * VBoxService - Virtual Machine Information for the Host, Windows specifics.
  */
@@ -392,7 +392,7 @@ bool VBoxServiceVMInfoWinIsLoggedIn(PVBOXSERVICEVMINFOUSER a_pUserInfo, PLUID a_
                               &dwDomainNameSize,
                               &enmOwnerType))
         {
-            DWOR dwErr = GetLastError();
+            DWORD dwErr = GetLastError();
             /*
              * If a network time-out prevents the function from finding the name or
              * if a SID that does not have a corresponding account name (such as a
