10cf39561baeeb923c7e28463467cdd891b368e2
qzuser <395143360@qq.com>
2022-06-02 17:42:20 +0800

    commit_32771 ------------------------------------------------------------------------ r32771 | vboxsync | 2010-09-27 20:09:42 +0800 (Mon, 27 Sep 2010) | 1 line
    VBoxService/VMInfo: Fixed LsaEnumerate() error case when shutting down the system, return rc.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo.cpp
+++ vbox/src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceVMInfo.cpp 32731 2010-09-23 14:50:12Z vboxsync $ */
+/* $Id: VBoxServiceVMInfo.cpp 32771 2010-09-27 12:09:42Z vboxsync $ */
 /** @file
  * VBoxService - Virtual Machine Information for the Host.
  */
@@ -331,7 +331,7 @@ static int vboxserviceVMInfoWriteUsers(void)
     }
     if (RT_SUCCESS(rc) && pszUserList)
         RTStrFree(pszUserList);
-    return VINF_SUCCESS;
+    return rc;
 }
 
 
