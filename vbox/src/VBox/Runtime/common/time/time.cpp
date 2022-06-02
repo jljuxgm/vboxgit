faba9196a74ccb941a02fed4a6a309923a8cfcb9
qzuser <395143360@qq.com>
2022-06-03 04:37:19 +0800

    commit_34993 ------------------------------------------------------------------------ r34993 | vboxsync | 2010-12-13 20:00:52 +0800 (Mon, 13 Dec 2010) | 1 line
    IPRT/RTTimeExplode: Use remainder.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/time/time.cpp
+++ vbox/src/VBox/Runtime/common/time/time.cpp
@@ -1,4 +1,4 @@
-/* $Id: time.cpp 34990 2010-12-13 11:12:42Z vboxsync $ */
+/* $Id: time.cpp 34993 2010-12-13 12:00:52Z vboxsync $ */
 /** @file
  * IPRT - Time.
  */
@@ -282,7 +282,7 @@ RTDECL(PRTTIME) RTTimeExplode(PRTTIME pTime, PCRTTIMESPEC pTimeSpec)
     pTime->u32Nanosecond = i32Rem;
 
     /* second */
-    i32Rem = ASMDivS64ByS32RetS32(i64Div, 60);
+    i32Rem = (int32_t)ASMModU64ByU32RetU32(i64Div, 60);
     i64Div /= 60;
     if (i32Rem < 0)
     {
