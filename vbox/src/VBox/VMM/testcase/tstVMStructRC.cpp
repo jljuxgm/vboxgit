568273a7677a9cb63603f75966bdca3e23fd3661
qzuser <395143360@qq.com>
2022-06-03 06:35:44 +0800

    commit_35601 ------------------------------------------------------------------------ r35601 | vboxsync | 2011-01-18 18:43:11 +0800 (Tue, 18 Jan 2011) | 1 line
    DBGF,CPUM: Expose hypervisor registers as hypercpuX.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/testcase/tstVMStructRC.cpp
+++ vbox/src/VBox/VMM/testcase/tstVMStructRC.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstVMStructRC.cpp 35466 2011-01-10 16:36:35Z vboxsync $ */
+/* $Id: tstVMStructRC.cpp 35601 2011-01-18 10:43:11Z vboxsync $ */
 /** @file
  * tstVMMStructRC - Generate structure member and size checks from the
  *                  RC perspective.
@@ -133,7 +133,8 @@ int main()
     GEN_CHECK_SIZE(DBGFCPU);
     GEN_CHECK_OFF(DBGFCPU, iActiveBp);
     GEN_CHECK_OFF(DBGFCPU, fSingleSteppingRaw);
-    GEN_CHECK_OFF(DBGFCPU, pRegSet);
+    GEN_CHECK_OFF(DBGFCPU, pGuestRegSet);
+    GEN_CHECK_OFF(DBGFCPU, pHyperRegSet);
 
     GEN_CHECK_SIZE(EM);
     GEN_CHECK_OFF(EM, offVM);
