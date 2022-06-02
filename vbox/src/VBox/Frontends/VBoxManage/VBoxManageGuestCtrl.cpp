fe9c7ff1e43a289051810006f5e9587276c8ddc7
qzuser <395143360@qq.com>
2022-06-02 22:12:08 +0800

    commit_33796 ------------------------------------------------------------------------ r33796 | vboxsync | 2010-11-06 00:03:20 +0800 (Sat, 06 Nov 2010) | 1 line
    Additional check.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ vbox/src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageGuestCtrl.cpp 33793 2010-11-05 14:21:25Z vboxsync $ */
+/* $Id: VBoxManageGuestCtrl.cpp 33796 2010-11-05 16:03:20Z vboxsync $ */
 /** @file
  * VBoxManage - Implementation of guestcontrol command.
  */
@@ -782,10 +782,12 @@ int ctrlCopyInit(const char *pszSource, const char *pszDest, uint32_t uFlags,
                 if (RT_SUCCESS(rc))
                     rc = RTStrAAppend(&pszDestAbs, RTPathFilename(pszSourceAbs));
 
-                RTListInit(pList);
-                rc = ctrlCopyDirectoryEntryAppend(pszSourceAbs, pszDestAbs, pList);
-                *pcObjects = 1;
-
+                if (RT_SUCCESS(rc))
+                {
+                    RTListInit(pList);
+                    rc = ctrlCopyDirectoryEntryAppend(pszSourceAbs, pszDestAbs, pList);
+                    *pcObjects = 1;
+                }
                 RTStrFree(pszDestAbs);
             }
             else
