29de08337e4d4688f2a5ae02f96e0ddd7d17ab10
qzuser <395143360@qq.com>
2022-06-02 11:07:57 +0800

    commit_31252 ------------------------------------------------------------------------ r31252 | vboxsync | 2010-07-30 23:38:52 +0800 (Fri, 30 Jul 2010) | 1 line
    Made 'DEFS += RTMEM_WRAP_TO_EF_APIS' work better.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/alloc.cpp
+++ vbox/src/VBox/Runtime/r3/alloc.cpp
@@ -1,4 +1,4 @@
-/* $Id: alloc.cpp 31157 2010-07-28 03:15:35Z vboxsync $ */
+/* $Id: alloc.cpp 31252 2010-07-30 15:38:52Z vboxsync $ */
 /** @file
  * IPRT - Memory Allocation.
  */
@@ -28,7 +28,7 @@
 /*******************************************************************************
 *   Defined Constants And Macros                                               *
 *******************************************************************************/
-#ifdef RTMEM_WRAP_TO_EF_APIS
+#if defined(RTMEM_WRAP_TO_EF_APIS) && !defined(RTMEM_NO_WRAP_TO_EF_APIS)
 # undef RTMEM_WRAP_TO_EF_APIS
 # define RTALLOC_USE_EFENCE 1
 #endif
