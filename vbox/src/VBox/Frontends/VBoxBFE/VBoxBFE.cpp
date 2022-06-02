63b9ee6539cb91b392b653d75fbfb9f427ec82a8
qzuser <395143360@qq.com>
2022-06-02 16:16:01 +0800

    commit_32388 ------------------------------------------------------------------------ r32388 | vboxsync | 2010-09-10 18:13:07 +0800 (Fri, 10 Sep 2010) | 2 lines
    more branding fixes
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxBFE/VBoxBFE.cpp
+++ vbox/src/VBox/Frontends/VBoxBFE/VBoxBFE.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxBFE.cpp 32191 2010-09-02 12:30:40Z vboxsync $ */
+/* $Id: VBoxBFE.cpp 32388 2010-09-10 10:13:07Z vboxsync $ */
 /** @file
  * Basic Frontend (BFE): VBoxBFE main routines.
  *
@@ -428,7 +428,7 @@ extern "C" DECLEXPORT(int) TrustedMain (int argc, char **argv, char **envp)
 #endif
     int rc = VINF_SUCCESS;
 
-    RTPrintf("Sun VirtualBox Simple SDL GUI built %s %s\n", __DATE__, __TIME__);
+    RTPrintf(VBOX_PRODUCT " Simple SDL GUI built %s %s\n", __DATE__, __TIME__);
 
     // less than one parameter is not possible
     if (argc < 2)
