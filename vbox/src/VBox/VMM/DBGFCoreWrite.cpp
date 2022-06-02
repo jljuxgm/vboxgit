576258b1c78ddd25665187608c518f2e7c6c532d
qzuser <395143360@qq.com>
2022-06-02 15:35:40 +0800

    commit_32218 ------------------------------------------------------------------------ r32218 | vboxsync | 2010-09-03 00:20:33 +0800 (Fri, 03 Sep 2010) | 2 lines
    debug build fix
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/DBGFCoreWrite.cpp
+++ vbox/src/VBox/VMM/DBGFCoreWrite.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFCoreWrite.cpp 32217 2010-09-02 15:47:40Z vboxsync $ */
+/* $Id: DBGFCoreWrite.cpp 32218 2010-09-02 16:20:33Z vboxsync $ */
 /** @file
  * DBGF - Debugger Facility, Guest Core Dump.
  */
@@ -237,7 +237,7 @@ static uint32_t dbgfR3GetRamRangeCount(PVM pVM)
  * @return VBox status code.
  * @remarks The VM must be suspended before calling this function.
  */
-static DECLCALLBACK(int) dbgfR3CoreWrite(PVM pVM, PVMCPU pVCpu, void *pvData)
+static DECLCALLBACK(VBOXSTRICTRC) dbgfR3CoreWrite(PVM pVM, PVMCPU pVCpu, void *pvData)
 {
     /*
      * Validate input.
