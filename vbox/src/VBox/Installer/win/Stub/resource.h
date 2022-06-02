f3631ea8f5b64533401de98ad8930d42fa882aa4
qzuser <395143360@qq.com>
2022-06-02 13:06:02 +0800

    commit_31659 ------------------------------------------------------------------------ r31659 | vboxsync | 2010-08-13 23:13:08 +0800 (Fri, 13 Aug 2010) | 2 lines
    export Windows installer to OSE
    
    ------------------------------------------------------------------------

---
+++ vbox/src/VBox/Installer/win/Stub/resource.h
@@ -0,0 +1,13 @@
+/* $Id: resource.h 31659 2010-08-13 15:13:08Z vboxsync $ */
+/** @file
+ * VBoxStub - resource header file.
+ */
+
+/*
+ * Copyright (C) 2009 Oracle Corporation
+ *
+ * Oracle Corporation confidential
+ * All rights reserved
+ */
+
+#define IDI_VIRTUALBOX 101
