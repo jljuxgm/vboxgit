2d9ae1bc9142f34111663c20cd087fd8cdf725f7
qzuser <395143360@qq.com>
2022-06-02 18:43:06 +0800

    commit_33056 ------------------------------------------------------------------------ r33056 | vboxsync | 2010-10-12 20:09:42 +0800 (Tue, 12 Oct 2010) | 2 lines
    Main-XML: add a xml memory writer
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/xml.cpp
+++ vbox/src/VBox/Runtime/r3/xml.cpp
@@ -1,4 +1,4 @@
-/* $Id: xml.cpp 32565 2010-09-16 14:39:24Z vboxsync $ */
+/* $Id: xml.cpp 33056 2010-10-12 12:09:42Z vboxsync $ */
 /** @file
  * IPRT - XML Manipulation API.
  */
@@ -1382,6 +1382,29 @@ void XmlMemParser::read(const void* pvBuf, int cbSize,
         throw XmlError(xmlCtxtGetLastError(m_ctxt));
 
     doc.refreshInternals();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// XmlMemWriter class
+//
+////////////////////////////////////////////////////////////////////////////////
+
+XmlMemWriter::XmlMemWriter()
+{
+}
+
+XmlMemWriter::~XmlMemWriter()
+{
+}
+
+void XmlMemWriter::write(const Document &doc, void **ppvBuf, size_t *pcbSize)
+{
+    xmlChar* pBuf;
+    int size;
+    xmlDocDumpFormatMemory(doc.m->plibDocument, &pBuf, &size, 1);
+    *ppvBuf = pBuf;
+    *pcbSize = size;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
