3d96a322c3389f2658a0fe1d00a077558b36509f
qzuser <395143360@qq.com>
2022-06-03 05:46:02 +0800

    commit_35351 ------------------------------------------------------------------------ r35351 | vboxsync | 2010-12-28 01:04:17 +0800 (Tue, 28 Dec 2010) | 1 line
    scm cleanups.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxManage/VBoxManageDebugVM.cpp
+++ vbox/src/VBox/Frontends/VBoxManage/VBoxManageDebugVM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageDebugVM.cpp 35306 2010-12-22 17:30:04Z vboxsync $ */
+/* $Id: VBoxManageDebugVM.cpp 35351 2010-12-27 17:04:17Z vboxsync $ */
 /** @file
  * VBoxManage - Implementation of the debugvm command.
  */
@@ -301,5 +301,4 @@ int handleDebugVM(HandlerArg *pArgs)
 
     return rcExit;
 }
-
 
