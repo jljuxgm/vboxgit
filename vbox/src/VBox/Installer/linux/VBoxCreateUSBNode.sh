8eee0b0b66681a3f090cf878a240e6eed3ad18ad
qzuser <395143360@qq.com>
2022-06-03 16:21:37 +0800

    commit_38093 ------------------------------------------------------------------------ r38093 | vboxsync | 2011-07-21 20:45:30 +0800 (Thu, 21 Jul 2011) | 1 line
    *.kmk,++: Mode fixes to maximize hardlinking potential during staging.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Installer/linux/VBoxCreateUSBNode.sh
+++ vbox/src/VBox/Installer/linux/VBoxCreateUSBNode.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $Id: VBoxCreateUSBNode.sh 37339 2011-06-07 09:40:34Z vboxsync $ */
+# $Id: VBoxCreateUSBNode.sh 38093 2011-07-21 12:45:30Z vboxsync $ */
 ## @file
 # VirtualBox USB Proxy Service, Linux Specialization.
 # udev helper for creating and removing device nodes for VirtualBox USB devices
