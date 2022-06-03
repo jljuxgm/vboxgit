252cdcb42cda1a4eefe2702c1c034197950403fc
qzuser <395143360@qq.com>
2022-06-03 23:46:16 +0800

    commit_39865 ------------------------------------------------------------------------ r39865 | vboxsync | 2012-01-25 09:04:59 +0800 (Wed, 25 Jan 2012) | 1 line
    build fix.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/win/process-win.cpp
+++ vbox/src/VBox/Runtime/r3/win/process-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: process-win.cpp 39864 2012-01-24 21:24:52Z vboxsync $ */
+/* $Id: process-win.cpp 39865 2012-01-25 01:04:59Z vboxsync $ */
 /** @file
  * IPRT - Process, Windows.
  */
@@ -857,7 +857,7 @@ static int rtProcCreateAsUserHlp(PRTUTF16 pwszUser, PRTUTF16 pwszPassword, PRTUT
                     }
                     else
                         dwErr = GetLastError(); /* LookupAccountNameW() failed. */
-                    RTMemFree(pSID);
+                    RTMemFree(pSid);
                     RTMemFree(pwszDomain);
                 }
             }
