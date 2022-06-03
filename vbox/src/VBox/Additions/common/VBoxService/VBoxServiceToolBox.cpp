161be83c68031b307867c19da834c45e5c9da6f6
qzuser <395143360@qq.com>
2022-06-04 03:21:26 +0800

    commit_40682 ------------------------------------------------------------------------ r40682 | vboxsync | 2012-03-28 22:36:01 +0800 (Wed, 28 Mar 2012) | 1 line
    VBoxService/GuestCtrl: Bugfixes.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
+++ vbox/src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
@@ -1,10 +1,10 @@
-/* $Id: VBoxServiceToolBox.cpp 39612 2011-12-14 14:19:55Z vboxsync $ */
+/* $Id: VBoxServiceToolBox.cpp 40682 2012-03-28 14:36:01Z vboxsync $ */
 /** @file
  * VBoxServiceToolbox - Internal (BusyBox-like) toolbox.
  */
 
 /*
- * Copyright (C) 2011 Oracle Corporation
+ * Copyright (C) 2012 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
