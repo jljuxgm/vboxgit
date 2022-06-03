38d3ecc23d802402e4c1c2895ce60aca50a6a279
qzuser <395143360@qq.com>
2022-06-03 16:36:56 +0800

    commit_38140 ------------------------------------------------------------------------ r38140 | vboxsync | 2011-07-25 18:01:03 +0800 (Mon, 25 Jul 2011) | 2 lines
    Solaris/VBoxNetBow: fix for using multiple host NICs, caused by using duplicate VNIC IDs.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
+++ vbox/src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetFltInternal.h 36956 2011-05-04 12:54:03Z vboxsync $ */
+/* $Id: VBoxNetFltInternal.h 38140 2011-07-25 10:01:03Z vboxsync $ */
 /** @file
  * VBoxNetFlt - Network Filter Driver (Host), Internal Header.
  */
@@ -183,8 +183,6 @@ typedef struct VBOXNETFLTINS
             bool fIsVNICTemplate;
             /** Handle to list of created VNICs. */
             list_t hVNICs;
-            /** Instance number while creating VNICs. */
-            uint64_t uInstance;
             /** The MAC address of the host interface. */
             RTMAC MacAddr;
             /** Handle of this interface (lower MAC). */
