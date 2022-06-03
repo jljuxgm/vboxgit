15414842d0bbc614232ff639554aaaec4f255705
qzuser <395143360@qq.com>
2022-06-04 06:50:49 +0800

    commit_41777 ------------------------------------------------------------------------ r41777 | vboxsync | 2012-06-17 02:49:15 +0800 (Sun, 17 Jun 2012) | 2 lines
    Doxygen.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMAll/REMAll.cpp
+++ vbox/src/VBox/VMM/VMMAll/REMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: REMAll.cpp 40274 2012-02-28 13:17:35Z vboxsync $ */
+/* $Id: REMAll.cpp 41777 2012-06-16 18:49:15Z vboxsync $ */
 /** @file
  * REM - Recompiled Execution Monitor, all Contexts part.
  */
@@ -238,7 +238,7 @@ VMMDECL(void) REMNotifyHandlerPhysicalFlushIfAlmostFull(PVM pVM, PVMCPU pVCpu)
 /**
  * Make REM flush all translation block upon the next call to REMR3State().
  *
- * @param   pVM             Pointer to the shared VM structure.
+ * @param   pVM             Pointer to the VM.
  */
 VMMDECL(void) REMFlushTBs(PVM pVM)
 {
