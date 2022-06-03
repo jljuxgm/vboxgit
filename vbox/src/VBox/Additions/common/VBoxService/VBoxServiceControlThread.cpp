c870a17683e195151d44e8b70cfa9acf54d2e916
qzuser <395143360@qq.com>
2022-06-03 20:58:51 +0800

    commit_39314 ------------------------------------------------------------------------ r39314 | vboxsync | 2011-11-15 23:07:39 +0800 (Tue, 15 Nov 2011) | 1 line
    VBoxService: Rename VBoxServiceControlExecThread.cpp -> VBoxServiceControlThread.cpp.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxService/VBoxServiceControlThread.cpp
+++ vbox/src/VBox/Additions/common/VBoxService/VBoxServiceControlThread.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceControlExecThread.cpp 39313 2011-11-15 14:43:38Z vboxsync $ */
+/* $Id: VBoxServiceControlThread.cpp 39314 2011-11-15 15:07:39Z vboxsync $ */
 /** @file
  * VBoxServiceControlExecThread - Thread for every started guest process.
  */
