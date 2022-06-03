779378a250bdd997c2b78f571613737c2176ece6
qzuser <395143360@qq.com>
2022-06-03 09:03:54 +0800

    commit_36262 ------------------------------------------------------------------------ r36262 | vboxsync | 2011-03-11 22:50:45 +0800 (Fri, 11 Mar 2011) | 1 line
    SUPDrv,IPRT,++: Enabled the code for supporting up to 256 host CPUs/cores/threads.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r0drv/nt/initterm-r0drv-nt.cpp
+++ vbox/src/VBox/Runtime/r0drv/nt/initterm-r0drv-nt.cpp
@@ -1,4 +1,4 @@
-/* $Id: initterm-r0drv-nt.cpp 36232 2011-03-09 16:41:09Z vboxsync $ */
+/* $Id: initterm-r0drv-nt.cpp 36262 2011-03-11 14:50:45Z vboxsync $ */
 /** @file
  * IPRT - Initialization & Termination, R0 Driver, NT.
  */
@@ -82,10 +82,8 @@ int rtR0InitNative(void)
     KAFFINITY ActiveProcessors = KeQueryActiveProcessors();
 #endif
     RTCpuSetEmpty(&g_rtMpNtCpuSet);
-#ifdef RT_WITH_LOTS_OF_CPUS
-# error "port me"
-#endif
     RTCpuSetFromU64(&g_rtMpNtCpuSet, ActiveProcessors);
+/** @todo Port to W2K8 with > 64 cpus/threads. */
 
 #ifdef IPRT_TARGET_NT4
     g_pfnrtNtExSetTimerResolution = NULL;
