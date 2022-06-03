26cdea634e7c686227def2b9335b2d38c12e9878
qzuser <395143360@qq.com>
2022-06-04 03:16:27 +0800

    commit_40656 ------------------------------------------------------------------------ r40656 | vboxsync | 2012-03-27 04:07:36 +0800 (Tue, 27 Mar 2012) | 1 line
    Added counters so that VT-x exit stats add up.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/include/HWACCMInternal.h
+++ vbox/src/VBox/VMM/include/HWACCMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: HWACCMInternal.h 40561 2012-03-21 11:57:10Z vboxsync $ */
+/* $Id: HWACCMInternal.h 40656 2012-03-26 20:07:36Z vboxsync $ */
 /** @file
  * HM - Internal header file.
  */
@@ -800,6 +800,7 @@ typedef struct HWACCMCPU
     STAMCOUNTER             StatExitShadowNM;
     STAMCOUNTER             StatExitGuestNM;
     STAMCOUNTER             StatExitShadowPF;
+    STAMCOUNTER             StatExitShadowPFEM;
     STAMCOUNTER             StatExitGuestPF;
     STAMCOUNTER             StatExitGuestUD;
     STAMCOUNTER             StatExitGuestSS;
@@ -808,6 +809,9 @@ typedef struct HWACCMCPU
     STAMCOUNTER             StatExitGuestDE;
     STAMCOUNTER             StatExitGuestDB;
     STAMCOUNTER             StatExitGuestMF;
+    STAMCOUNTER             StatExitGuestBP;
+    STAMCOUNTER             StatExitGuestXF;
+    STAMCOUNTER             StatExitGuestXcpUnk;
     STAMCOUNTER             StatExitInvpg;
     STAMCOUNTER             StatExitInvd;
     STAMCOUNTER             StatExitCpuid;
