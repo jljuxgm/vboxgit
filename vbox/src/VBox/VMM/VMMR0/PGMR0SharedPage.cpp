7c45dc4809e5944f9b95d9c9b6b920581e0c0c8c
qzuser <395143360@qq.com>
2022-06-03 11:34:02 +0800

    commit_36983 ------------------------------------------------------------------------ r36983 | vboxsync | 2011-05-06 21:48:56 +0800 (Fri, 06 May 2011) | 1 line
    nit
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR0/PGMR0SharedPage.cpp
+++ vbox/src/VBox/VMM/VMMR0/PGMR0SharedPage.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMR0SharedPage.cpp 36891 2011-04-29 13:22:57Z vboxsync $ */
+/* $Id: PGMR0SharedPage.cpp 36983 2011-05-06 13:48:56Z vboxsync $ */
 /** @file
  * PGM - Page Manager and Monitor, Page Sharing, Ring-0.
  */
@@ -142,5 +142,5 @@ VMMR0DECL(int) PGMR0SharedModuleCheck(PVM pVM, PGVM pGVM, VMCPUID idCpu, PGMMSHA
 
     return rc;
 }
-#endif
+#endif /* VBOX_WITH_PAGE_SHARING */
 
