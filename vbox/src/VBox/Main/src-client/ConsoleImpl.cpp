c706e0fbd5fa56d3fcae9d2c7d74a31a856d4e89
qzuser <395143360@qq.com>
2022-06-03 14:21:55 +0800

    commit_37661 ------------------------------------------------------------------------ r37661 | vboxsync | 2011-06-28 19:40:47 +0800 (Tue, 28 Jun 2011) | 2 lines
    Main/Console: fix very old copy/paste bug in comment
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/ConsoleImpl.cpp
+++ vbox/src/VBox/Main/src-client/ConsoleImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.cpp 37591 2011-06-22 15:58:22Z vboxsync $ */
+/* $Id: ConsoleImpl.cpp 37661 2011-06-28 11:40:47Z vboxsync $ */
 /** @file
  * VBox Console COM Class implementation
  */
@@ -9601,7 +9601,7 @@ DECLCALLBACK(int) Console::drvStatus_Construct(PPDMDRVINS pDrvIns, PCFGMNODE pCf
 
 
 /**
- * Keyboard driver registration record.
+ * Console status driver (LED) registration record.
  */
 const PDMDRVREG Console::DrvStatusReg =
 {
