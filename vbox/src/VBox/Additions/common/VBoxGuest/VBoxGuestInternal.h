bc48e1f5a9c46fcdbb6a71cbef2fc1f0df69572d
qzuser <395143360@qq.com>
2022-06-03 18:34:58 +0800

    commit_38653 ------------------------------------------------------------------------ r38653 | vboxsync | 2011-09-06 17:28:41 +0800 (Tue, 06 Sep 2011) | 2 lines
    additional assertion
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuestInternal.h
+++ vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuestInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestInternal.h 38652 2011-09-06 09:02:35Z vboxsync $ */
+/* $Id: VBoxGuestInternal.h 38653 2011-09-06 09:28:41Z vboxsync $ */
 /** @file
  * VBoxGuest - Guest Additions Driver.
  */
@@ -195,6 +195,7 @@ AssertCompileMemberOffset(VBOXGUESTDEVEXT, win, 384);
 AssertCompileMemberOffset(VBOXGUESTDEVEXT, win, 320);
 #endif
 AssertCompileMemberAlignment(VBOXGUESTDEVEXT, win, 64);
+AssertCompileMemberSize(VBOXGUESTDEVEXT, win, 256);
 
 /**
  * The VBoxGuest per session data.
