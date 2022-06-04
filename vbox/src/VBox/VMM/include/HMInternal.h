4dd4719ba257e79bf1f439fc44fef0e1ec793ea7
qzuser <395143360@qq.com>
2022-06-04 16:32:15 +0800

    commit_44245 ------------------------------------------------------------------------ r44245 | vboxsync | 2013-01-08 13:48:31 +0800 (Tue, 08 Jan 2013) | 2 lines
    VMM: Added stats counter for preemption timer exits.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/include/HMInternal.h
+++ vbox/src/VBox/VMM/include/HMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: HMInternal.h 44202 2012-12-24 14:29:59Z vboxsync $ */
+/* $Id: HMInternal.h 44245 2013-01-08 05:48:31Z vboxsync $ */
 /** @file
  * HM - Internal header file.
  */
@@ -783,6 +783,7 @@ typedef struct HMCPU
     STAMCOUNTER             StatExitIntWindow;
     STAMCOUNTER             StatExitMaxResume;
     STAMCOUNTER             StatExitPreemptPending;
+    STAMCOUNTER             StatExitPreemptTimer;
     STAMCOUNTER             StatExitMtf;
     STAMCOUNTER             StatIntReinject;
     STAMCOUNTER             StatPendingHostIrq;
