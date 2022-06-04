9bc9dc06272a25c8132eaea4a37c2f6b8a57a255
qzuser <395143360@qq.com>
2022-06-05 07:23:11 +0800

    commit_47693 ------------------------------------------------------------------------ r47693 | vboxsync | 2013-08-13 21:09:31 +0800 (Tue, 13 Aug 2013) | 1 line
    FE/Qt: Popup-center: Popup-stack: Support for non-top-level parents.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/globals/UIPopupCenter.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/globals/UIPopupCenter.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIPopupCenter.cpp 47644 2013-08-09 13:40:03Z vboxsync $ */
+/* $Id: UIPopupCenter.cpp 47693 2013-08-13 13:09:31Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -371,7 +371,7 @@ void UIPopupCenter::assignPopupStackParent(UIPopupStack *pPopupStack, QWidget *p
     AssertPtrReturnVoid(pParent);
 
     /* Assign event-filter: */
-    pParent->installEventFilter(pPopupStack);
+    pParent->window()->installEventFilter(pPopupStack);
 
     /* Assign parent depending on passed *stack* type: */
     switch (stackType)
@@ -400,7 +400,7 @@ void UIPopupCenter::unassignPopupStackParent(UIPopupStack *pPopupStack, QWidget 
     pPopupStack->setParent(0);
 
     /* Unassign event-filter: */
-    pParent->removeEventFilter(pPopupStack);
+    pParent->window()->removeEventFilter(pPopupStack);
 }
 
 void UIPopupCenter::cannotSendACPIToMachine(QWidget *pParent)
--- vbox/src/VBox/Frontends/VirtualBox/src/widgets/UIPopupStack.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/widgets/UIPopupStack.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIPopupStack.cpp 47644 2013-08-09 13:40:03Z vboxsync $ */
+/* $Id: UIPopupStack.cpp 47693 2013-08-13 13:09:31Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -115,6 +115,8 @@ void UIPopupStack::sltAdjustGeometry()
 
     /* Read parent geometry: */
     QRect geo(parentWidget()->geometry());
+    if (!parentWidget()->isWindow())
+        geo.moveTo(parentWidget()->mapToGlobal(QPoint(0, 0)));
 
     /* Determine size: */
     int iWidth = parentWidget()->width();
