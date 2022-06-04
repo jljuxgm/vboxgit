f937b1d05bb06d8533d23847181bf2e4a79ea910
qzuser <395143360@qq.com>
2022-06-04 23:32:32 +0800

    commit_45618 ------------------------------------------------------------------------ r45618 | vboxsync | 2013-04-19 02:41:07 +0800 (Fri, 19 Apr 2013) | 1 line
    Do HMR3Init first in vmR3InitRing3 so the other components can skip raw-mode bits during init.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/testcase/tstVMStruct.h
+++ vbox/src/VBox/VMM/testcase/tstVMStruct.h
@@ -1,4 +1,4 @@
-/* $Id: tstVMStruct.h 45305 2013-04-03 11:15:02Z vboxsync $ */
+/* $Id: tstVMStruct.h 45618 2013-04-18 18:41:07Z vboxsync $ */
 /** @file
  * tstVMMStruct - Statements for generating VM and VMCPU offset and size tests.
  *
@@ -1344,7 +1344,7 @@
     GEN_CHECK_OFF(VM, fPATMEnabled);
     GEN_CHECK_OFF(VM, fCSAMEnabled);
     GEN_CHECK_OFF(VM, fHMEnabled);
-    GEN_CHECK_OFF(VM, fHwVirtExtForced);
+    GEN_CHECK_OFF(VM, fHMEnabledFixed);
     GEN_CHECK_OFF(VM, fFaultTolerantMaster);
     GEN_CHECK_OFF(VM, fUseLargePages);
     GEN_CHECK_OFF(VM, hTraceBufRC);
