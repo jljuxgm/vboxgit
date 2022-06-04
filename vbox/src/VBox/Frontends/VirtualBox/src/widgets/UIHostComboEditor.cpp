0ca2bed166ed21d545aae5844b63e31705aab398
qzuser <395143360@qq.com>
2022-06-04 18:40:50 +0800

    commit_44753 ------------------------------------------------------------------------ r44753 | vboxsync | 2013-02-19 22:57:00 +0800 (Tue, 19 Feb 2013) | 1 line
    FE/Qt: 6065: Global settings / Input page / VM shortcut table will reflect 'Host+' prefix for each of the usual shortcuts it have.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/widgets/UIHostComboEditor.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/widgets/UIHostComboEditor.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIHostComboEditor.cpp 44737 2013-02-18 14:08:20Z vboxsync $ */
+/* $Id: UIHostComboEditor.cpp 44753 2013-02-19 14:57:00Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -263,6 +263,16 @@ void UINativeHotKey::retranslateKeyNames()
 namespace UIHostCombo
 {
     int m_iMaxComboSize = 3;
+}
+
+int UIHostCombo::hostComboModifierIndex()
+{
+    return -1;
+}
+
+QString UIHostCombo::hostComboModifierName()
+{
+    return UIHostComboEditor::tr("Host+");
 }
 
 QString UIHostCombo::hostComboCacheKey()
