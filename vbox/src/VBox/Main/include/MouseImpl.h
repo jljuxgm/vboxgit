f10c6487efd78fd44a73d601a2f6ccd31c2d6a1e
qzuser <395143360@qq.com>
2022-06-05 07:51:19 +0800

    commit_47841 ------------------------------------------------------------------------ r47841 | vboxsync | 2013-08-19 21:20:58 +0800 (Mon, 19 Aug 2013) | 1 line
    Main/Devices: reverted combined use of two emulated USB devices for reporting absolute events.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/MouseImpl.h
+++ vbox/src/VBox/Main/include/MouseImpl.h
@@ -1,4 +1,4 @@
-/* $Id: MouseImpl.h 47804 2013-08-16 12:26:40Z vboxsync $ */
+/* $Id: MouseImpl.h 47841 2013-08-19 13:20:58Z vboxsync $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -25,7 +25,7 @@
 #include <VBox/vmm/pdmdrv.h>
 
 /** Maximum number of devices supported */
-enum { MOUSE_MAX_DEVICES = 10 };
+enum { MOUSE_MAX_DEVICES = 3 };
 /** Mouse driver instance data. */
 typedef struct DRVMAINMOUSE DRVMAINMOUSE, *PDRVMAINMOUSE;
 
