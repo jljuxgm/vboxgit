e2036bfee7a13e293cf97189bb65a075df363b14
qzuser <395143360@qq.com>
2022-06-05 06:58:55 +0800

    commit_47563 ------------------------------------------------------------------------ r47563 | vboxsync | 2013-08-07 01:13:57 +0800 (Wed, 07 Aug 2013) | 1 line
    FE/Qt: Settings dialog: New validation stuff: Step 2 (Removing old validation stuff).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/settings/global/UIGlobalSettingsInput.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/settings/global/UIGlobalSettingsInput.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGlobalSettingsInput.cpp 47559 2013-08-06 13:58:47Z vboxsync $ */
+/* $Id: UIGlobalSettingsInput.cpp 47563 2013-08-06 17:13:57Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -159,11 +159,7 @@ void UIGlobalSettingsInput::saveFromCacheTo(QVariant &data)
     UISettingsPageGlobal::uploadData(data);
 }
 
-#ifdef VBOX_WITH_NEW_SETTINGS_VALIDATOR
 void UIGlobalSettingsInput::setValidator(UIPageValidator *pValidator)
-#else /* VBOX_WITH_NEW_SETTINGS_VALIDATOR */
-void UIGlobalSettingsInput::setValidator(QIWidgetValidator *pValidator)
-#endif /* !VBOX_WITH_NEW_SETTINGS_VALIDATOR */
 {
     /* Configure validation: */
     m_pValidator = pValidator;
