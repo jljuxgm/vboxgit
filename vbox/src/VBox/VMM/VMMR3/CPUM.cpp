e52e1f211cef77af41c43ef20d3ca766ec592256
qzuser <395143360@qq.com>
2022-06-04 08:11:02 +0800

    commit_42191 ------------------------------------------------------------------------ r42191 | vboxsync | 2012-07-17 22:08:35 +0800 (Tue, 17 Jul 2012) | 2 lines
    CPUM: CPUMCPUVENDOR_VIA
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR3/CPUM.cpp
+++ vbox/src/VBox/VMM/VMMR3/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 42032 2012-07-05 23:24:44Z vboxsync $ */
+/* $Id: CPUM.cpp 42191 2012-07-17 14:08:35Z vboxsync $ */
 /** @file
  * CPUM - CPU Monitor / Manager.
  */
@@ -539,6 +539,12 @@ static CPUMCPUVENDOR cpumR3DetectVendor(uint32_t uEAX, uint32_t uEBX, uint32_t u
         &&  uECX == X86_CPUID_VENDOR_INTEL_ECX
         &&  uEDX == X86_CPUID_VENDOR_INTEL_EDX)
         return CPUMCPUVENDOR_INTEL;
+
+    if (    uEAX >= 1
+        &&  uEBX == X86_CPUID_VENDOR_VIA_EBX
+        &&  uECX == X86_CPUID_VENDOR_VIA_ECX
+        &&  uEDX == X86_CPUID_VENDOR_VIA_EDX)
+        return CPUMCPUVENDOR_VIA;
 
     /** @todo detect the other buggers... */
     return CPUMCPUVENDOR_UNKNOWN;
