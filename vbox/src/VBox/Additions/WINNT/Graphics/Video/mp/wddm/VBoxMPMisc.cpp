4f576e85566eb133f18b9e80e48da72e726687b6
qzuser <395143360@qq.com>
2022-06-04 12:05:44 +0800

    commit_43298 ------------------------------------------------------------------------ r43298 | vboxsync | 2012-09-11 21:02:58 +0800 (Tue, 11 Sep 2012) | 1 line
    wddm: assertions cleanup
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPMisc.cpp
+++ vbox/src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPMisc.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxMPMisc.cpp 42170 2012-07-16 20:17:25Z vboxsync $ */
+/* $Id: VBoxMPMisc.cpp 43298 2012-09-11 13:02:58Z vboxsync $ */
 
 /** @file
  * VBox WDDM Miniport driver
@@ -2596,16 +2596,9 @@ static VOID vboxWddmSlVSyncDpc(
                 pDevExt->u.primary.DxgkInterface.DxgkCbNotifyInterrupt(pDevExt->u.primary.DxgkInterface.DeviceHandle, &notify);
                 bNeedDpc = TRUE;
             }
-#ifdef DEBUG_misha
-            else
-                Assert(0);
-#endif
+
             vboxWddmAllocationRelease(pPrimary);
         }
-#ifdef DEBUG_misha
-        else
-            Assert(0);
-#endif
     }
 
     if (bNeedDpc)
