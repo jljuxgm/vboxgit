5a436f6c2a0b576a1ade576943f9305c7c7b2492
qzuser <395143360@qq.com>
2022-06-04 09:51:20 +0800

    commit_42708 ------------------------------------------------------------------------ r42708 | vboxsync | 2012-08-09 18:15:38 +0800 (Thu, 09 Aug 2012) | 1 line
    Runtime: add an fMode parameter to RTDirCreateTemp() and adjust callers.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
+++ vbox/src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceToolBox.cpp 42655 2012-08-07 11:39:09Z vboxsync $ */
+/* $Id: VBoxServiceToolBox.cpp 42708 2012-08-09 10:15:38Z vboxsync $ */
 /** @file
  * VBoxServiceToolbox - Internal (BusyBox-like) toolbox.
  */
@@ -1235,7 +1235,7 @@ static RTEXITCODE VBoxServiceToolboxMkTemp(int argc, char **argv)
 
     if (RT_SUCCESS(rc))
     {
-        rc = RTDirCreateTemp(pszName);
+        rc = RTDirCreateTemp(pszName, 0700);
         toolboxMkTempReport("Created temporary directory '%s'.\n",
                             pszName, RT_SUCCESS(rc), rc, fOutputFlags, NULL);
         toolboxMkTempReport("The following error occurred while creating a temporary directory with template '%s': %Rrc.\n",
