c73dc0eb0eaf8610394791a009c4e9715e76b1b6
qzuser <395143360@qq.com>
2022-06-05 07:38:58 +0800

    commit_47776 ------------------------------------------------------------------------ r47776 | vboxsync | 2013-08-15 23:51:51 +0800 (Thu, 15 Aug 2013) | 2 lines
    Warnings and space.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-server/MachineImpl.cpp
+++ vbox/src/VBox/Main/src-server/MachineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.cpp 47561 2013-08-06 15:18:17Z vboxsync $ */
+/* $Id: MachineImpl.cpp 47776 2013-08-15 15:51:51Z vboxsync $ */
 /** @file
  * Implementation of IMachine in VBoxSVC.
  */
@@ -13020,7 +13020,7 @@ void SessionMachine::uninit(Uninit::Reason aReason)
     RTThreadSleep(500);
     mParent->AddRef();
     LONG c = mParent->Release();
-    LogFlowThisFunc(("vbox ref=%d\n", c));
+    LogFlowThisFunc(("vbox ref=%d\n", c)); NOREF(c);
     unconst(mParent) = NULL;
     unconst(mPeer) = NULL;
 
