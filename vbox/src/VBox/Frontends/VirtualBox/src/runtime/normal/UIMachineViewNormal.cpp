413bf5742af13c1e6d06976e6410cb74c2e9b93d
qzuser <395143360@qq.com>
2022-06-04 08:35:32 +0800

    commit_42318 ------------------------------------------------------------------------ r42318 | vboxsync | 2012-07-23 17:27:05 +0800 (Mon, 23 Jul 2012) | 1 line
    FE/Qt: attempt to fix auto-resize in the face of host resolution changes.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineViewNormal.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineViewNormal.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMachineViewNormal.cpp 41689 2012-06-13 17:13:36Z vboxsync $ */
+/* $Id: UIMachineViewNormal.cpp 42318 2012-07-23 09:27:05Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -62,6 +62,9 @@ UIMachineViewNormal::UIMachineViewNormal(  UIMachineWindow *pMachineWindow
 
     /* Prepare event-filters: */
     prepareFilters();
+
+    /* Prepare connections: */
+    prepareConnections();
 
     /* Prepare console connections: */
     prepareConsoleConnections();
