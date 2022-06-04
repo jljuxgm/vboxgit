9e4abb69e772c5ca2bbc3f7da865444b02d637f5
qzuser <395143360@qq.com>
2022-06-05 04:38:57 +0800

    commit_46858 ------------------------------------------------------------------------ r46858 | vboxsync | 2013-06-28 16:28:21 +0800 (Fri, 28 Jun 2013) | 1 line
    VBoxGuest/win: fix win 8.1 detection
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuest-win.h
+++ vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuest-win.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuest-win.h 44991 2013-03-11 14:55:10Z vboxsync $ */
+/* $Id: VBoxGuest-win.h 46858 2013-06-28 08:28:21Z vboxsync $ */
 /** @file
  * VBoxGuest - Windows specifics.
  */
@@ -134,7 +134,8 @@ typedef enum VBGDNTVER
     VBGDNTVER_WIN2K3,
     VBGDNTVER_WINVISTA,
     VBGDNTVER_WIN7,
-    VBGDNTVER_WIN8
+    VBGDNTVER_WIN8,
+    VBGDNTVER_WIN8_1,
 } VBGDNTVER;
 extern VBGDNTVER g_enmVbgdNtVer;
 
