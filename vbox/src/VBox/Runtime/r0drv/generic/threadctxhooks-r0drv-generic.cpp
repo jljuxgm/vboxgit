8a23b9a22d2c5e5b568387d85f0dd9c3ebc171cd
qzuser <395143360@qq.com>
2022-06-05 05:48:36 +0800

    commit_47201 ------------------------------------------------------------------------ r47201 | vboxsync | 2013-07-17 00:03:16 +0800 (Wed, 17 Jul 2013) | 2 lines
    Runtime/r0drv: build fix.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r0drv/generic/threadctxhooks-r0drv-generic.cpp
+++ vbox/src/VBox/Runtime/r0drv/generic/threadctxhooks-r0drv-generic.cpp
@@ -1,4 +1,4 @@
-/* $Id: threadctxhooks-r0drv-generic.cpp 47200 2013-07-16 16:00:43Z vboxsync $ */
+/* $Id: threadctxhooks-r0drv-generic.cpp 47201 2013-07-16 16:03:16Z vboxsync $ */
 /** @file
  * IPRT - Thread-Context Hooks, Ring-0 Driver, Generic.
  */
@@ -31,6 +31,7 @@
 #include <iprt/thread.h>
 #include <iprt/err.h>
 
+#include "internal/iprt.h"
 
 RTDECL(int) RTThreadCtxHooksCreate(PRTTHREADCTX phThreadCtx)
 {
