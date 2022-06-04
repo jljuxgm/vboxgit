62241326f4713cd9db350b419038ff0b1a440f02
qzuser <395143360@qq.com>
2022-06-04 16:57:51 +0800

    commit_44338 ------------------------------------------------------------------------ r44338 | vboxsync | 2013-01-23 22:45:34 +0800 (Wed, 23 Jan 2013) | 2 lines
    Frontends/VBoxBalloonCtrl: forgotten help text
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxBalloonCtrl/VBoxModBallooning.cpp
+++ vbox/src/VBox/Frontends/VBoxBalloonCtrl/VBoxModBallooning.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxModBallooning.cpp 44336 2013-01-23 13:51:33Z vboxsync $ */
+/* $Id: VBoxModBallooning.cpp 44338 2013-01-23 14:45:34Z vboxsync $ */
 /** @file
  * VBoxModBallooning - Module for handling the automatic ballooning of VMs.
  */
@@ -574,6 +574,7 @@ VBOXMODULE g_ModBallooning =
     /* (Legacy) note. */
     "Set \"VBoxInternal/Guest/BalloonSizeMax\" for a per-VM maximum ballooning size.\n"
 #endif
+    "--balloon-safety-margin Free memory when deflating a balloon in MB (1024 MB).\n"
     ,
     /* methods. */
     VBoxModBallooningPreInit,
