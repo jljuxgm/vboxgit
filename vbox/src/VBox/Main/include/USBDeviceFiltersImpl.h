b53206c0ebbea6fd4ac6028e72a82d016ea8465e
qzuser <395143360@qq.com>
2022-06-05 06:27:48 +0800

    commit_47401 ------------------------------------------------------------------------ r47401 | vboxsync | 2013-07-26 03:12:24 +0800 (Fri, 26 Jul 2013) | 1 line
    Main,Frontends: Second step of USB controller rework. There is one controller instance for every USB controller now. Adapt frontends and testsuite to work with the changed API
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/USBDeviceFiltersImpl.h
+++ vbox/src/VBox/Main/include/USBDeviceFiltersImpl.h
@@ -1,4 +1,4 @@
-/* $Id: USBDeviceFiltersImpl.h 47377 2013-07-24 15:22:34Z vboxsync $ */
+/* $Id: USBDeviceFiltersImpl.h 47401 2013-07-25 19:12:24Z vboxsync $ */
 
 /** @file
  *
@@ -27,7 +27,7 @@ class USBDeviceFilter;
 
 namespace settings
 {
-    struct USBController;
+    struct USB;
 }
 
 class ATL_NO_VTABLE USBDeviceFilters :
@@ -66,8 +66,8 @@ public:
 
     // public methods only for internal purposes
 
-    HRESULT loadSettings(const settings::USBController &data);
-    HRESULT saveSettings(settings::USBController &data);
+    HRESULT loadSettings(const settings::USB &data);
+    HRESULT saveSettings(settings::USB &data);
 
     void rollback();
     void commit();
