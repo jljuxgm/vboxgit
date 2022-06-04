16933554676466a86d895bb86b604dfef0a410a9
qzuser <395143360@qq.com>
2022-06-04 21:53:04 +0800

    commit_45404 ------------------------------------------------------------------------ r45404 | vboxsync | 2013-04-08 21:35:44 +0800 (Mon, 08 Apr 2013) | 2 lines
    Build fix.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/path/RTPathSplitReassemble.cpp
+++ vbox/src/VBox/Runtime/common/path/RTPathSplitReassemble.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTPathSplitReassemble.cpp 45400 2013-04-08 12:08:00Z vboxsync $ */
+/* $Id: RTPathSplitReassemble.cpp 45404 2013-04-08 13:35:44Z vboxsync $ */
 /** @file
  * IPRT - RTPathSplitReassemble.
  */
@@ -75,7 +75,7 @@ RTDECL(int) RTPathSplitReassemble(PRTPATHSPLIT pSplit, uint32_t fFlags, char *ps
      * Do the joining.
      */
     uint32_t const  cchOrgPath = pSplit->cchPath;
-    uint32_t        cchDstPath = 0;
+    size_t          cchDstPath = 0;
     uint32_t const  cComps     = pSplit->cComps;
     uint32_t        idxComp    = 0;
     char           *pszDst     = pszDstPath;
