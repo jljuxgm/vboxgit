5c497eb9996bc7ad02e063c2ef8237ae05631916
qzuser <395143360@qq.com>
2022-06-04 20:46:40 +0800

    commit_45268 ------------------------------------------------------------------------ r45268 | vboxsync | 2013-04-01 18:12:20 +0800 (Mon, 01 Apr 2013) | 1 line
    FE/Qt: Message-center cleanup (part 8).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/main.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/main.cpp
@@ -1,4 +1,4 @@
-/* $Id: main.cpp 45267 2013-04-01 10:06:23Z vboxsync $ */
+/* $Id: main.cpp 45268 2013-04-01 10:12:20Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -496,7 +496,7 @@ extern "C" DECLEXPORT(int) TrustedMain (int argc, char **argv, char ** /*envp*/)
             }
             else if (noSelector)
             {
-                msgCenter().cannotRunInSelectorMode();
+                msgCenter().cannotStartSelector();
             }
             else
             {
