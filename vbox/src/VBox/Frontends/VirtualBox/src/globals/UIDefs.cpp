83a20698a21738ff6f8c71d051361abd0f906ad1
qzuser <395143360@qq.com>
2022-06-05 04:03:08 +0800

    commit_46686 ------------------------------------------------------------------------ r46686 | vboxsync | 2013-06-20 01:58:20 +0800 (Thu, 20 Jun 2013) | 1 line
    FE/Qt: Runtime UI: Possibility to hide (restrict) status-bar indicators one-by-one (using extra-data).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/globals/UIDefs.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/globals/UIDefs.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIDefs.cpp 46647 2013-06-19 11:01:10Z vboxsync $ */
+/* $Id: UIDefs.cpp 46686 2013-06-19 17:58:20Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -64,6 +64,7 @@ const char* UIDefs::GUI_AutomountGuestScreens = "GUI/AutomountGuestScreens";
 const char* UIDefs::GUI_SaveMountedAtRuntime = "GUI/SaveMountedAtRuntime";
 const char* UIDefs::GUI_PassCAD = "GUI/PassCAD";
 const char* UIDefs::GUI_DefaultCloseAction = "GUI/DefaultCloseAction";
+const char* UIDefs::GUI_RestrictedStatusBarIndicators = "GUI/RestrictedStatusBarIndicators";
 
 /* Mini tool-bar definitions: */
 const char* UIDefs::GUI_ShowMiniToolBar = "GUI/ShowMiniToolBar";
