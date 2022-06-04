d2df18e8031724150075ed5541b4e25c06ae7308
qzuser <395143360@qq.com>
2022-06-05 02:27:55 +0800

    commit_46208 ------------------------------------------------------------------------ r46208 | vboxsync | 2013-05-22 17:26:27 +0800 (Wed, 22 May 2013) | 1 line
    tstRTStrCache: check that a real strcache impl doesn't return duplicates.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/generic/strcache-stubs-generic.cpp
+++ vbox/src/VBox/Runtime/generic/strcache-stubs-generic.cpp
@@ -1,4 +1,4 @@
-/* $Id: strcache-stubs-generic.cpp 46107 2013-05-15 19:11:31Z vboxsync $ */
+/* $Id: strcache-stubs-generic.cpp 46208 2013-05-22 09:26:27Z vboxsync $ */
 /** @file
  * IPRT - String Cache, stub implementation.
  */
@@ -122,4 +122,11 @@ RTDECL(size_t) RTStrCacheLength(const char *psz)
     return strlen(psz);
 }
 RT_EXPORT_SYMBOL(RTStrCacheLength);
+
+
+RTDECL(bool) RTStrCacheIsRealImpl(void)
+{
+    return false;
+}
+RT_EXPORT_SYMBOL(RTStrCacheIsRealImpl);
 
