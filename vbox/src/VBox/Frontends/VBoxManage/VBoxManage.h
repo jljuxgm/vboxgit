43a944303a7b56a8d151199e55d8c378cd0434e8
qzuser <395143360@qq.com>
2022-06-04 08:22:18 +0800

    commit_42248 ------------------------------------------------------------------------ r42248 | vboxsync | 2012-07-20 16:39:45 +0800 (Fri, 20 Jul 2012) | 2 lines
    API change for SetVideoModeHint to be able to disable guest screens and to set the origin of guest screens
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxManage/VBoxManage.h
+++ vbox/src/VBox/Frontends/VBoxManage/VBoxManage.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxManage.h 42176 2012-07-17 12:23:50Z vboxsync $ */
+/* $Id: VBoxManage.h 42248 2012-07-20 08:39:45Z vboxsync $ */
 /** @file
  * VBoxManage - VirtualBox command-line interface, internal header file.
  */
@@ -242,6 +242,7 @@ RTEXITCODE handleConvertFromRaw(int argc, char *argv[]);
 int handleShowHardDiskInfo(HandlerArg *a);
 int handleCloseMedium(HandlerArg *a);
 int parseDiskType(const char *psz, MediumType_T *pDiskType);
+int parseBool(const char *psz, bool *pb);
 
 /* VBoxManageStorageController.cpp */
 int handleStorageAttach(HandlerArg *a);
