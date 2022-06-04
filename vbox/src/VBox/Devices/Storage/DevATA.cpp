61680e41fdfcd6f5291aee8d6198535998f736b0
qzuser <395143360@qq.com>
2022-06-05 07:40:40 +0800

    commit_47785 ------------------------------------------------------------------------ r47785 | vboxsync | 2013-08-16 16:50:39 +0800 (Fri, 16 Aug 2013) | 1 line
    DevATA: Unset BSY before each device <-> host PIO transfer as required by protocol.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Storage/DevATA.cpp
+++ vbox/src/VBox/Devices/Storage/DevATA.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevATA.cpp 47784 2013-08-16 08:46:08Z vboxsync $ */
+/* $Id: DevATA.cpp 47785 2013-08-16 08:50:39Z vboxsync $ */
 /** @file
  * VBox storage devices: ATA/ATAPI controller device (disk and cdrom).
  */
@@ -986,6 +986,7 @@ static void ataPIOTransferStart(ATADevState *s, uint32_t start, uint32_t size)
     s->iIOBufferPIODataStart = start;
     s->iIOBufferPIODataEnd = start + size;
     ataSetStatus(s, ATA_STAT_DRQ | ATA_STAT_SEEK);
+    ataUnsetStatus(s, ATA_STAT_BUSY);
 }
 
 
