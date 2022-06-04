1cd331fc250fa56e4530393c3eec49ebeac085a2
qzuser <395143360@qq.com>
2022-06-04 23:50:53 +0800

    commit_45663 ------------------------------------------------------------------------ r45663 | vboxsync | 2013-04-22 19:28:05 +0800 (Mon, 22 Apr 2013) | 1 line
    FE/Qt: Popup-pane: Remove tool-button borders.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/widgets/UIPopupPane.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/widgets/UIPopupPane.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIPopupPane.cpp 45662 2013-04-22 11:21:52Z vboxsync $ */
+/* $Id: UIPopupPane.cpp 45663 2013-04-22 11:28:05Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -727,6 +727,7 @@ QIToolButton* UIPopupPaneButtonPane::addButton(int iButtonID, const QString &str
 {
     /* Create button: */
     QIToolButton *pButton = new QIToolButton;
+    pButton->removeBorder();
     pButton->setToolTip(strToolTip.isEmpty() ? defaultToolTip(iButtonID) : strToolTip);
     pButton->setIcon(defaultIcon(iButtonID));
 
