c23f9dfcb1f1016928d7b33ebb4b6e8b7f752e1e
qzuser <395143360@qq.com>
2022-06-05 10:54:24 +0800

    commit_48646 ------------------------------------------------------------------------ r48646 | vboxsync | 2013-09-24 16:03:21 +0800 (Tue, 24 Sep 2013) | 2 lines
    VMM/CPUM: Clarify by explicit comment that the msrApicBase shouldn't be added to CPUMCTX.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR3/CPUM.cpp
+++ vbox/src/VBox/VMM/VMMR3/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 48368 2013-09-06 17:28:13Z vboxsync $ */
+/* $Id: CPUM.cpp 48646 2013-09-24 08:03:21Z vboxsync $ */
 /** @file
  * CPUM - CPU Monitor / Manager.
  */
@@ -249,6 +249,7 @@ static const SSMFIELD g_aCpumCtxFields[] =
     SSMFIELD_ENTRY(         CPUMCTX, msrCSTAR),
     SSMFIELD_ENTRY(         CPUMCTX, msrSFMASK),
     SSMFIELD_ENTRY(         CPUMCTX, msrKERNELGSBASE),
+    /* msrApicBase is not included here, it resides in the APIC device state. */
     SSMFIELD_ENTRY(         CPUMCTX, ldtr.Sel),
     SSMFIELD_ENTRY(         CPUMCTX, ldtr.ValidSel),
     SSMFIELD_ENTRY(         CPUMCTX, ldtr.fFlags),
