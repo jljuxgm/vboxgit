905f444d9f2c117d06e1238d4ad9451a1da81146
qzuser <395143360@qq.com>
2022-06-05 15:06:31 +0800

    commit_49538 ------------------------------------------------------------------------ r49538 | vboxsync | 2013-11-19 00:21:40 +0800 (Tue, 19 Nov 2013) | 2 lines
    VMM/CPUM: typo
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR3/CPUM.cpp
+++ vbox/src/VBox/VMM/VMMR3/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 49182 2013-10-18 14:14:27Z vboxsync $ */
+/* $Id: CPUM.cpp 49538 2013-11-18 16:21:40Z vboxsync $ */
 /** @file
  * CPUM - CPU Monitor / Manager.
  */
@@ -3939,7 +3939,7 @@ static DECLCALLBACK(void) cpumR3CpuIdInfo(PVM pVM, PCDBGFINFOHLP pHlp, const cha
     if (iVerbosity && cCentaurMax >= 1)
     {
         ASMCpuIdExSlow(0xc0000001, 0, 0, 0, &Host.eax, &Host.ebx, &Host.ecx, &Host.edx);
-        uint32_t uEdxGst = pVM->cpum.s.aGuestCpuIdExt[1].edx;
+        uint32_t uEdxGst = pVM->cpum.s.aGuestCpuIdCentaur[1].edx;
         uint32_t uEdxHst = Host.edx;
 
         if (iVerbosity == 1)
