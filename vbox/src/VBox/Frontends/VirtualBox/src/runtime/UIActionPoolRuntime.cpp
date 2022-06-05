84a1d29f96c9e7dfed51e1b779a07e526c98cf01
qzuser <395143360@qq.com>
2022-06-06 05:20:53 +0800

    commit_52678 ------------------------------------------------------------------------ r52678 | vboxsync | 2014-09-10 23:10:43 +0800 (Wed, 10 Sep 2014) | 1 line
    FE/Qt: 7462: Runtime UI: Do not show Debug menu for release build unless it's allowed.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIActionPoolRuntime.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIActionPoolRuntime.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIActionPoolRuntime.cpp 52657 2014-09-09 15:05:09Z vboxsync $ */
+/* $Id: UIActionPoolRuntime.cpp 52678 2014-09-10 15:10:43Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIActionPoolRuntime class implementation.
  */
@@ -2147,7 +2147,7 @@ void UIActionPoolRuntime::updateMenus()
 
 #ifdef VBOX_WITH_DEBUGGER_GUI
     /* 'Debug' menu: */
-    addMenu(m_mainMenus, action(UIActionIndexRT_M_Debug));
+    addMenu(m_mainMenus, action(UIActionIndexRT_M_Debug), vboxGlobal().isDebuggerEnabled());
     updateMenuDebug();
 #endif /* VBOX_WITH_DEBUGGER_GUI */
 
