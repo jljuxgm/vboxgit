09d74e59f2ec8d5b98f869b5ac89b30b13e4e03a
qzuser <395143360@qq.com>
2022-06-05 14:38:07 +0800

    commit_49451 ------------------------------------------------------------------------ r49451 | vboxsync | 2013-11-12 20:38:05 +0800 (Tue, 12 Nov 2013) | 1 line
    DrvKeyboardQueue: activate on resume (from saved state).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Input/DrvKeyboardQueue.cpp
+++ vbox/src/VBox/Devices/Input/DrvKeyboardQueue.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvKeyboardQueue.cpp 49423 2013-11-08 16:25:35Z vboxsync $ */
+/* $Id: DrvKeyboardQueue.cpp 49451 2013-11-12 12:38:05Z vboxsync $ */
 /** @file
  * VBox input devices: Keyboard queue driver
  */
@@ -224,6 +224,7 @@ static DECLCALLBACK(void)  drvKbdQueueSuspend(PPDMDRVINS pDrvIns)
 static DECLCALLBACK(void)  drvKbdQueueResume(PPDMDRVINS pDrvIns)
 {
     PDRVKBDQUEUE        pThis = PDMINS_2_DATA(pDrvIns, PDRVKBDQUEUE);
+    pThis->fInactive = false;
     pThis->fSuspended = false;
 }
 
