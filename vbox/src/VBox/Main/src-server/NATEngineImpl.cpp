f63f69affb8425e094cdb6d95eaaf3d26b59a7db
qzuser <395143360@qq.com>
2022-06-05 16:52:31 +0800

    commit_49875 ------------------------------------------------------------------------ r49875 | vboxsync | 2013-12-11 20:32:42 +0800 (Wed, 11 Dec 2013) | 1 line
    Main: gcc warnings.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-server/NATEngineImpl.cpp
+++ vbox/src/VBox/Main/src-server/NATEngineImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: NATEngineImpl.cpp 49874 2013-12-11 12:28:07Z vboxsync $ */
+/* $Id: NATEngineImpl.cpp 49875 2013-12-11 12:32:42Z vboxsync $ */
 /** @file
  * Implementation of INATEngine in VBoxSVC.
  */
@@ -69,7 +69,7 @@ struct NATEngine::Data
 // constructor / destructor
 ////////////////////////////////////////////////////////////////////////////////
 
-NATEngine::NATEngine():mParent(NULL), mAdapter(NULL), mData(NULL) {}
+NATEngine::NATEngine():mData(NULL), mParent(NULL), mAdapter(NULL) {}
 NATEngine::~NATEngine(){}
 
 HRESULT NATEngine::FinalConstruct()
