ca7e86ad200c6b4c3317ec174f03df9c564f05e1
qzuser <395143360@qq.com>
2022-06-05 19:05:51 +0800

    commit_50407 ------------------------------------------------------------------------ r50407 | vboxsync | 2014-02-11 09:58:32 +0800 (Tue, 11 Feb 2014) | 1 line
    duh
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR3/DBGFAddrSpace.cpp
+++ vbox/src/VBox/VMM/VMMR3/DBGFAddrSpace.cpp
@@ -1,4 +1,4 @@
-/* $Id: DBGFAddrSpace.cpp 50406 2014-02-10 20:35:11Z vboxsync $ */
+/* $Id: DBGFAddrSpace.cpp 50407 2014-02-11 01:58:32Z vboxsync $ */
 /** @file
  * DBGF - Debugger Facility, Address Space Management.
  */
@@ -177,7 +177,7 @@ int dbgfR3AsInit(PUVM pUVM)
     for (unsigned i = 0; i < RT_ELEMENTS(s_aProps); i++)
     {
         char szEnvValue[8192];
-        int rcEnv = RTEnvGetEx(RTENV_DEFAULT, s_aProps[i].pszEnvName, szEnvValue, sizeof(szEnvValue), NULL);
+        rc = RTEnvGetEx(RTENV_DEFAULT, s_aProps[i].pszEnvName, szEnvValue, sizeof(szEnvValue), NULL);
         if (RT_SUCCESS(rc))
         {
             rc = RTDbgCfgChangeString(pUVM->dbgf.s.hDbgCfg, s_aProps[i].enmProp, RTDBGCFGOP_PREPEND, szEnvValue);
