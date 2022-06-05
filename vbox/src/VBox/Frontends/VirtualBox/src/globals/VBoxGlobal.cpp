1eae2d1774ae613c5b147040aa3ab7dd63d708e2
qzuser <395143360@qq.com>
2022-06-06 03:43:40 +0800

    commit_52221 ------------------------------------------------------------------------ r52221 | vboxsync | 2014-07-29 12:12:21 +0800 (Tue, 29 Jul 2014) | 2 lines
    FE/Qt: Show both configured and effective paravirt. provider in Session info. dialog.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGlobal.cpp 52203 2014-07-25 23:46:23Z vboxsync $ */
+/* $Id: VBoxGlobal.cpp 52221 2014-07-29 04:12:21Z vboxsync $ */
 /** @file
  * VBox Qt GUI - VBoxGlobal class implementation.
  */
@@ -1210,7 +1210,7 @@ QString VBoxGlobal::detailsReport (const CMachine &aMachine, bool aWithLinks)
             iRowCount += 2; /* VT-x/AMD-V items. */
 
         /* Paravirtualization Interface: */
-        const QString strParavirtProvider = gpConverter->toString(aMachine.GetEffectiveParavirtProvider());
+        const QString strParavirtProvider = gpConverter->toString(aMachine.GetParavirtProvider());
 
         iRowCount += 1; /* Paravirtualization Interface. */
 
