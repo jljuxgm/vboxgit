74c97dd0d52f8e7e456ddfdb83a3f37378b17466
qzuser <395143360@qq.com>
2022-06-05 18:42:58 +0800

    commit_50309 ------------------------------------------------------------------------ r50309 | vboxsync | 2014-02-03 22:41:28 +0800 (Mon, 03 Feb 2014) | 1 line
    FE/Qt: 6863: Runtime UI: SATA hot-plug support (step 5): Add hard-disk status-bar indicator's context menu.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
@@ -1,4 +1,4 @@
-/* $Id: UISession.cpp 50302 2014-01-31 13:55:49Z vboxsync $ */
+/* $Id: UISession.cpp 50309 2014-02-03 14:41:28Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UISession class implementation.
  */
@@ -1259,6 +1259,7 @@ void UISession::updateSessionSettings()
 {
     bool fAllowReconfiguration = m_machineState != KMachineState_Stuck && m_fReconfigurable;
     gActionPool->action(UIActionIndexRuntime_Simple_SettingsDialog)->setEnabled(fAllowReconfiguration);
+    gActionPool->action(UIActionIndexRuntime_Simple_StorageSettings)->setEnabled(fAllowReconfiguration);
     gActionPool->action(UIActionIndexRuntime_Simple_SharedFoldersSettings)->setEnabled(fAllowReconfiguration);
     gActionPool->action(UIActionIndexRuntime_Simple_VideoCaptureSettings)->setEnabled(fAllowReconfiguration);
     gActionPool->action(UIActionIndexRuntime_Simple_NetworkSettings)->setEnabled(fAllowReconfiguration);
