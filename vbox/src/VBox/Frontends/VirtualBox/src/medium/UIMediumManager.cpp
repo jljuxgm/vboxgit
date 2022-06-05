7bdab633e842f27c0543d838397c93fa8315640d
qzuser <395143360@qq.com>
2022-06-05 20:23:34 +0800

    commit_50704 ------------------------------------------------------------------------ r50704 | vboxsync | 2014-03-05 21:14:20 +0800 (Wed, 05 Mar 2014) | 1 line
    FE/Qt: Medium Manager rework/cleanup: Few more comments.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMediumManager.cpp 50700 2014-03-05 11:51:38Z vboxsync $ */
+/* $Id: UIMediumManager.cpp 50704 2014-03-05 13:14:20Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt4 GUI ("VirtualBox"):
@@ -266,7 +266,7 @@ UIMediumManager::~UIMediumManager()
 /* static */
 void UIMediumManager::showModeless(QWidget *pCenterWidget /* = 0 */, bool fRefresh /* = true */)
 {
-    /* Prepare instance if not prepared: */
+    /* Create instance if not yet created: */
     if (!m_spInstance)
         m_spInstance = new UIMediumManager(pCenterWidget, fRefresh);
 
