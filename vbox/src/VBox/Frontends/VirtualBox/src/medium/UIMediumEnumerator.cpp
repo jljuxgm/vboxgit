ef0980f60ab8e2a7026b7cfbbf9ebcb9d70d6e50
qzuser <395143360@qq.com>
2022-06-05 09:19:59 +0800

    commit_48274 ------------------------------------------------------------------------ r48274 | vboxsync | 2013-09-05 00:17:19 +0800 (Thu, 05 Sep 2013) | 1 line
    FE/Qt: Build-fix for r88688.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/medium/UIMediumEnumerator.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/medium/UIMediumEnumerator.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMediumEnumerator.cpp 48273 2013-09-04 16:15:32Z vboxsync $ */
+/* $Id: UIMediumEnumerator.cpp 48274 2013-09-04 16:17:19Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -16,6 +16,9 @@
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
+
+/* Qt includes: */
+#include <QSet>
 
 /* GUI includes: */
 #include "UIMediumEnumerator.h"
