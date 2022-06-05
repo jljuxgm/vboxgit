89bf837c430108dd8512a056aa0fdae7808ad77a
qzuser <395143360@qq.com>
2022-06-05 12:14:47 +0800

    commit_48954 ------------------------------------------------------------------------ r48954 | vboxsync | 2013-10-08 05:57:06 +0800 (Tue, 08 Oct 2013) | 1 line
    Installer: Whitespace and svn:keywords cleanups by scm.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Installer/solaris/VBoxISAExec.c
+++ vbox/src/VBox/Installer/solaris/VBoxISAExec.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxISAExec.c 44528 2013-02-04 14:27:54Z vboxsync $ */
+/* $Id: VBoxISAExec.c 48954 2013-10-07 21:57:06Z vboxsync $ */
 /** @file
  * VBoxISAExec, ISA exec wrapper, Solaris hosts.
  */
