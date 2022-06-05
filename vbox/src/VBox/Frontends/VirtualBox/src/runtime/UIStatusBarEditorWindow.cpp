25679a32d30e1b53045f24f1e89ef9d2306ab69d
qzuser <395143360@qq.com>
2022-06-06 03:13:54 +0800

    commit_52071 ------------------------------------------------------------------------ r52071 | vboxsync | 2014-07-17 17:14:14 +0800 (Thu, 17 Jul 2014) | 1 line
    FE/Qt: Mac OS X: Build-fix for r95073.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIStatusBarEditorWindow.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIStatusBarEditorWindow.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIStatusBarEditorWindow.cpp 52070 2014-07-17 09:09:10Z vboxsync $ */
+/* $Id: UIStatusBarEditorWindow.cpp 52071 2014-07-17 09:14:14Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIStatusBarEditorWindow class implementation.
  */
@@ -33,7 +33,9 @@
 #include "UIIconPool.h"
 #include "QIToolButton.h"
 #include "VBoxGlobal.h"
-
+#ifdef Q_WS_MAC
+# include "VBoxUtils-darwin.h"
+#endif /* Q_WS_MAC */
 
 /** QWidget extension
   * used as status-bar editor button. */
