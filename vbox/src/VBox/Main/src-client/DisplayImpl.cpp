4e93b2d4b0f43a39a5a0615d6571079b44d95a44
qzuser <395143360@qq.com>
2022-06-06 01:38:47 +0800

    commit_51605 ------------------------------------------------------------------------ r51605 | vboxsync | 2014-06-11 20:57:05 +0800 (Wed, 11 Jun 2014) | 1 line
    Main,Frontends: removed obsolete IFramebuffer methods: Address, UsesGuestVRAM, Lock, Unlock
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/DisplayImpl.cpp
+++ vbox/src/VBox/Main/src-client/DisplayImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: DisplayImpl.cpp 51604 2014-06-11 12:16:51Z vboxsync $ */
+/* $Id: DisplayImpl.cpp 51605 2014-06-11 12:57:05Z vboxsync $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -1122,12 +1122,8 @@ void Display::handleDisplayUpdate (unsigned uScreenId, int x, int y, int w, int 
     IFramebuffer *pFramebuffer = maFramebuffers[uScreenId].pFramebuffer;
     if (pFramebuffer != NULL)
     {
-        pFramebuffer->Lock();
-
         if (w != 0 && h != 0)
             pFramebuffer->NotifyUpdate(x, y, w, h);
-
-        pFramebuffer->Unlock();
     }
 
 #ifndef VBOX_WITH_HGSMI
