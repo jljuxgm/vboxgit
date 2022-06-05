c9e222deca9f4ffad0402a648b06f04b9b3359ed
qzuser <395143360@qq.com>
2022-06-05 18:25:17 +0800

    commit_50232 ------------------------------------------------------------------------ r50232 | vboxsync | 2014-01-25 05:42:33 +0800 (Sat, 25 Jan 2014) | 1 line
    Another Solaris build fix
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/USB/solaris/USBProxyDevice-solaris.cpp
+++ vbox/src/VBox/Devices/USB/solaris/USBProxyDevice-solaris.cpp
@@ -1,4 +1,4 @@
-/* $Id: USBProxyDevice-solaris.cpp 50230 2014-01-24 21:30:09Z vboxsync $ */
+/* $Id: USBProxyDevice-solaris.cpp 50232 2014-01-24 21:42:33Z vboxsync $ */
 /** @file
  * USB device proxy - the Solaris backend.
  */
@@ -97,7 +97,7 @@ typedef struct USBPROXYDEVSOL
     RTPIPE                         hPipeWakeupR;
 } USBPROXYDEVSOL, *PUSBPROXYDEVSOL;
 
-PVUSBURB usbProxySolarisUrbComplete(PUSBPROXYDEVSOL pDevSol);
+static PVUSBURB usbProxySolarisUrbComplete(PUSBPROXYDEVSOL pDevSol);
 
 
 /**
