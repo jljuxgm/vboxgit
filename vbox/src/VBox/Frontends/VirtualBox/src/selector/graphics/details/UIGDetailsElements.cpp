7b29f6fe5d11c500a566045ea48c2466200076bc
qzuser <395143360@qq.com>
2022-06-05 10:21:19 +0800

    commit_48538 ------------------------------------------------------------------------ r48538 | vboxsync | 2013-09-19 23:17:43 +0800 (Thu, 19 Sep 2013) | 4 lines
    Main/NetworkAdapter+Machine+Appliance+SystemProperties+Medium+Console+Settings+IDL: make NAT networking a separate network attachment type which improves the user experience, store the necessary settings, plus changing the design of the methods which will move images and entire VMs, they lacked a progress object
    Frontends/VirtualBox: adapted fully, can configure NAT networks with proper drop down list support
    Frontends/VBoxManage: also supports NAT networks completely, and adds the long missing code to list intnets
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGDetailsElements.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGDetailsElements.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGDetailsElements.cpp 48314 2013-09-05 15:54:32Z vboxsync $ */
+/* $Id: UIGDetailsElements.cpp 48538 2013-09-19 15:17:43Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -674,6 +674,12 @@ void UIGDetailsUpdateThreadNetwork::run()
                                       strAttachmentType.arg(QApplication::translate("UIGDetails", "Generic Driver, '%1'", "details (network)").arg(adapter.GetGenericDriver())) :
                                       strAttachmentType.arg(QApplication::translate("UIGDetails", "Generic Driver, '%1' {&nbsp;%2&nbsp;}", "details (network)")
                                                             .arg(adapter.GetGenericDriver(), strGenericDriverProperties));
+                            break;
+                        }
+                        case KNetworkAttachmentType_NATNetwork:
+                        {
+                            strAttachmentType = strAttachmentType.arg(QApplication::translate("UIGDetails", "NAT Network, '%1'", "details (network)")
+                                                                      .arg(adapter.GetNATNetwork()));
                             break;
                         }
                         default:
