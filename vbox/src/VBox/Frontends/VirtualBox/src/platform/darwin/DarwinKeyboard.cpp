8eccf104bf59a1a51d0b7817e3cc720390951994
qzuser <395143360@qq.com>
2022-06-05 11:36:25 +0800

    commit_48800 ------------------------------------------------------------------------ r48800 | vboxsync | 2013-10-02 00:57:00 +0800 (Wed, 02 Oct 2013) | 1 line
    OS X host: leds sync: disable.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/platform/darwin/DarwinKeyboard.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/platform/darwin/DarwinKeyboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: DarwinKeyboard.cpp 48799 2013-10-01 16:27:46Z vboxsync $ */
+/* $Id: DarwinKeyboard.cpp 48800 2013-10-01 16:57:00Z vboxsync $ */
 /** @file
  * Common GUI Library - Darwin Keyboard routines.
  *
@@ -21,7 +21,7 @@
 *   Header Files                                                               *
 *******************************************************************************/
 #define LOG_GROUP LOG_GROUP_GUI
-#define VBOX_WITH_KBD_LEDS_SYNC
+//#define VBOX_WITH_KBD_LEDS_SYNC
 
 #include "DarwinKeyboard.h"
 #include <iprt/assert.h>
