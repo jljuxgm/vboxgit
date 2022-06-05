1e1d356486732abf9dceb4020c5df0726422953e
qzuser <395143360@qq.com>
2022-06-05 11:59:46 +0800

    commit_48895 ------------------------------------------------------------------------ r48895 | vboxsync | 2013-10-05 03:04:19 +0800 (Sat, 05 Oct 2013) | 2 lines
    Runtime: warning
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/time/time.cpp
+++ vbox/src/VBox/Runtime/common/time/time.cpp
@@ -1,4 +1,4 @@
-/* $Id: time.cpp 48835 2013-10-03 13:29:58Z vboxsync $ */
+/* $Id: time.cpp 48895 2013-10-04 19:04:19Z vboxsync $ */
 /** @file
  * IPRT - Time.
  */
@@ -858,7 +858,7 @@ RTDECL(PRTTIME) RTTimeFromString(PRTTIME pTime, const char *pszString)
      */
     if (*pszString == 'Z')
     {
-        *pszString++;
+        pszString++;
         pTime->fFlags &= ~RTTIME_FLAGS_TYPE_MASK;
         pTime->fFlags |= ~RTTIME_FLAGS_TYPE_UTC;
         pTime->offUTC = 0;
