5a27beefd5345316cf393997842d7f3abe27f1cf
qzuser <395143360@qq.com>
2022-06-06 05:53:20 +0800

    commit_52836 ------------------------------------------------------------------------ r52836 | vboxsync | 2014-09-24 21:02:36 +0800 (Wed, 24 Sep 2014) | 1 line
    scmDiffCompare: Use slow diff when ignoring trailing or leading spaces, not trailing or trailing.
    ------------------------------------------------------------------------

--- vbox/src/bldprogs/scmdiff.cpp
+++ vbox/src/bldprogs/scmdiff.cpp
@@ -1,4 +1,4 @@
-/* $Id: scmdiff.cpp 40530 2012-03-19 11:13:17Z vboxsync $ */
+/* $Id: scmdiff.cpp 52836 2014-09-24 13:02:36Z vboxsync $ */
 /** @file
  * IPRT Testcase / Tool - Source Code Massager.
  */
@@ -236,7 +236,7 @@ DECLINLINE(bool) scmDiffCompare(PSCMDIFFSTATE pState,
         || memcmp(pchLeft, pchRight, cchLeft))
     {
         if (   pState->fIgnoreTrailingWhite
-            || pState->fIgnoreTrailingWhite)
+            || pState->fIgnoreLeadingWhite)
             return scmDiffCompareSlow(pState,
                                       pchLeft, cchLeft, enmEolLeft,
                                       pchRight, cchRight, enmEolRight);
