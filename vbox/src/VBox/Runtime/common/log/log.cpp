65cbb78ee234c16e4bfaf6d45b129062c13c5671
qzuser <395143360@qq.com>
2022-06-05 21:27:06 +0800

    commit_50915 ------------------------------------------------------------------------ r50915 | vboxsync | 2014-03-28 03:11:46 +0800 (Fri, 28 Mar 2014) | 1 line
    log.cpp had assertion temp commented out - fixed
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/log/log.cpp
+++ vbox/src/VBox/Runtime/common/log/log.cpp
@@ -1,4 +1,4 @@
-/* $Id: log.cpp 50914 2014-03-27 19:07:53Z vboxsync $ */
+/* $Id: log.cpp 50915 2014-03-27 19:11:46Z vboxsync $ */
 /** @file
  * Runtime VBox - Logger.
  */
@@ -1500,7 +1500,7 @@ static unsigned rtlogGroupFlags(const char *psz)
                 }
             } /* strincmp */
         } /* for each flags */
-        // AssertMsg(fFound, ("%.15s...", psz));
+        AssertMsg(fFound, ("%.15s...", psz));
     }
 
     /*
