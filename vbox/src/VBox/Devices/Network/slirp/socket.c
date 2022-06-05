be313917171b538150c95be4429458b2026a78b9
qzuser <395143360@qq.com>
2022-06-06 07:29:41 +0800

    commit_53310 ------------------------------------------------------------------------ r53310 | vboxsync | 2014-11-12 12:16:52 +0800 (Wed, 12 Nov 2014) | 5 lines
    NAT: We don't support Windows 2000, so use Windows ICMP API directly.
    We have been using runtime lookup for some functions and direct calls
    for some others which is inconsistent and proves "by construction"
    that the indirection is not necessary.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Network/slirp/socket.c
+++ vbox/src/VBox/Devices/Network/slirp/socket.c
@@ -1,4 +1,4 @@
-/* $Id: socket.c 53056 2014-10-14 15:21:09Z vboxsync $ */
+/* $Id: socket.c 53310 2014-11-12 04:16:52Z vboxsync $ */
 /** @file
  * NAT - socket handling.
  */
@@ -1460,7 +1460,7 @@ sorecvfrom_icmp_win(PNATState pData, struct socket *so)
     int out_len;
     int size;
 
-    len = pData->pfIcmpParseReplies(pData->pvIcmpBuffer, pData->cbIcmpBuffer);
+    len = IcmpParseReplies(pData->pvIcmpBuffer, pData->cbIcmpBuffer);
     if (len < 0)
     {
         LogRel(("NAT: Error (%d) occurred on ICMP receiving\n", GetLastError()));
