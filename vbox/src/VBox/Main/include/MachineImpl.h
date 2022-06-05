e20444e70ee44ff39990ba3b1bc2eeaa56a99afb
qzuser <395143360@qq.com>
2022-06-06 03:33:43 +0800

    commit_52171 ------------------------------------------------------------------------ r52171 | vboxsync | 2014-07-24 23:14:18 +0800 (Thu, 24 Jul 2014) | 1 line
    Main: VM startup logging on windows.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/MachineImpl.h
+++ vbox/src/VBox/Main/include/MachineImpl.h
@@ -1,4 +1,4 @@
-/* $Id: MachineImpl.h 52090 2014-07-18 06:37:32Z vboxsync $ */
+/* $Id: MachineImpl.h 52171 2014-07-24 15:14:18Z vboxsync $ */
 /** @file
  * Implementation of IMachine in VBoxSVC - Header.
  */
@@ -532,6 +532,7 @@ public:
 
     void i_getLogFolder(Utf8Str &aLogFolder);
     Utf8Str i_queryLogFilename(ULONG idx);
+    Utf8Str i_getStartupLogFilename(void);
 
     void i_composeSavedStateFilename(Utf8Str &strStateFilePath);
 
