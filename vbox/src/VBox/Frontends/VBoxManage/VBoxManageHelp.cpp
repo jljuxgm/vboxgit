2844bbef9af0e89edba44d23b165db39efd7ae82
qzuser <395143360@qq.com>
2022-06-05 13:21:09 +0800

    commit_49190 ------------------------------------------------------------------------ r49190 | vboxsync | 2013-10-18 23:26:52 +0800 (Fri, 18 Oct 2013) | 1 line
    Main,VBoxManage: Implement hotpluggable setting stubs
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ vbox/src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageHelp.cpp 49103 2013-10-15 06:20:39Z vboxsync $ */
+/* $Id: VBoxManageHelp.cpp 49190 2013-10-18 15:26:52Z vboxsync $ */
 /** @file
  * VBoxManage - help and other message output.
  */
@@ -534,6 +534,7 @@ void printUsage(USAGECATEGORY u64Cmd, PRTSTREAM pStrm)
                      "                            [--tempeject on|off]\n"
                      "                            [--nonrotational on|off]\n"
                      "                            [--discard on|off]\n"
+                     "                            [--hotpluggable on|off]\n"
                      "                            [--bandwidthgroup <name>]\n"
                      "                            [--forceunmount]\n"
                      "                            [--server <name>|<ip>]\n"
