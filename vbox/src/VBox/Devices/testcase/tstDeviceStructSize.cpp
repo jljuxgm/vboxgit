694e8fe543a5b051c241f006e6ab394f988803f3
qzuser <395143360@qq.com>
2022-06-05 09:06:54 +0800

    commit_48214 ------------------------------------------------------------------------ r48214 | vboxsync | 2013-09-01 00:16:58 +0800 (Sun, 01 Sep 2013) | 1 line
    Added new PS/2 mouse implementation (currently disabled).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/testcase/tstDeviceStructSize.cpp
+++ vbox/src/VBox/Devices/testcase/tstDeviceStructSize.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstDeviceStructSize.cpp 47466 2013-07-29 15:14:11Z vboxsync $ */
+/* $Id: tstDeviceStructSize.cpp 48214 2013-08-31 16:16:58Z vboxsync $ */
 /** @file
  * tstDeviceStructSize - testcase for check structure sizes/alignment
  *                       and to verify that HC and RC uses the same
@@ -36,6 +36,8 @@
 #include "../Input/DevPS2.cpp"
 #undef LOG_GROUP
 #include "../Input/PS2K.cpp"
+#undef LOG_GROUP
+#include "../Input/PS2M.cpp"
 #ifdef VBOX_WITH_E1000
 # undef LOG_GROUP
 # include "../Network/DevE1000.cpp"
