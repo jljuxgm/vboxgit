2cbf6ee7304e173ac590b23a37696109c4b26033
qzuser <395143360@qq.com>
2022-06-05 09:53:55 +0800

    commit_48425 ------------------------------------------------------------------------ r48425 | vboxsync | 2013-09-11 19:41:57 +0800 (Wed, 11 Sep 2013) | 1 line
    Main: ApplianceImplExport: do not crash if VM has no CD/DVD medium.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-server/ApplianceImplExport.cpp
+++ vbox/src/VBox/Main/src-server/ApplianceImplExport.cpp
@@ -1,4 +1,4 @@
-/* $Id: ApplianceImplExport.cpp 48353 2013-09-06 12:28:50Z vboxsync $ */
+/* $Id: ApplianceImplExport.cpp 48425 2013-09-11 11:41:57Z vboxsync $ */
 /** @file
  *
  * IAppliance and IVirtualSystem COM class implementations.
@@ -477,6 +477,9 @@ STDMETHODIMP Machine::ExportTo(IAppliance *aAppliance, IN_BSTR location, IVirtua
 
                 case DeviceType_DVD:
                 {
+                    if (!pMedium)
+                        break;
+
                     /* get info about whether medium is a real drive/device or not */
                     BOOL fHostDrive = false;
                     rc = pMedium->COMGETTER(HostDrive)(&fHostDrive);
