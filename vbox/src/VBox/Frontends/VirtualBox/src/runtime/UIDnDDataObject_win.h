a74ea99011d3003ab6185da1d3dd64bdb5e62da1
qzuser <395143360@qq.com>
2022-06-05 19:53:08 +0800

    commit_50603 ------------------------------------------------------------------------ r50603 | vboxsync | 2014-02-26 20:56:01 +0800 (Wed, 26 Feb 2014) | 1 line
    DnD: Build fix.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIDnDDataObject_win.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIDnDDataObject_win.h
@@ -1,4 +1,4 @@
-/* $Id: UIDnDDataObject_win.h 50602 2014-02-26 12:50:39Z vboxsync $ */
+/* $Id: UIDnDDataObject_win.h 50603 2014-02-26 12:56:01Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -25,6 +25,9 @@
 #include <QString>
 #include <QStringList>
 #include <QVariant>
+
+/* COM includes: */
+#include "CSession.h"
 
 /* Forward declarations: */
 class UIDnDDrag;
