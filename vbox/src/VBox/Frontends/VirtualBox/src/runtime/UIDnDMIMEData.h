2d6c103c79e33b8daa6a05e1a0a221cc6d8fb470
qzuser <395143360@qq.com>
2022-06-05 19:51:10 +0800

    commit_50595 ------------------------------------------------------------------------ r50595 | vboxsync | 2014-02-26 17:48:29 +0800 (Wed, 26 Feb 2014) | 1 line
    DnD: Windows build fix.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIDnDMIMEData.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIDnDMIMEData.h
@@ -1,4 +1,4 @@
-/* $Id: UIDnDMIMEData.h 50593 2014-02-26 08:44:58Z vboxsync $ */
+/* $Id: UIDnDMIMEData.h 50595 2014-02-26 09:48:29Z vboxsync $ */
 /** @file
  *
  * VBox frontends: Qt GUI ("VirtualBox"):
@@ -62,7 +62,7 @@ public:
 
     UIDnDMimeData(CSession &session, QStringList formats,
                   Qt::DropAction defAction,
-                  Qt::DropActions actions, QWidget *pParent);
+                  Qt::DropActions actions, UIDnDDrag *pParent);
 
     int setData(const QString &mimeType);
 
@@ -100,7 +100,7 @@ protected:
 
 private:
 
-    QWidget          *m_pParent;
+    UIDnDDrag        *m_pParent;
     CSession          m_session;
     QStringList       m_lstFormats;
     Qt::DropAction    m_defAction;
