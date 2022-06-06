6389f46e5aac46aaadf89eee3be3e65014b16015
qzuser <395143360@qq.com>
2022-06-06 13:29:49 +0800

    commit_54629 ------------------------------------------------------------------------ r54629 | vboxsync | 2015-03-05 00:17:42 +0800 (Thu, 05 Mar 2015) | 2 lines
    FE/Qt: Before calling IMachine::CanShowConsoleWindow() check the session state first. This prevents an annoying E_INVALID_VM_STATE message in the VBoxSVC.log file.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGlobal.cpp 54627 2015-03-04 14:47:32Z vboxsync $ */
+/* $Id: VBoxGlobal.cpp 54629 2015-03-04 16:17:42Z vboxsync $ */
 /** @file
  * VBox Qt GUI - VBoxGlobal class implementation.
  */
@@ -4553,7 +4553,8 @@ bool VBoxGlobal::launchMachine(CMachine &machine, LaunchMode enmLaunchMode /* = 
     if (enmLaunchMode != LaunchMode_Separate)
     {
         /* Switch to machine window(s) if possible: */
-        if (machine.CanShowConsoleWindow())
+        if (   machine.GetSessionState() == KSessionState_Locked /* precondition for CanShowConsoleWindow() */
+            && machine.CanShowConsoleWindow())
             return VBoxGlobal::switchToMachine(machine);
 
         /* Make sure machine-state is one of required: */
