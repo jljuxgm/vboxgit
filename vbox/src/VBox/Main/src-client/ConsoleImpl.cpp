6ed5f9f96621c1f0c88def761bcc8734dc7c8b03
qzuser <395143360@qq.com>
2022-06-06 21:12:47 +0800

    commit_55542 ------------------------------------------------------------------------ r55542 | vboxsync | 2015-04-30 18:54:08 +0800 (Thu, 30 Apr 2015) | 1 line
    Main: take approvals of VBoxEventType_OnCanShowWindow into account.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/ConsoleImpl.cpp
+++ vbox/src/VBox/Main/src-client/ConsoleImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.cpp 55443 2015-04-27 12:09:42Z vboxsync $ */
+/* $Id: ConsoleImpl.cpp 55542 2015-04-30 10:54:08Z vboxsync $ */
 /** @file
  * VBox Console COM Class implementation
  */
@@ -6696,8 +6696,10 @@ HRESULT Console::i_onShowWindow(BOOL aCheck, BOOL *aCanShow, LONG64 *aWinId)
             if (pCanShowEvent)
             {
                 BOOL fVetoed = FALSE;
+                BOOL fApproved = FALSE;
                 pCanShowEvent->IsVetoed(&fVetoed);
-                *aCanShow = !fVetoed;
+                pCanShowEvent->IsApproved(&fApproved);
+                *aCanShow = fApproved || !fVetoed;
             }
             else
             {
