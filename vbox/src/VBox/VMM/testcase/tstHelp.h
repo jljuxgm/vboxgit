4c4f8830088894f75224f127a1785019403add85
qzuser <395143360@qq.com>
2022-06-06 17:18:24 +0800

    commit_55052 ------------------------------------------------------------------------ r55052 | vboxsync | 2015-04-01 03:09:50 +0800 (Wed, 01 Apr 2015) | 1 line
    build fix.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/testcase/tstHelp.h
+++ vbox/src/VBox/VMM/testcase/tstHelp.h
@@ -1,4 +1,4 @@
-/* $Id: tstHelp.h 55051 2015-03-31 19:06:19Z vboxsync $ */
+/* $Id: tstHelp.h 55052 2015-03-31 19:09:50Z vboxsync $ */
 /** @file
  * VMM testcase - Helper stuff.
  */
@@ -37,7 +37,7 @@ RT_C_DECLS_END
         if (off != RT_OFFSETOF(type, m)) \
         { \
             printf("error! %#010x %s  Off by %d!! (expected off=%#x)\n", \
-                   RT_OFFSETOF(type, m), #type "." #m, off - RT_OFFSETOF(type, m), off); \
+                   RT_OFFSETOF(type, m), #type "." #m, off - RT_OFFSETOF(type, m), (int)off); \
             rc++; \
         } \
         /*else */ \
