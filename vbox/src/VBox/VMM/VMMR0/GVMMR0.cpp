5bf98405a2d79812e4cc92012bdfd2cc6f442bf1
qzuser <395143360@qq.com>
2022-06-06 11:34:48 +0800

    commit_54191 ------------------------------------------------------------------------ r54191 | vboxsync | 2015-02-13 10:46:40 +0800 (Fri, 13 Feb 2015) | 1 line
    GVMMR0.cpp: Enable the periodic preemtion timer code on soalris.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR0/GVMMR0.cpp
+++ vbox/src/VBox/VMM/VMMR0/GVMMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: GVMMR0.cpp 52618 2014-09-05 12:07:29Z vboxsync $ */
+/* $Id: GVMMR0.cpp 54191 2015-02-13 02:46:40Z vboxsync $ */
 /** @file
  * GVMM - Global VM Manager.
  */
@@ -84,7 +84,7 @@
 /*******************************************************************************
 *   Defined Constants And Macros                                               *
 *******************************************************************************/
-#if defined(RT_OS_LINUX) || defined(DOXYGEN_RUNNING)
+#if defined(RT_OS_LINUX) || defined(RT_OS_SOLARIS) || defined(DOXYGEN_RUNNING)
 /** Define this to enable the periodic preemption timer. */
 # define GVMM_SCHED_WITH_PPT
 #endif
