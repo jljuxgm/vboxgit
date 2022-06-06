28b80db9bd138a4e781da5d8177113bb987c0944
qzuser <395143360@qq.com>
2022-06-06 13:29:13 +0800

    commit_54627 ------------------------------------------------------------------------ r54627 | vboxsync | 2015-03-04 22:47:32 +0800 (Wed, 04 Mar 2015) | 1 line
    FE/Qt: Reverting r98765 as it's too early for it, need to make it build under Win/X11 first.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGMachinePreview.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGMachinePreview.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGMachinePreview.cpp 54622 2015-03-04 14:23:10Z vboxsync $ */
+/* $Id: UIGMachinePreview.cpp 54627 2015-03-04 14:47:32Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIGMachinePreview class implementation.
  */
@@ -40,12 +40,6 @@
 
 #endif /* !VBOX_WITH_PRECOMPILED_HEADERS */
 
-/* VirtualBox interface declarations: */
-#ifndef VBOX_WITH_XPCOM
-# include "VirtualBox.h"
-#else /* !VBOX_WITH_XPCOM */
-# include "VirtualBox_XPCOM.h"
-#endif /* VBOX_WITH_XPCOM */
 
 UIGMachinePreview::UIGMachinePreview(QIGraphicsWidget *pParent)
     : QIWithRetranslateUI4<QIGraphicsWidget>(pParent)
