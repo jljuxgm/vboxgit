5a0e132c315ea1b1a9b05c358aac975d7fca5914
qzuser <395143360@qq.com>
2022-06-06 15:44:23 +0800

    commit_54892 ------------------------------------------------------------------------ r54892 | vboxsync | 2015-03-22 02:01:12 +0800 (Sun, 22 Mar 2015) | 1 line
    typo
    ------------------------------------------------------------------------

--- vbox/src/VBox/HostDrivers/Support/SUPDrv.cpp
+++ vbox/src/VBox/HostDrivers/Support/SUPDrv.cpp
@@ -1,4 +1,4 @@
-p/* $Id: SUPDrv.cpp 54889 2015-03-20 23:50:18Z vboxsync $ */
+/* $Id: SUPDrv.cpp 54890 2015-03-20 23:51:19Z vboxsync $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Common code.
  */
