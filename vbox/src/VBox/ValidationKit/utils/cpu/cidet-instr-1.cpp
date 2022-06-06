5faad1b4626f5878dacd21329e8001d0c65801f0
qzuser <395143360@qq.com>
2022-06-07 05:35:53 +0800

    commit_56295 ------------------------------------------------------------------------ r56295 | vboxsync | 2015-06-09 22:29:55 +0800 (Tue, 09 Jun 2015) | 1 line
    ValidationKit: Updated (C) year.
    ------------------------------------------------------------------------

--- vbox/src/VBox/ValidationKit/utils/cpu/cidet-instr-1.cpp
+++ vbox/src/VBox/ValidationKit/utils/cpu/cidet-instr-1.cpp
@@ -1,10 +1,10 @@
-/* $Id: cidet-instr-1.cpp 53609 2014-12-30 18:34:49Z vboxsync $ */
+/* $Id: cidet-instr-1.cpp 56295 2015-06-09 14:29:55Z vboxsync $ */
 /** @file
  * CPU Instruction Decoding & Execution Tests - First bunch of instructions.
  */
 
 /*
- * Copyright (C) 2014 Oracle Corporation
+ * Copyright (C) 2014-2015 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
