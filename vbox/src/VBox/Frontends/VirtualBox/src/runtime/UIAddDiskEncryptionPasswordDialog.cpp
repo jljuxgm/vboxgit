ce35a2ce2dc96dfd97df894ede3b35b79f1842be
qzuser <395143360@qq.com>
2022-06-06 16:18:47 +0800

    commit_54939 ------------------------------------------------------------------------ r54939 | vboxsync | 2015-03-25 21:11:17 +0800 (Wed, 25 Mar 2015) | 2 lines
    tdTreeDepth1.py: make pylint happy
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIAddDiskEncryptionPasswordDialog.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIAddDiskEncryptionPasswordDialog.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIAddDiskEncryptionPasswordDialog.cpp 54935 2015-03-24 18:50:52Z vboxsync $ */
+/* $Id: UIAddDiskEncryptionPasswordDialog.cpp 54936 2015-03-24 18:59:07Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIAddDiskEncryptionPasswordDialog class implementation.
  */
@@ -398,7 +398,7 @@ UIAddDiskEncryptionPasswordDialog::UIAddDiskEncryptionPasswordDialog(QWidget *pP
 {
     /* Prepare: */
     prepare();
-    /* Retranslate: */
+    /* Translate: */
     retranslateUi();
 }
 
