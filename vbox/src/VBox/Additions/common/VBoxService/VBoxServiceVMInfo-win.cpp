e81ba6ec2b442681ef08dc2382798342e2a0159a
qzuser <395143360@qq.com>
2022-06-06 09:08:18 +0800

    commit_53764 ------------------------------------------------------------------------ r53764 | vboxsync | 2015-01-10 00:03:52 +0800 (Sat, 10 Jan 2015) | 2 lines
    Additions/VBoxService: added a few missing \n and limit the occurrence of an error message
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo-win.cpp
+++ vbox/src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxServiceVMInfo-win.cpp 53333 2014-11-14 16:25:32Z vboxsync $ */
+/* $Id: VBoxServiceVMInfo-win.cpp 53764 2015-01-09 16:03:52Z vboxsync $ */
 /** @file
  * VBoxService - Virtual Machine Information for the Host, Windows specifics.
  */
@@ -260,7 +260,7 @@ static int VBoxServiceVMInfoWinProcessesGetTokenInfo(PVBOXSERVICEVMINFOPROC pPro
                 break;
 
             default:
-                VBoxServiceError("Token class not implemented: %ld", tkClass);
+                VBoxServiceError("Token class not implemented: %ld\n", tkClass);
                 rc = VERR_NOT_IMPLEMENTED;
                 break;
         }
