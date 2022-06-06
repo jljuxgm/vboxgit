731aea402848833cd409e11c796f7c7e162fc187
qzuser <395143360@qq.com>
2022-06-07 03:17:39 +0800

    commit_55988 ------------------------------------------------------------------------ r55988 | vboxsync | 2015-05-21 07:24:44 +0800 (Thu, 21 May 2015) | 1 line
    iprt/log.h,SUPDrv: Replaced the 'personal' logging groups with 6 more generic logging levels (7 thru 12) and a 'Warn' level. The 'Warn' level is enabled by 'group.e' together with level 1 logging.  Modified the new RTLog[Rel][Get]DefaultInstanceEx functions to only take one 32-bit parameter to minimize call setup time and size.  Major support driver version bump.  LogAleksey=Log7, LogBird=Log8, LogSunlover=Log9, none of the other personal macros was used.  Log*Warning got renamed to Log1*Warning so as to not confuse it with the LogWarn/LogRelWarn macros.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/SessionImpl.cpp
+++ vbox/src/VBox/Main/src-client/SessionImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: SessionImpl.cpp 55800 2015-05-11 14:09:09Z vboxsync $ */
+/* $Id: SessionImpl.cpp 55988 2015-05-20 23:24:44Z vboxsync $ */
 /** @file
  * VBox Client Session COM Class implementation in VBoxC.
  */
@@ -546,7 +546,7 @@ HRESULT Session::uninitialize()
     }
     else
     {
-        LogWarningThisFunc(("UNEXPECTED uninitialization!\n"));
+        Log1WarningThisFunc(("UNEXPECTED uninitialization!\n"));
         rc = autoCaller.rc();
     }
 
