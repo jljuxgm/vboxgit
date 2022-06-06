064a4a8e3d2ce30b0c674239b814934e5a2d7b36
qzuser <395143360@qq.com>
2022-06-06 20:15:42 +0800

    commit_55471 ------------------------------------------------------------------------ r55471 | vboxsync | 2015-04-28 16:02:03 +0800 (Tue, 28 Apr 2015) | 1 line
    Additions/x11/vboxvideo: test for susceptibility to X.Org Server 1.3 RandR bug, works on all later server versions.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR3/CPUM.cpp
+++ vbox/src/VBox/VMM/VMMR3/CPUM.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUM.cpp 55466 2015-04-28 00:10:51Z vboxsync $ */
+/* $Id: CPUM.cpp 55470 2015-04-28 00:49:31Z vboxsync $ */
 /** @file
  * CPUM - CPU Monitor / Manager.
  */
@@ -1413,6 +1413,7 @@ static DECLCALLBACK(int) cpumR3LoadExec(PVM pVM, PSSMHANDLE pSSM, uint32_t uVers
             SSMR3GetU32(pSSM, &pVCpu->cpum.s.fUseFlags);
             SSMR3GetU32(pSSM, &pVCpu->cpum.s.fChanged);
 
+            rc = VINF_SUCCESS;
             if (uVersion > CPUM_SAVED_STATE_VERSION_NO_MSR_SIZE)
                 rc = SSMR3GetMem(pSSM, &pVCpu->cpum.s.GuestMsrs.au64[0], cbMsrs);
             else if (uVersion >= CPUM_SAVED_STATE_VERSION_VER3_0)
