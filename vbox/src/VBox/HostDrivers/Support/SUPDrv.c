9b38748c81ec90b3eab7fb952866a88cd97d112e
qzuser <395143360@qq.com>
2022-06-06 11:56:47 +0800

    commit_54275 ------------------------------------------------------------------------ r54275 | vboxsync | 2015-02-19 02:31:23 +0800 (Thu, 19 Feb 2015) | 2 lines
    missed copyright year update.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/HostDrivers/Support/SUPDrv.c
+++ vbox/src/VBox/HostDrivers/Support/SUPDrv.c
@@ -1,10 +1,10 @@
-/* $Id: SUPDrv.c 54274 2015-02-18 18:30:43Z vboxsync $ */
+/* $Id: SUPDrv.c 54275 2015-02-18 18:31:23Z vboxsync $ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Common code.
  */
 
 /*
- * Copyright (C) 2006-2014 Oracle Corporation
+ * Copyright (C) 2006-2015 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
