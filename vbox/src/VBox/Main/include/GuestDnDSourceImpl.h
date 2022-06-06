b4e5968b12f2606161f218ba94dbb3f981d286ab
qzuser <395143360@qq.com>
2022-06-07 03:09:05 +0800

    commit_55964 ------------------------------------------------------------------------ r55964 | vboxsync | 2015-05-20 19:38:57 +0800 (Wed, 20 May 2015) | 1 line
    Storage/VD: Make sure the disk is destroyed when destroying the driver instance, the power off callback is not called when the driver is destroyed during driver chain replumbing (reconfiguring media after taking a snapshot for example)
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/GuestDnDSourceImpl.h
+++ vbox/src/VBox/Main/include/GuestDnDSourceImpl.h
@@ -1,4 +1,4 @@
-/* $Id: GuestDnDSourceImpl.h 55549 2015-04-30 12:28:26Z vboxsync $ */
+/* $Id: GuestDnDSourceImpl.h 55963 2015-05-20 11:18:14Z vboxsync $ */
 /** @file
  * VBox Console COM Class implementation - Guest drag'n drop source.
  */
@@ -73,7 +73,13 @@ protected:
 
 protected:
 
+    static Utf8Str i_guestErrorToString(int guestRc);
+    static Utf8Str i_hostErrorToString(int hostRc);
+
+    /** @name Thread callbacks.
+     * @{ */
     static DECLCALLBACK(int) i_receiveDataThread(RTTHREAD Thread, void *pvUser);
+    /** @}  */
 
     /** @name Callbacks for dispatch handler.
      * @{ */
