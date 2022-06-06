69eb8d3771cd336bd5b10509dc65061b0fbeca28
qzuser <395143360@qq.com>
2022-06-06 18:12:23 +0800

    commit_55185 ------------------------------------------------------------------------ r55185 | vboxsync | 2015-04-10 22:38:55 +0800 (Fri, 10 Apr 2015) | 1 line
    Oops
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/ConsoleImpl.cpp
+++ vbox/src/VBox/Main/src-client/ConsoleImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImpl.cpp 55182 2015-04-10 14:26:59Z vboxsync $ */
+/* $Id: ConsoleImpl.cpp 55185 2015-04-10 14:38:55Z vboxsync $ */
 /** @file
  * VBox Console COM Class implementation
  */
@@ -3558,8 +3558,6 @@ const char *Console::i_convertControllerTypeToDev(StorageControllerType_T enmCtr
             return "i82078";
         case StorageControllerType_USB:
             return "Msd";
-        case StorageControllerType_NVMe:
-            return "nvme";
         default:
             return NULL;
     }
