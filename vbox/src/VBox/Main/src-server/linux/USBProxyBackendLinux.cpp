8386201f622d3505d444f556664bda1ff4357bed
qzuser <395143360@qq.com>
2022-06-08 03:01:33 +0800

    commit_60106 ------------------------------------------------------------------------ r60106 | vboxsync | 2016-03-19 06:03:05 +0800 (Sat, 19 Mar 2016) | 1 line
    USBProxyBackendLinux: Hopefully fix crashes introduced with last commit
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-server/linux/USBProxyBackendLinux.cpp
+++ vbox/src/VBox/Main/src-server/linux/USBProxyBackendLinux.cpp
@@ -1,4 +1,4 @@
-/* $Id: USBProxyBackendLinux.cpp 60089 2016-03-18 10:51:02Z vboxsync $ */
+/* $Id: USBProxyBackendLinux.cpp 60106 2016-03-18 22:03:05Z vboxsync $ */
 /** @file
  * VirtualBox USB Proxy Service, Linux Specialization.
  */
@@ -60,7 +60,7 @@
  * Initialize data members.
  */
 USBProxyBackendLinux::USBProxyBackendLinux()
-    : USBProxyBackend(), mhWakeupPipeR(NIL_RTPIPE), mhWakeupPipeW(NIL_RTPIPE)
+    : USBProxyBackend(), mhWakeupPipeR(NIL_RTPIPE), mhWakeupPipeW(NIL_RTPIPE), mpWaiter(NULL)
 {
     LogFlowThisFunc(("\n"));
 }
