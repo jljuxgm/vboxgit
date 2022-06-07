2eaf227c1eb0825c345afa8b5d3cca90acf1a061
qzuser <395143360@qq.com>
2022-06-07 14:06:27 +0800

    commit_58089 ------------------------------------------------------------------------ r58089 | vboxsync | 2015-10-07 20:33:00 +0800 (Wed, 07 Oct 2015) | 1 line
    VBoxGuest,VBoxService,Doxyfile.Core: fixes.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuest-win.cpp
+++ vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuest-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuest-win.cpp 58053 2015-10-06 14:42:35Z vboxsync $ */
+/* $Id: VBoxGuest-win.cpp 58089 2015-10-07 12:33:00Z vboxsync $ */
 /** @file
  * VBoxGuest - Windows specifics.
  */
@@ -554,7 +554,7 @@ NTSTATUS vbgdNtInit(PDRIVER_OBJECT pDrvObj, PDEVICE_OBJECT pDevObj, PUNICODE_STR
  * Cleans up hardware resources.
  * Do not delete DevExt here.
  *
- * @param   pDrvObj     Driver object.
+ * @param   pDevObj     Device object.
  */
 NTSTATUS vbgdNtCleanup(PDEVICE_OBJECT pDevObj)
 {
