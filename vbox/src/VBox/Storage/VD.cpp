fd5790005b778f8060207dd9653829f28ecc445e
qzuser <395143360@qq.com>
2022-06-08 05:46:17 +0800

    commit_60873 ------------------------------------------------------------------------ r60873 | vboxsync | 2016-05-08 01:53:54 +0800 (Sun, 08 May 2016) | 1 line
    please stay without 130 columns.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Storage/VD.cpp
+++ vbox/src/VBox/Storage/VD.cpp
@@ -1,4 +1,4 @@
-/* $Id: VD.cpp 60032 2016-03-15 11:08:38Z vboxsync $ */
+/* $Id: VD.cpp 60873 2016-05-07 17:53:54Z vboxsync $ */
 /** @file
  * VBoxHDD - VBox HDD Container implementation.
  */
@@ -3750,7 +3750,8 @@ static int vdPluginLoadFromPath(const char *pszPath)
         goto out;
     }
 
-    while ((rc = RTDirReadEx(pPluginDir, pPluginDirEntry, &cbPluginDirEntry, RTFSOBJATTRADD_NOTHING, RTPATH_F_ON_LINK)) != VERR_NO_MORE_FILES)
+    while (   (rc = RTDirReadEx(pPluginDir, pPluginDirEntry, &cbPluginDirEntry, RTFSOBJATTRADD_NOTHING, RTPATH_F_ON_LINK))
+           != VERR_NO_MORE_FILES)
     {
         char *pszPluginPath = NULL;
 
