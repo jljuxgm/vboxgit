743a7742761fde3954da8427a491cc2995b1d0aa
qzuser <395143360@qq.com>
2022-06-07 14:46:50 +0800

    commit_58142 ------------------------------------------------------------------------ r58142 | vboxsync | 2015-10-09 20:07:45 +0800 (Fri, 09 Oct 2015) | 1 line
    FE/Qt: Mac OS X: Zoom botton handling (full-screen/maximize).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/scale/UIMachineWindowScale.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/scale/UIMachineWindowScale.h
@@ -1,4 +1,4 @@
-/* $Id: UIMachineWindowScale.h 57027 2015-07-20 16:41:01Z vboxsync $ */
+/* $Id: UIMachineWindowScale.h 58142 2015-10-09 12:07:45Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIMachineWindowScale class declaration.
  */
@@ -45,6 +45,10 @@ private:
 
     /** Save settings routine. */
     void saveSettings();
+#ifdef Q_WS_MAC
+    /** Cleanup visual-state routine. */
+    void cleanupVisualState();
+#endif /* Q_WS_MAC */
 
     /** Updates visibility according to visual-state. */
     void showInNecessaryMode();
