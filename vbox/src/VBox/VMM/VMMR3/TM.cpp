6fb39b8b7fc14180a5637618f909cae5fd6a0bfd
qzuser <395143360@qq.com>
2022-06-07 08:54:51 +0800

    commit_57078 ------------------------------------------------------------------------ r57078 | vboxsync | 2015-07-25 04:16:59 +0800 (Sat, 25 Jul 2015) | 1 line
    sup,iprt: SUPIsHostVirtualized belongs in IPRT and does not need to be inlined. Created RTSystemIsInserideVM() with simple amd64+x86 implementation.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR3/TM.cpp
+++ vbox/src/VBox/VMM/VMMR3/TM.cpp
@@ -1,4 +1,4 @@
-/* $Id: TM.cpp 57075 2015-07-24 16:27:27Z vboxsync $ */
+/* $Id: TM.cpp 57078 2015-07-24 20:16:59Z vboxsync $ */
 /** @file
  * TM - Time Manager.
  */
@@ -1360,7 +1360,7 @@ static DECLCALLBACK(int) tmR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t uVersion, u
      * switches during VM runtime (e.g. by GIM).
      */
     uint64_t uGipCpuHz;
-    bool fRelax  = SUPIsHostVirtualized();
+    bool fRelax  = RTSystemIsInsideVM();
     bool fCompat = SUPIsTscFreqCompatible(pVM->tm.s.cTSCTicksPerSecond, &uGipCpuHz, fRelax);
     if (!fCompat)
     {
