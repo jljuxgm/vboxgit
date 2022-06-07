a0317e40c1ed9b1496819241ae06115b70b5b593
qzuser <395143360@qq.com>
2022-06-07 12:30:00 +0800

    commit_57805 ------------------------------------------------------------------------ r57805 | vboxsync | 2015-09-17 23:01:21 +0800 (Thu, 17 Sep 2015) | 1 line
    FE/Qt: Small cleanup related to UIMachine.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGlobal.cpp 57611 2015-09-03 14:38:36Z vboxsync $ */
+/* $Id: VBoxGlobal.cpp 57805 2015-09-17 15:01:21Z vboxsync $ */
 /** @file
  * VBox Qt GUI - VBoxGlobal class implementation.
  */
@@ -455,11 +455,6 @@ UISelectorWindow &VBoxGlobal::selectorWnd()
     }
 
     return *mSelectorWnd;
-}
-
-UIMachine* VBoxGlobal::virtualMachine() const
-{
-    return gpMachine;
 }
 
 QWidget* VBoxGlobal::activeMachineWindow() const
