fef615cedd45d607c8b8e4cf6826b6d3e4720eda
qzuser <395143360@qq.com>
2022-06-07 10:24:40 +0800

    commit_57357 ------------------------------------------------------------------------ r57357 | vboxsync | 2015-08-14 23:04:46 +0800 (Fri, 14 Aug 2015) | 1 line
    Additions/x11/VBoxClient: remove global VT switching handling again.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/x11/VBoxClient/hostversion.cpp
+++ vbox/src/VBox/Additions/x11/VBoxClient/hostversion.cpp
@@ -1,4 +1,4 @@
-/* $Id: hostversion.cpp 55600 2015-05-02 08:05:53Z vboxsync $ */
+/* $Id: hostversion.cpp 57357 2015-08-14 15:04:46Z vboxsync $ */
 /** @file
  * X11 guest client - host version check.
  */
@@ -207,8 +207,6 @@ struct VBCLSERVICE vbclHostVersionInterface =
     getPidFilePath,
     VBClServiceDefaultHandler, /* init */
     run,
-    VBClServiceDefaultHandler, /* pause */
-    VBClServiceDefaultHandler, /* resume */
     VBClServiceDefaultCleanup
 };
 
