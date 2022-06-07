e705dbc26457abb37d710b9ae860284da8adbd87
qzuser <395143360@qq.com>
2022-06-07 08:29:28 +0800

    commit_56994 ------------------------------------------------------------------------ r56994 | vboxsync | 2015-07-19 07:15:01 +0800 (Sun, 19 Jul 2015) | 1 line
    Main: Log and assert formatting fixes.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-all/VirtualBoxBase.cpp
+++ vbox/src/VBox/Main/src-all/VirtualBoxBase.cpp
@@ -1,4 +1,4 @@
-/* $Id: VirtualBoxBase.cpp 55988 2015-05-20 23:24:44Z vboxsync $ */
+/* $Id: VirtualBoxBase.cpp 56994 2015-07-18 23:15:01Z vboxsync $ */
 
 /** @file
  *
@@ -181,7 +181,6 @@ HRESULT VirtualBoxBase::setErrorInternal(HRESULT aResultCode,
                 &aIID,
                 pcszComponent,
                 aText.c_str(),
-                aWarning,
                 preserve,
                 aResultDetail));
 
