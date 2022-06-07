8af7e4ca8ad83f69f319165bdbd955c01a1c6b5d
qzuser <395143360@qq.com>
2022-06-07 22:15:48 +0800

    commit_58993 ------------------------------------------------------------------------ r58993 | vboxsync | 2015-12-05 00:26:50 +0800 (Sat, 05 Dec 2015) | 1 line
    bugref:8153: vboxvideo: ABI issues / software rendering on Linux guests: add a new service to VBoxClient to test whether accelerated 3D pass-through is currently available by trying to connect to the HGCM service and signalling success or failure in the process exit code.  Add generic HGCM connect and disconnect functionality to VBoxGuestR3Lib to support this.  Add the concept of non-daemon services to VBoxClient as we do not need to daemonise or create a pid-file for this quick test.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/x11/VBoxClient/VBoxClient.h
+++ vbox/src/VBox/Additions/x11/VBoxClient/VBoxClient.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxClient.h 57357 2015-08-14 15:04:46Z vboxsync $ */
+/* $Id: VBoxClient.h 58993 2015-12-04 16:26:50Z vboxsync $ */
 /** @file
  *
  * VirtualBox additions user session daemon.
@@ -77,5 +77,6 @@ extern struct VBCLSERVICE **VBClGetHostVersionService();
 #ifdef VBOX_WITH_DRAG_AND_DROP
 extern struct VBCLSERVICE **VBClGetDragAndDropService();
 #endif /* VBOX_WITH_DRAG_AND_DROP */
+extern struct VBCLSERVICE **VBClCheck3DService();
 
 #endif /* !___vboxclient_vboxclient_h */
