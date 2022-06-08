3b644ac6e7044b75e053b77d2fa8f53f4a009236
qzuser <395143360@qq.com>
2022-06-08 19:55:36 +0800

    commit_64387 ------------------------------------------------------------------------ r64387 | vboxsync | 2016-10-24 22:06:02 +0800 (Mon, 24 Oct 2016) | 1 line
    PDM,Devices: Some PCI device type cleanup.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Graphics/DevVGA-SVGA.h
+++ vbox/src/VBox/Devices/Graphics/DevVGA-SVGA.h
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA.h 64373 2016-10-23 19:03:39Z vboxsync $ */
+/* $Id: DevVGA-SVGA.h 64387 2016-10-24 14:06:02Z vboxsync $ */
 /** @file
  * VMware SVGA device
  */
@@ -42,7 +42,7 @@
 #define VMSVGA_ACTION_CHANGEMODE_BIT    0
 #define VMSVGA_ACTION_CHANGEMODE        RT_BIT(VMSVGA_ACTION_CHANGEMODE_BIT)
 
-DECLCALLBACK(int) vmsvgaR3IORegionMap(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, uint32_t iRegion,
+DECLCALLBACK(int) vmsvgaR3IORegionMap(PPDMDEVINS pDevIns, PPDMPCIDEV pPciDev, uint32_t iRegion,
                                       RTGCPHYS GCPhysAddress, RTGCPHYS cb, PCIADDRESSSPACE enmType);
 
 DECLCALLBACK(void) vmsvgaPortSetViewport(PPDMIDISPLAYPORT pInterface, uint32_t uScreenId,
