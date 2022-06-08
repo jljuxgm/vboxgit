e08d354114ef4a2071e2472fa315c2624d9231e6
qzuser <395143360@qq.com>
2022-06-09 06:45:53 +0800

    commit_66680 ------------------------------------------------------------------------ r66680 | vboxsync | 2017-04-26 23:26:09 +0800 (Wed, 26 Apr 2017) | 1 line
    FE/Qt: Selector UI: Small cleanup related to Virtual Medium Manager menu action.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/selector/UIActionPoolSelector.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/selector/UIActionPoolSelector.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIActionPoolSelector.cpp 62493 2016-07-22 18:44:18Z vboxsync $ */
+/* $Id: UIActionPoolSelector.cpp 66680 2017-04-26 15:26:09Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIActionPoolSelector class implementation.
  */
@@ -57,13 +57,13 @@ protected:
     }
 };
 
-class UIActionSimpleMediumManagerDialog : public UIActionSimple
+class UIActionSimpleVirtualMediumManagerDialog : public UIActionSimple
 {
     Q_OBJECT;
 
 public:
 
-    UIActionSimpleMediumManagerDialog(UIActionPool *pParent)
+    UIActionSimpleVirtualMediumManagerDialog(UIActionPool *pParent)
         : UIActionSimple(pParent, ":/diskimage_16px.png") {}
 
 protected:
@@ -1033,7 +1033,7 @@ void UIActionPoolSelector::preparePool()
 {
     /* 'File' actions: */
     m_pool[UIActionIndexST_M_File] = new UIActionMenuFile(this);
-    m_pool[UIActionIndexST_M_File_S_ShowMediumManager] = new UIActionSimpleMediumManagerDialog(this);
+    m_pool[UIActionIndexST_M_File_S_ShowVirtualMediumManager] = new UIActionSimpleVirtualMediumManagerDialog(this);
     m_pool[UIActionIndexST_M_File_S_ImportAppliance] = new UIActionSimpleImportApplianceWizard(this);
     m_pool[UIActionIndexST_M_File_S_ExportAppliance] = new UIActionSimpleExportApplianceWizard(this);
 #ifdef VBOX_GUI_WITH_EXTRADATA_MANAGER_UI
