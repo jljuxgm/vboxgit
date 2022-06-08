ff08cde79c3add1dc5d85124dbe53680afea529f
qzuser <395143360@qq.com>
2022-06-08 08:38:44 +0800

    commit_61683 ------------------------------------------------------------------------ r61683 | vboxsync | 2016-06-13 23:11:12 +0800 (Mon, 13 Jun 2016) | 1 line
    SVM: Allow additional return code.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR0/HMSVMR0.cpp
+++ vbox/src/VBox/VMM/VMMR0/HMSVMR0.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMSVMR0.cpp 61666 2016-06-10 16:58:56Z vboxsync $ */
+/* $Id: HMSVMR0.cpp 61683 2016-06-13 15:11:12Z vboxsync $ */
 /** @file
  * HM SVM (AMD-V) - Host Context Ring-0.
  */
@@ -4676,7 +4676,8 @@ HMSVM_EXIT_DECL hmR0SvmExitMsr(PVMCPU pVCpu, PCPUMCTX pCtx, PSVMTRANSIENT pSvmTr
             if (RT_LIKELY(rc == VINF_SUCCESS))
                 HMSVM_CHECK_SINGLE_STEP(pVCpu, rc);     /* RIP updated by EMInterpretInstruction(). */
             else
-                AssertMsg(rc == VERR_EM_INTERPRETER, ("hmR0SvmExitMsr: WrMsr. EMInterpretInstruction failed rc=%Rrc\n", rc));
+                AssertMsg(   rc == VERR_EM_INTERPRETER
+                          || rc == VINF_CPUM_R3_MSR_WRITE, ("hmR0SvmExitMsr: WrMsr. EMInterpretInstruction failed rc=%Rrc\n", rc));
         }
 
         if (rc == VINF_SUCCESS)
