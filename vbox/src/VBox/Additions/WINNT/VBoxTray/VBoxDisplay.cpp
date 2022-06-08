b2274406b9bed47bf3a277864aee443722ae3ec7
qzuser <395143360@qq.com>
2022-06-08 08:25:39 +0800

    commit_61620 ------------------------------------------------------------------------ r61620 | vboxsync | 2016-06-09 19:36:41 +0800 (Thu, 09 Jun 2016) | 1 line
    VBoxTray: primary guest display must remain at 0,0.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/WINNT/VBoxTray/VBoxDisplay.cpp
+++ vbox/src/VBox/Additions/WINNT/VBoxTray/VBoxDisplay.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxDisplay.cpp 60020 2016-03-14 12:41:40Z vboxsync $ */
+/* $Id: VBoxDisplay.cpp 61620 2016-06-09 11:36:41Z vboxsync $ */
 /** @file
  * VBoxSeamless - Display notifications.
  */
@@ -459,6 +459,7 @@ DWORD VBoxDisplayGetConfig(const DWORD NumDevices, DWORD *pDevPrimaryNum, DWORD 
     }
 
     *pNumDevices = DevNum;
+    *pDevPrimaryNum = DevPrimaryNum;
 
     return NO_ERROR;
 }
