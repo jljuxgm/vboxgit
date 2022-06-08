3a983e61889f66610dffc4ec3782d974b6a5e1b3
qzuser <395143360@qq.com>
2022-06-08 19:50:58 +0800

    commit_64367 ------------------------------------------------------------------------ r64367 | vboxsync | 2016-10-23 01:25:58 +0800 (Sun, 23 Oct 2016) | 1 line
    ValidationKit/Bs3Kit: Doxygen
    ------------------------------------------------------------------------

--- vbox/src/VBox/ValidationKit/bootsectors/bs3kit/bs3-cmn-PitIrqHandler.c
+++ vbox/src/VBox/ValidationKit/bootsectors/bs3kit/bs3-cmn-PitIrqHandler.c
@@ -1,6 +1,6 @@
-/* $Id: bs3-cmn-PitIrqHandler.c 62471 2016-07-22 18:04:30Z vboxsync $ */
+/* $Id: bs3-cmn-PitIrqHandler.c 64367 2016-10-22 17:25:58Z vboxsync $ */
 /** @file
- * BS3Kit - The PIT IRQ Handler and associated data..
+ * BS3Kit - The PIT IRQ Handler and associated data.
  */
 
 /*
