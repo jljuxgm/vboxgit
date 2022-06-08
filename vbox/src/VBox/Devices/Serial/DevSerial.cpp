d914e5ad316d6dfee3bbacdce06dca28cfcef536
qzuser <395143360@qq.com>
2022-06-08 11:41:46 +0800

    commit_62507 ------------------------------------------------------------------------ r62507 | vboxsync | 2016-07-23 03:09:53 +0800 (Sat, 23 Jul 2016) | 1 line
    (C) 2016
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Serial/DevSerial.cpp
+++ vbox/src/VBox/Devices/Serial/DevSerial.cpp
@@ -1,11 +1,11 @@
-/* $Id: DevSerial.cpp 58132 2015-10-09 00:09:37Z vboxsync $ */
+/* $Id: DevSerial.cpp 62507 2016-07-22 19:09:53Z vboxsync $ */
 /** @file
  * DevSerial - 16550A UART emulation.
  * (taken from hw/serial.c 2010/05/15 with modifications)
  */
 
 /*
- * Copyright (C) 2006-2015 Oracle Corporation
+ * Copyright (C) 2006-2016 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
