cc7b102dbdfb86e0d52c81148b61bd3a536dd219
qzuser <395143360@qq.com>
2022-06-09 03:12:10 +0800

    commit_65702 ------------------------------------------------------------------------ r65702 | vboxsync | 2017-02-09 22:59:35 +0800 (Thu, 09 Feb 2017) | 1 line
    FE/Qt: Global preferences: Display page: Softly remove 'auto' value for an 'GUI/MaxGuestResolution' extra-data flag, it will be silently replaced with null string ('softly' means reg-exp will still accept it).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/VBoxGlobalSettings.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/VBoxGlobalSettings.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGlobalSettings.cpp 62493 2016-07-22 18:44:18Z vboxsync $ */
+/* $Id: VBoxGlobalSettings.cpp 65702 2017-02-09 14:59:35Z vboxsync $ */
 /** @file
  * VBox Qt GUI - VBoxGlobalSettingsData, VBoxGlobalSettings class implementation.
  */
@@ -67,7 +67,7 @@ VBoxGlobalSettingsData::VBoxGlobalSettingsData()
 #endif
     guiFeatures = QString::null;
     languageId  = QString::null;
-    maxGuestRes = "auto";
+    maxGuestRes = QString::null;
     remapScancodes = QString::null;
     proxySettings = QString::null;
     hostScreenSaverDisabled = false;
