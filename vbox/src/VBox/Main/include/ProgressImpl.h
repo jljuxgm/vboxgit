aef3dfbe61f34052281afe58543b0c4ef842b28a
qzuser <395143360@qq.com>
2022-06-09 00:47:28 +0800

    commit_65103 ------------------------------------------------------------------------ r65103 | vboxsync | 2017-01-04 20:08:18 +0800 (Wed, 04 Jan 2017) | 2 lines
    Main: doxygen fixes
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/ProgressImpl.h
+++ vbox/src/VBox/Main/include/ProgressImpl.h
@@ -1,4 +1,4 @@
-/* $Id: ProgressImpl.h 63147 2016-08-08 11:12:33Z vboxsync $ */
+/* $Id: ProgressImpl.h 65103 2017-01-04 12:08:18Z vboxsync $ */
 /** @file
  *
  * VirtualBox COM class implementation
@@ -78,7 +78,7 @@ public:
      * @param aDescription
      * @param aCancelable
      * @param cOperations
-     * @param bstrFirstOperationDescription
+     * @param aFirstOperationDescription
      * @return
      */
     HRESULT init(
