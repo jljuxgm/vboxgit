0c4112cec33f8ad3c123e2bc42528d96ae6a9ffc
qzuser <395143360@qq.com>
2022-06-08 17:23:19 +0800

    commit_63781 ------------------------------------------------------------------------ r63781 | vboxsync | 2016-09-10 05:05:41 +0800 (Sat, 10 Sep 2016) | 1 line
    Storage/VD: Rename VBOXHDDBACKEND to VDIMAGEBACKEND to follow the naming scheme used for the other backend types (cache and filter)
    ------------------------------------------------------------------------

--- vbox/src/VBox/Storage/VHDX.cpp
+++ vbox/src/VBox/Storage/VHDX.cpp
@@ -1,4 +1,4 @@
-/* $Id: VHDX.cpp 63567 2016-08-16 14:06:54Z vboxsync $ */
+/* $Id: VHDX.cpp 63781 2016-09-09 21:05:41Z vboxsync $ */
 /** @file
  * VHDX - VHDX Disk image, Core Code.
  */
@@ -2486,12 +2486,12 @@ static DECLCALLBACK(void) vhdxDump(void *pBackendData)
 }
 
 
-const VBOXHDDBACKEND g_VhdxBackend =
+const VDIMAGEBACKEND g_VhdxBackend =
 {
     /* pszBackendName */
     "VHDX",
     /* cbSize */
-    sizeof(VBOXHDDBACKEND),
+    sizeof(VDIMAGEBACKEND),
     /* uBackendCaps */
     VD_CAP_FILE | VD_CAP_VFS,
     /* paFileExtensions */
