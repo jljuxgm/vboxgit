825aa87dcc728cae8848d5bb706a239bcf9572ae
qzuser <395143360@qq.com>
2022-06-08 11:45:13 +0800

    commit_62523 ------------------------------------------------------------------------ r62523 | vboxsync | 2016-07-23 03:17:44 +0800 (Sat, 23 Jul 2016) | 1 line
    (C) 2016
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/darwin/vboxfs/mount.vboxvfs.c
+++ vbox/src/VBox/Additions/darwin/vboxfs/mount.vboxvfs.c
@@ -1,10 +1,10 @@
-/* $Id: mount.vboxvfs.c 57063 2015-07-23 15:50:11Z vboxsync $ */
+/* $Id: mount.vboxvfs.c 62523 2016-07-22 19:17:44Z vboxsync $ */
 /** @file
  * VBoxVFS - mount tool.
  */
 
 /*
- * Copyright (C) 2013-2015 Oracle Corporation
+ * Copyright (C) 2013-2016 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
