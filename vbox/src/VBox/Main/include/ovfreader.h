3bffd16da9e522e0d1622a3a5443abb31b4effb2
qzuser <395143360@qq.com>
2022-06-08 23:41:18 +0800

    commit_64971 ------------------------------------------------------------------------ r64971 | vboxsync | 2016-12-21 18:25:17 +0800 (Wed, 21 Dec 2016) | 2 lines
    Main: missing initialization
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/ovfreader.h
+++ vbox/src/VBox/Main/include/ovfreader.h
@@ -1,4 +1,4 @@
-/* $Id: ovfreader.h 63331 2016-08-11 11:56:21Z vboxsync $ */
+/* $Id: ovfreader.h 64971 2016-12-21 10:25:17Z vboxsync $ */
 /** @file
  * VirtualBox Main - OVF reader declarations.
  *
@@ -346,6 +346,7 @@ public:
 
     VirtualHardwareItem()
         : ulInstanceID(0),
+          fResourceRequired(false),
           fAutomaticAllocation(false),
           fAutomaticDeallocation(false),
           ullVirtualQuantity(0),
