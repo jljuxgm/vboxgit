f64dfa5a544cba3c039fde2ef70220f6b8feab10
qzuser <395143360@qq.com>
2022-06-08 13:20:44 +0800

    commit_62928 ------------------------------------------------------------------------ r62928 | vboxsync | 2016-08-03 23:31:34 +0800 (Wed, 03 Aug 2016) | 1 line
    Devices: warnings
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Storage/DrvHostFloppy.cpp
+++ vbox/src/VBox/Devices/Storage/DrvHostFloppy.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostFloppy.cpp 62679 2016-07-29 12:52:10Z vboxsync $ */
+/* $Id: DrvHostFloppy.cpp 62928 2016-08-03 15:31:34Z vboxsync $ */
 /** @file
  *
  * VBox storage devices:
@@ -182,6 +182,7 @@ static DECLCALLBACK(int) drvHostFloppyPoll(PDRVHOSTBASE pThis)
  */
 static DECLCALLBACK(int) drvHostFloppyConstruct(PPDMDRVINS pDrvIns, PCFGMNODE pCfg, uint32_t fFlags)
 {
+    RT_NOREF(fFlags);
     PDRVHOSTFLOPPY pThis = PDMINS_2_DATA(pDrvIns, PDRVHOSTFLOPPY);
     LogFlow(("drvHostFloppyConstruct: iInstance=%d\n", pDrvIns->iInstance));
 
