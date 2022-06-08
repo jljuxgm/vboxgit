b4652f4c7f4b782bedc71ca6a55c51ce58b8a641
qzuser <395143360@qq.com>
2022-06-08 14:09:18 +0800

    commit_63073 ------------------------------------------------------------------------ r63073 | vboxsync | 2016-08-06 06:46:32 +0800 (Sat, 06 Aug 2016) | 1 line
    GA/NT/SharedFolders: warnings
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/WINNT/SharedFolders/driver/file.c
+++ vbox/src/VBox/Additions/WINNT/SharedFolders/driver/file.c
@@ -1,9 +1,6 @@
-/* $Id: file.c 62522 2016-07-22 19:17:25Z vboxsync $ */
+/* $Id: file.c 63073 2016-08-05 22:46:32Z vboxsync $ */
 /** @file
- *
- * VirtualBox Windows Guest Shared Folders
- *
- * File System Driver file routines
+ * VirtualBox Windows Guest Shared Folders - File System Driver file routines.
  */
 
 /*
