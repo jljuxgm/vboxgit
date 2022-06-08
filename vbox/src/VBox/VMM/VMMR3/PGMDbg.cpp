0aa39807d631d93ff325919cab55cb03b8e54c2a
qzuser <395143360@qq.com>
2022-06-09 02:19:14 +0800

    commit_65469 ------------------------------------------------------------------------ r65469 | vboxsync | 2017-01-27 05:40:08 +0800 (Fri, 27 Jan 2017) | 1 line
    PGM: pgmLogState.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR3/PGMDbg.cpp
+++ vbox/src/VBox/VMM/VMMR3/PGMDbg.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMDbg.cpp 65468 2017-01-26 21:39:49Z vboxsync $ */
+/* $Id: PGMDbg.cpp 65469 2017-01-26 21:40:08Z vboxsync $ */
 /** @file
  * PGM - Page Manager and Monitor - Debugger & Debugging APIs.
  */
@@ -2485,7 +2485,7 @@ VMMR3_INT_DECL(int) PGMR3DumpHierarchyGst(PVM pVM, uint64_t cr3, uint32_t fFlags
  */
 void pgmLogState(PVM pVM)
 {
-#if 1
+#if 0
     RTLogRelPrintf("\npgmLogState pgmLogState pgmLogState pgmLogState pgmLogState\n");
 
     /*
