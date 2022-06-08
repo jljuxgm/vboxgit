da27dace3efb5e5dd285886d0004ade86966f451
qzuser <395143360@qq.com>
2022-06-09 05:56:02 +0800

    commit_66450 ------------------------------------------------------------------------ r66450 | vboxsync | 2017-04-06 03:06:04 +0800 (Thu, 06 Apr 2017) | 1 line
    IEM: More vstmxcsr tests.
    ------------------------------------------------------------------------

--- vbox/src/VBox/ValidationKit/bootsectors/bs3-cpu-generated-1.h
+++ vbox/src/VBox/ValidationKit/bootsectors/bs3-cpu-generated-1.h
@@ -1,4 +1,4 @@
-/* $Id: bs3-cpu-generated-1.h 66419 2017-04-04 15:49:07Z vboxsync $ */
+/* $Id: bs3-cpu-generated-1.h 66450 2017-04-05 19:06:04Z vboxsync $ */
 /** @file
  * BS3Kit - bs3-cpu-generated-1, common header file.
  */
@@ -417,6 +417,7 @@ typedef enum BS3CG1DST
     /* Control registers.*/
     BS3CG1DST_CR0,
     BS3CG1DST_CR4,
+    BS3CG1DST_XCR0,
 
     /* FPU registers. */
     BS3CG1DST_FPU_FIRST,
