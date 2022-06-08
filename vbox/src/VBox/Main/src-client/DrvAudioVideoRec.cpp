8fe8aeed4d66e3143218065dd4941ac9206dd87b
qzuser <395143360@qq.com>
2022-06-08 15:20:52 +0800

    commit_63362 ------------------------------------------------------------------------ r63362 | vboxsync | 2016-08-12 22:21:25 +0800 (Fri, 12 Aug 2016) | 1 line
    Audio: PDMPCMPROPS -> PDMAUDIOPCMPROPS.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/DrvAudioVideoRec.cpp
+++ vbox/src/VBox/Main/src-client/DrvAudioVideoRec.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvAudioVideoRec.cpp 62117 2016-07-07 16:16:01Z vboxsync $ */
+/* $Id: DrvAudioVideoRec.cpp 63362 2016-08-12 14:21:25Z vboxsync $ */
 /** @file
  * Video recording audio backend for Main.
  */
@@ -129,7 +129,7 @@ static DECLCALLBACK(int) drvAudioVideoRecInit(PPDMIHOSTAUDIO pInterface)
 }
 
 /** @todo Replace this with drvAudioHlpPcmPropsFromCfg(). */
-static int drvAudioVideoRecPcmInitInfo(PDMPCMPROPS * pProps, PPDMAUDIOSTREAMCFG as)
+static int drvAudioVideoRecPcmInitInfo(PDMAUDIOPCMPROPS * pProps, PPDMAUDIOSTREAMCFG as)
 {
     int rc = VINF_SUCCESS;
 
