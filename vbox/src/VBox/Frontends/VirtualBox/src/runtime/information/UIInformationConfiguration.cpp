3f0e11b5ef6a5d14465e9664aed55ca749e4f196
qzuser <395143360@qq.com>
2022-06-09 01:51:16 +0800

    commit_65337 ------------------------------------------------------------------------ r65337 | vboxsync | 2017-01-16 22:05:31 +0800 (Mon, 16 Jan 2017) | 1 line
    FE/Qt: bugref:6899: Accessibility support (step 179): A bit of care for UIInformationItem: Refactoring.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/information/UIInformationConfiguration.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/information/UIInformationConfiguration.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIInformationConfiguration.cpp 65274 2017-01-13 08:13:30Z vboxsync $ */
+/* $Id: UIInformationConfiguration.cpp 65337 2017-01-16 14:05:31Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIInformationConfiguration class implementation.
  */
@@ -25,6 +25,7 @@
 
 /* GUI includes: */
 # include "UIInformationConfiguration.h"
+# include "UIInformationDataItem.h"
 # include "UIInformationItem.h"
 # include "UIInformationView.h"
 # include "UIExtraDataManager.h"
