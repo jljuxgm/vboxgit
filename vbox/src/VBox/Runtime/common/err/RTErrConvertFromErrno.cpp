1c83dd24de788dc97630118ded2cd4e8aee4cae2
qzuser <395143360@qq.com>
2022-06-09 07:25:39 +0800

    commit_66863 ------------------------------------------------------------------------ r66863 | vboxsync | 2017-05-10 21:11:58 +0800 (Wed, 10 May 2017) | 2 lines
    build fix
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/err/RTErrConvertFromErrno.cpp
+++ vbox/src/VBox/Runtime/common/err/RTErrConvertFromErrno.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTErrConvertFromErrno.cpp 66862 2017-05-10 13:02:36Z vboxsync $ */
+/* $Id: RTErrConvertFromErrno.cpp 66863 2017-05-10 13:11:58Z vboxsync $ */
 /** @file
  * IPRT - Convert errno to iprt status codes.
  */
@@ -33,6 +33,7 @@
 
 #include <iprt/assert.h>
 #include <iprt/errno.h>
+#include <iprt/log.h>
 
 
 RTDECL(int)  RTErrConvertFromErrno(unsigned uNativeCode)
