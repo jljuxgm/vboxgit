e19edf591994c5e10d89e872dd6bf2a6ae553f69
qzuser <395143360@qq.com>
2022-06-08 19:25:34 +0800

    commit_64253 ------------------------------------------------------------------------ r64253 | vboxsync | 2016-10-13 22:22:17 +0800 (Thu, 13 Oct 2016) | 1 line
    Windows build fix
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Storage/DrvHostBase-win.cpp
+++ vbox/src/VBox/Devices/Storage/DrvHostBase-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostBase-win.cpp 64252 2016-10-13 14:20:01Z vboxsync $ */
+/* $Id: DrvHostBase-win.cpp 64253 2016-10-13 14:22:17Z vboxsync $ */
 /** @file
  * DrvHostBase - Host base drive access driver, Windows specifics.
  */
@@ -250,12 +250,6 @@ DECLHIDDEN(int) drvHostBasePollerWakeupOs(PDRVHOSTBASE pThis)
 
 DECLHIDDEN(void) drvHostBaseDestructOs(PDRVHOSTBASE pThis)
 {
-    if (pThis->EventPoller != NULL)
-    {
-        RTSemEventDestroy(pThis->EventPoller);
-        pThis->EventPoller = NULL;
-    }
-
     if (pThis->hwndDeviceChange)
     {
         if (SetWindowLongPtr(pThis->hwndDeviceChange, GWLP_USERDATA, 0) == (LONG_PTR)pThis)
