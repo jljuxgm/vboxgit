2d8da71874952b3fd52d2b6370474a1cfd6abcab
qzuser <395143360@qq.com>
2022-06-09 04:17:54 +0800

    commit_66000 ------------------------------------------------------------------------ r66000 | vboxsync | 2017-03-09 04:29:40 +0800 (Thu, 09 Mar 2017) | 1 line
    VMM: Nested Hw.virt: Preps for SVM vmrun/#VMEXIT impl.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMAll/HMAll.cpp
+++ vbox/src/VBox/VMM/VMMAll/HMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMAll.cpp 65989 2017-03-07 21:36:03Z vboxsync $ */
+/* $Id: HMAll.cpp 66000 2017-03-08 20:29:40Z vboxsync $ */
 /** @file
  * HM - All contexts.
  */
@@ -554,19 +554,6 @@ VMM_INT_DECL(void) HMTrapXcptUDForGIMDisable(PVMCPU pVCpu)
 {
     pVCpu->hm.s.fGIMTrapXcptUD = false;
     HMCPU_CF_SET(pVCpu, HM_CHANGED_GUEST_XCPT_INTERCEPTS);
-}
-
-
-
-/**
- * SVM nested-guest \#VMEXIT handler.
- * 
- * @param   pVCpu       The cross context virtual CPU structure. 
- * @param   uExitCode   The exit reason.
- */
-VMM_INT_DECL(void) HMNstGstSvmVmExit(PVMCPU pVCpu, uint64_t uExitCode)
-{
-    RT_NOREF2(pVCpu, uExitCode);
 }
 
 
