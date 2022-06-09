7ffe7574093015bd6b1e18d850b43cbe3881b626
qzuser <395143360@qq.com>
2022-06-09 15:02:03 +0800

    commit_68670 ------------------------------------------------------------------------ r68670 | vboxsync | 2017-09-06 17:02:20 +0800 (Wed, 06 Sep 2017) | 1 line
    include file build fixes (kmk -C include)
    ------------------------------------------------------------------------

--- vbox/include/VBox/VBoxNetCfg-win.h
+++ vbox/include/VBox/VBoxNetCfg-win.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxNetCfg-win.h 62688 2016-07-29 13:30:27Z vboxsync $ */
+/* $Id: VBoxNetCfg-win.h 68670 2017-09-06 09:02:20Z vboxsync $ */
 /** @file
  * Network Configuration API for Windows platforms.
  */
@@ -41,6 +41,7 @@
 #include <Netcfgn.h>
 #include <iprt/win/Setupapi.h>
 #include <VBox/cdefs.h>
+#include <iprt/types.h>
 
 /** @defgroup grp_vboxnetcfgwin     The Windows Network Configration Library
  * @{ */
