ee44b32f879b04182ada049c87f7797cb4c890e1
qzuser <395143360@qq.com>
2022-06-09 16:55:43 +0800

    commit_69105 ------------------------------------------------------------------------ r69105 | vboxsync | 2017-10-17 18:20:49 +0800 (Tue, 17 Oct 2017) | 1 line
    include/iprt/: (C) year
    ------------------------------------------------------------------------

--- vbox/include/iprt/formats/elf.h
+++ vbox/include/iprt/formats/elf.h
@@ -1,10 +1,10 @@
-/* $Id: elf.h 62474 2016-07-22 18:16:43Z vboxsync $ */
+/* $Id: elf.h 69105 2017-10-17 10:20:49Z vboxsync $ */
 /** @file
  * ELF types, current architecture.
  */
 
 /*
- * Copyright (C) 2010-2016 Oracle Corporation
+ * Copyright (C) 2010-2017 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
--- vbox/include/iprt/s3.h
+++ vbox/include/iprt/s3.h
@@ -1,10 +1,10 @@
-/* $Id: s3.h 62473 2016-07-22 18:15:28Z vboxsync $ */
+/* $Id: s3.h 69105 2017-10-17 10:20:49Z vboxsync $ */
 /** @file
  * IPRT - Simple Storage Service (S3) Communication API.
  */
 
 /*
- * Copyright (C) 2009-2016 Oracle Corporation
+ * Copyright (C) 2009-2017 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
