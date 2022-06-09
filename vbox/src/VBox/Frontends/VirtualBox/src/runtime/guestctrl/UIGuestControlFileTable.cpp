2c3c541cabb11571700034871899524de142b07e
qzuser <395143360@qq.com>
2022-06-10 03:20:06 +0800

    commit_71352 ------------------------------------------------------------------------ r71352 | vboxsync | 2018-03-15 22:09:01 +0800 (Thu, 15 Mar 2018) | 1 line
    FE/Qt bugref:6699 Burn fix
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/guestctrl/UIGuestControlFileTable.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/guestctrl/UIGuestControlFileTable.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGuestControlFileTable.cpp 71350 2018-03-15 13:22:58Z vboxsync $ */
+/* $Id: UIGuestControlFileTable.cpp 71352 2018-03-15 14:09:01Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIGuestControlFileTable class implementation.
  */
@@ -1213,6 +1213,7 @@ FileObjectType UIGuestFileTable::getFileType(const CFsObjInfo &fsInfo)
 
     return FileObjectType_Other;
 }
+
 
 /*********************************************************************************************************************************
 *   UIHostFileTable implementation.                                                                                              *
