13105a3fc0a93777aaf78468a7e2a3ae18696170
qzuser <395143360@qq.com>
2022-06-09 17:43:02 +0800

    commit_69308 ------------------------------------------------------------------------ r69308 | vboxsync | 2017-10-25 21:51:16 +0800 (Wed, 25 Oct 2017) | 1 line
    VBoxGuest: scm updates
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibInternal.h
+++ vbox/src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR3LibInternal.h 68652 2017-09-05 14:37:04Z vboxsync $ */
+/* $Id: VBoxGuestR3LibInternal.h 69308 2017-10-25 13:51:16Z vboxsync $ */
 /** @file
  * VBoxGuestR3Lib - Ring-3 support library for the guest additions, Internal header.
  */
@@ -113,5 +113,4 @@ DECLINLINE(void) VbglHGCMParmPtrSetString(HGCMFunctionParameter *pParm, const ch
 RT_C_DECLS_END
 
 #endif
-
 
