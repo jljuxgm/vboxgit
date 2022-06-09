8ee8b208813f157a8c7bb8513c3fc67c0da7bbea
qzuser <395143360@qq.com>
2022-06-09 15:06:43 +0800

    commit_68688 ------------------------------------------------------------------------ r68688 | vboxsync | 2017-09-07 02:18:29 +0800 (Thu, 07 Sep 2017) | 1 line
    VBoxGuest*.h,VMMDev*.h: use assertcompile.h instead of assert.h.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuest-win-pnp.cpp
+++ vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuest-win-pnp.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuest-win-pnp.cpp 68654 2017-09-05 17:22:12Z vboxsync $ */
+/* $Id: VBoxGuest-win-pnp.cpp 68688 2017-09-06 18:18:29Z vboxsync $ */
 /** @file
  * VBoxGuest-win-pnp - Windows Plug'n'Play specifics.
  */
@@ -25,6 +25,7 @@
 #include <VBox/log.h>
 #include <VBox/version.h>
 #include <VBox/VBoxGuestLib.h>
+#include <iprt/assert.h>
 
 
 /*********************************************************************************************************************************
