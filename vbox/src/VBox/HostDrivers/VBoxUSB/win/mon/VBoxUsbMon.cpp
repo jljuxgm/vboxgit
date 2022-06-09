bb383619d5ae55132b1fcfda1ae1197b133dbba8
qzuser <395143360@qq.com>
2022-06-09 22:05:09 +0800

    commit_70294 ------------------------------------------------------------------------ r70294 | vboxsync | 2017-12-22 00:42:22 +0800 (Fri, 22 Dec 2017) | 1 line
    Don't need stdio.h that much.
    ------------------------------------------------------------------------

--- vbox/src/VBox/HostDrivers/VBoxUSB/win/mon/VBoxUsbMon.cpp
+++ vbox/src/VBox/HostDrivers/VBoxUSB/win/mon/VBoxUsbMon.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxUsbMon.cpp 69500 2017-10-28 15:14:05Z vboxsync $ */
+/* $Id: VBoxUsbMon.cpp 70294 2017-12-21 16:42:22Z vboxsync $ */
 /** @file
  * VBox USB Monitor
  */
@@ -33,7 +33,6 @@
 #include <vbox/err.h>
 #include <VBox/usblib.h>
 #include <excpt.h>
-#include <stdio.h>
 
 
 /*********************************************************************************************************************************
