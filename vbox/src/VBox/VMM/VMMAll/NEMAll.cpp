232922fda33d810c7c4c99e9c63679d34a57a413
qzuser <395143360@qq.com>
2022-06-10 07:32:50 +0800

    commit_72546 ------------------------------------------------------------------------ r72546 | vboxsync | 2018-06-13 23:45:39 +0800 (Wed, 13 Jun 2018) | 1 line
    NEM/win,TM: Setting TSC on TM start/restore/resume/etc as best we can. bugref:9044
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMAll/NEMAll.cpp
+++ vbox/src/VBox/VMM/VMMAll/NEMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: NEMAll.cpp 72522 2018-06-12 08:45:27Z vboxsync $ */
+/* $Id: NEMAll.cpp 72546 2018-06-13 15:45:39Z vboxsync $ */
 /** @file
  * NEM - Native execution manager, R0 and R3 context code.
  */
@@ -130,10 +130,21 @@ VMM_INT_DECL(int) NEMImportStateOnDemand(PVMCPU pVCpu, PCPUMCTX pCtx, uint64_t f
 }
 #endif
 
+
 #ifndef VBOX_WITH_NATIVE_NEM
 VMM_INT_DECL(int) NEMHCQueryCpuTick(PVMCPU pVCpu, uint64_t *pcTicks, uint32_t *puAux)
 {
     RT_NOREF(pVCpu, pcTicks, puAux);
+    AssertFailed();
+    return VERR_NEM_IPE_9;
+}
+#endif
+
+
+#ifndef VBOX_WITH_NATIVE_NEM
+VMM_INT_DECL(int) NEMHCResumeCpuTickOnAll(PVM pVM, PVMCPU pVCpu, uint64_t uPausedTscValue)
+{
+    RT_NOREF(pVM, pVCpu, uPausedTscValue);
     AssertFailed();
     return VERR_NEM_IPE_9;
 }
