996ac328d49847e06d5018d70081e2a13f64af1c
qzuser <395143360@qq.com>
2022-06-09 14:56:46 +0800

    commit_68651 ------------------------------------------------------------------------ r68651 | vboxsync | 2017-09-05 22:36:21 +0800 (Tue, 05 Sep 2017) | 1 line
    VBGLR3Internal.h -> VBoxGuestR3LibInternal.h
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibInternal.h
+++ vbox/src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuestR3LibInternal.h 68650 2017-09-05 14:34:23Z vboxsync $ */
+/* $Id: VBoxGuestR3LibInternal.h 68651 2017-09-05 14:36:21Z vboxsync $ */
 /** @file
  * VBoxGuestR3Lib - Ring-3 support library for the guest additions, Internal header.
  */
@@ -24,8 +24,8 @@
  * terms and conditions of either the GPL or the CDDL or both.
  */
 
-#ifndef ___VBGLR3Internal_h
-#define ___VBGLR3Internal_h
+#ifndef ___VBoxGuestR3LibInternal_h
+#define ___VBoxGuestR3LibInternal_h
 
 #if defined(RT_OS_WINDOWS) /** @todo explain why this is here. */
 # include <iprt/win/windows.h>
@@ -116,4 +116,5 @@ DECLINLINE(void) VbglHGCMParmPtrSetString(HGCMFunctionParameter *pParm, const ch
 RT_C_DECLS_END
 
 #endif
+
 
