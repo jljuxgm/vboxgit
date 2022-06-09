d89c972aa851e8dfc13a67a23a906e4320a2b6a7
qzuser <395143360@qq.com>
2022-06-09 20:40:21 +0800

    commit_70045 ------------------------------------------------------------------------ r70045 | vboxsync | 2017-12-09 09:12:43 +0800 (Sat, 09 Dec 2017) | 2 lines
    VBoxGuest-netbsd.c: fix spurious trailing whitespace in previous.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuest-netbsd.c
+++ vbox/src/VBox/Additions/common/VBoxGuest/VBoxGuest-netbsd.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxGuest-netbsd.c 70044 2017-12-09 00:52:16Z vboxsync $ */
+/* $Id: VBoxGuest-netbsd.c 70045 2017-12-09 01:12:43Z vboxsync $ */
 /** @file
  * VirtualBox Guest Additions Driver for NetBSD.
  */
@@ -583,7 +583,7 @@ VBoxGuestNetBSDWsmIOCtl(void *cookie, u_long cmd, void *data, int flag, struct l
     case WSMOUSEIO_GCALIBCOORDS:
         return tpcalib_ioctl(&sc->sc_tpcalib, cmd, data, flag, l);
 
-    default: 
+    default:
         return EPASSTHROUGH;
     }
     return 0;
