22e75c2f4691e77529e1c4a8b620533c2514c7f2
qzuser <395143360@qq.com>
2022-06-10 06:52:14 +0800

    commit_72355 ------------------------------------------------------------------------ r72355 | vboxsync | 2018-05-26 21:28:34 +0800 (Sat, 26 May 2018) | 1 line
    Main, VMMDev: implemented IDisplay::SetScreenLayout, VMMDev multimonitor resize request and VBoxManage controlvm setscreenlayout (another build fix). bugref:8393
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxManage/VBoxManageControlVM.cpp
+++ vbox/src/VBox/Frontends/VBoxManage/VBoxManageControlVM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageControlVM.cpp 72352 2018-05-26 12:37:50Z vboxsync $ */
+/* $Id: VBoxManageControlVM.cpp 72355 2018-05-26 13:28:34Z vboxsync $ */
 /** @file
  * VBoxManage - Implementation of the controlvm command.
  */
@@ -1491,7 +1491,7 @@ RTEXITCODE handleControlVM(HandlerArg *a)
                 ULONG aWidth = 0;
                 ULONG aHeight = 0;
                 ULONG aBitsPerPixel = 0;
-                if (aStatus = GuestMonitorStatus_Enabled)
+                if (aStatus == GuestMonitorStatus_Enabled)
                 {
                     if (argc < 7)
                     {
