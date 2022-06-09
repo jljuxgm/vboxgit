f1a43f0788a31cd8a4eed5c256520b3c257c7a72
qzuser <395143360@qq.com>
2022-06-09 17:40:56 +0800

    commit_69298 ------------------------------------------------------------------------ r69298 | vboxsync | 2017-10-25 20:56:50 +0800 (Wed, 25 Oct 2017) | 1 line
    Devices/Network: scm updates
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Network/DevE1000.cpp
+++ vbox/src/VBox/Devices/Network/DevE1000.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevE1000.cpp 69001 2017-10-06 12:12:04Z vboxsync $ */
+/* $Id: DevE1000.cpp 69298 2017-10-25 12:56:50Z vboxsync $ */
 /** @file
  * DevE1000 - Intel 82540EM Ethernet Controller Emulation.
  *
@@ -4429,7 +4429,7 @@ static int e1kFallbackAddToFrame(PE1KSTATE pThis, E1KTXDESC *pDesc, bool fOnWork
         e1kXmitFreeBuf(pThis);
     }
 
-    return VINF_SUCCESS; // @todo consider rc;
+    return VINF_SUCCESS; /// @todo consider rc;
 }
 #endif /* E1K_WITH_TXD_CACHE */
 
