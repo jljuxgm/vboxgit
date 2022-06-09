dc72ab41480b2ce16bfc2985565bccdb87b23c47
qzuser <395143360@qq.com>
2022-06-09 10:39:01 +0800

    commit_67704 ------------------------------------------------------------------------ r67704 | vboxsync | 2017-06-30 00:12:28 +0800 (Fri, 30 Jun 2017) | 1 line
    FE/Qt: bugref:8400: Virtual Media Manager: A bit of rework (and more to follow).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.h
@@ -1,4 +1,4 @@
-/* $Id: UIMediumManager.h 67573 2017-06-23 08:59:10Z vboxsync $ */
+/* $Id: UIMediumManager.h 67704 2017-06-29 16:12:28Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIMediumManager class declaration.
  */
@@ -24,17 +24,17 @@
 #include "UIMediumDefs.h"
 
 /* Forward declarations: */
+class QFrame;
+class QLabel;
+class QTabWidget;
+class QTreeWidgetItem;
+class QIDialogButtonBox;
+class QILabel;
+class QITreeWidget;
+class UIEnumerationProgressBar;
 class UIMedium;
 class UIMediumItem;
 class UIToolBar;
-class UIEnumerationProgressBar;
-class QTabWidget;
-class QITreeWidget;
-class QTreeWidgetItem;
-class QFrame;
-class QLabel;
-class QILabel;
-class QIDialogButtonBox;
 
 
 /** Functor interface allowing to check if passed UIMediumItem is suitable. */
