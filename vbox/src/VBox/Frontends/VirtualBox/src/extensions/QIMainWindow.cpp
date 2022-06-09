a8c334b119bc87bdb2c5a7eb03710aadf1db4a18
qzuser <395143360@qq.com>
2022-06-10 03:29:41 +0800

    commit_71400 ------------------------------------------------------------------------ r71400 | vboxsync | 2018-03-20 21:17:08 +0800 (Tue, 20 Mar 2018) | 1 line
    FE/Qt: bugref:9049: Tiny cleanup for QIMainWindow.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/extensions/QIMainWindow.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/extensions/QIMainWindow.cpp
@@ -1,10 +1,10 @@
-/* $Id: QIMainWindow.cpp 71027 2018-02-15 14:33:48Z vboxsync $ */
+/* $Id: QIMainWindow.cpp 71400 2018-03-20 13:17:08Z vboxsync $ */
 /** @file
  * VBox Qt GUI - QIMainWindow class implementation.
  */
 
 /*
- * Copyright (C) 2016-2017 Oracle Corporation
+ * Copyright (C) 2016-2018 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
