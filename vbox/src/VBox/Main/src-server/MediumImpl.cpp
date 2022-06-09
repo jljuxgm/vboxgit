98243bbef6cabe2bd17acab152df4556551eb435
qzuser <395143360@qq.com>
2022-06-09 11:58:48 +0800

    commit_68043 ------------------------------------------------------------------------ r68043 | vboxsync | 2017-07-19 19:51:10 +0800 (Wed, 19 Jul 2017) | 2 lines
    Main/Medium: fix lock order violation when setting the medium description
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-server/MediumImpl.cpp
+++ vbox/src/VBox/Main/src-server/MediumImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: MediumImpl.cpp 67885 2017-07-10 16:45:06Z vboxsync $ */
+/* $Id: MediumImpl.cpp 68043 2017-07-19 11:51:10Z vboxsync $ */
 /** @file
  * VirtualBox COM class implementation
  */
@@ -1698,6 +1698,7 @@ HRESULT Medium::setDescription(const com::Utf8Str &aDescription)
         }
 
         // save the settings
+        alock.release();
         i_markRegistriesModified();
         m->pVirtualBox->i_saveModifiedRegistries();
     }
