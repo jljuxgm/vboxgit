e2206ed1f9405136354168fc87d139fd81b8b722
qzuser <395143360@qq.com>
2022-06-10 02:12:47 +0800

    commit_71051 ------------------------------------------------------------------------ r71051 | vboxsync | 2018-02-19 20:32:43 +0800 (Mon, 19 Feb 2018) | 1 line
    FE/Qt bugref:6699 Fix windows builds
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/information/guestctrl/UIGuestControlInterface.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/information/guestctrl/UIGuestControlInterface.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIGuestControlInterface.cpp 71049 2018-02-19 12:16:09Z vboxsync $ */
+/* $Id: UIGuestControlInterface.cpp 71051 2018-02-19 12:32:43Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIGuestControlInterface class implementation.
  */
@@ -54,7 +54,7 @@
         commandData.m_strPassword = ValueUnion.psz;   \
         break;
 
-QString generateErrorString(int getOptErrorCode, const RTGETOPTUNION &valueUnion)
+QString generateErrorString(int getOptErrorCode, const RTGETOPTUNION &/*valueUnion*/)
 {
     QString errorString;
     // if (valueUnion.pDef)
