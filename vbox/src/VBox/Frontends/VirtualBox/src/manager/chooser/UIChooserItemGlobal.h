801e1c435408880b73f51ed4129250cb005798c8
qzuser <395143360@qq.com>
2022-06-11 06:03:51 +0800

    commit_77805 ------------------------------------------------------------------------ r77805 | vboxsync | 2019-03-20 19:09:52 +0800 (Wed, 20 Mar 2019) | 1 line
    FE/Qt: bugref:9241: VirtualBox Manager: Chooser pane: Reworking margins and indents. This should prevent right margin from overlapping with scroll-bar and make group indent more obvious, so that each grouped VM is now indented on half of own icon size comparing to VMs of group above.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGlobal.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGlobal.h
@@ -1,4 +1,4 @@
-/* $Id: UIChooserItemGlobal.h 77638 2019-03-10 19:21:30Z vboxsync $ */
+/* $Id: UIChooserItemGlobal.h 77805 2019-03-20 11:09:52Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIChooserItemGlobal class declaration.
  */
@@ -167,7 +167,9 @@ private:
     enum GlobalItemData
     {
         /* Layout hints: */
-        GlobalItemData_Margin,
+        GlobalItemData_MarginHL,
+        GlobalItemData_MarginHR,
+        GlobalItemData_MarginV,
         GlobalItemData_Spacing,
         GlobalItemData_ButtonMargin,
     };
