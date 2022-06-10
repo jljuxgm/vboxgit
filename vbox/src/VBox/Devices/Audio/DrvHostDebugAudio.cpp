d33ce97695132bee318a809ffa263f335798ba00
qzuser <395143360@qq.com>
2022-06-10 11:11:48 +0800

    commit_73454 ------------------------------------------------------------------------ r73454 | vboxsync | 2018-08-02 20:48:47 +0800 (Thu, 02 Aug 2018) | 1 line
    Audio/Debug: This backend does not support any recording (injecting input data) yet, so tell the others as well.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Audio/DrvHostDebugAudio.cpp
+++ vbox/src/VBox/Devices/Audio/DrvHostDebugAudio.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostDebugAudio.cpp 73393 2018-07-30 12:10:55Z vboxsync $ */
+/* $Id: DrvHostDebugAudio.cpp 73454 2018-08-02 12:48:47Z vboxsync $ */
 /** @file
  * Debug audio driver.
  *
@@ -73,8 +73,8 @@ static DECLCALLBACK(int) drvHostDebugAudioGetConfig(PPDMIHOSTAUDIO pInterface, P
     pBackendCfg->cbStreamOut    = sizeof(DEBUGAUDIOSTREAM);
     pBackendCfg->cbStreamIn     = sizeof(DEBUGAUDIOSTREAM);
 
-    pBackendCfg->cMaxStreamsOut = 1; /* Output */
-    pBackendCfg->cMaxStreamsIn  = 2; /* Line input + microphone input. */
+    pBackendCfg->cMaxStreamsOut = 1; /* Output; writing to a file. */
+    pBackendCfg->cMaxStreamsIn  = 0; /** @todo Right now we don't support any input (capturing, injecting from a file). */
 
     return VINF_SUCCESS;
 }
