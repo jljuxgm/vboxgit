9f914126963c476842fc290adacfddef575c395d
qzuser <395143360@qq.com>
2022-06-10 16:58:27 +0800

    commit_74768 ------------------------------------------------------------------------ r74768 | vboxsync | 2018-10-11 20:18:33 +0800 (Thu, 11 Oct 2018) | 1 line
    IPRT/ldr/asn1/pkcs7: Ironed out issues in decoding indefinite ASN.1 length records and successfully verified the first Mach-O signature.  bugref:9232 [build fix]
    ------------------------------------------------------------------------

--- vbox/src/VBox/HostDrivers/Support/win/SUPHardenedVerifyImage-win.cpp
+++ vbox/src/VBox/HostDrivers/Support/win/SUPHardenedVerifyImage-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: SUPHardenedVerifyImage-win.cpp 74760 2018-10-11 11:25:24Z vboxsync $ */
+/* $Id: SUPHardenedVerifyImage-win.cpp 74768 2018-10-11 12:18:33Z vboxsync $ */
 /** @file
  * VirtualBox Support Library/Driver - Hardened Image Verification, Windows.
  */
@@ -1055,7 +1055,7 @@ static DECLCALLBACK(int) supHardNtViCallback(RTLDRMOD hLdrMod, RTLDRSIGNATURETYP
                                              void const *pvExternalData, size_t cbExternalData,
                                              PRTERRINFO pErrInfo, void *pvUser)
 {
-    RT_NOREF2(hLdrMod, enmSignature);
+    RT_NOREF(hLdrMod, enmSignature, pvExternalData, cbExternalData);
 
     /*
      * Check out the input.
