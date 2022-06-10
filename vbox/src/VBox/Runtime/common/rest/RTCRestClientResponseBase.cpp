dd2b87bed53888f34d54b2d81fa2f2dd7a455a80
qzuser <395143360@qq.com>
2022-06-10 13:05:50 +0800

    commit_73923 ------------------------------------------------------------------------ r73923 | vboxsync | 2018-08-28 15:44:45 +0800 (Tue, 28 Aug 2018) | 1 line
    iprt/rest: Added error info, copy of http status, and content type to RTCRestClientResponseBase.  bugref:9167
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/rest/RTCRestClientResponseBase.cpp
+++ vbox/src/VBox/Runtime/common/rest/RTCRestClientResponseBase.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTCRestClientResponseBase.cpp 73922 2018-08-28 07:30:28Z vboxsync $ */
+/* $Id: RTCRestClientResponseBase.cpp 73923 2018-08-28 07:44:45Z vboxsync $ */
 /** @file
  * IPRT - C++ REST, RTCRestClientResponseBase implementation.
  */
@@ -60,8 +60,8 @@ RTCRestClientResponseBase::~RTCRestClientResponseBase()
 RTCRestClientResponseBase::RTCRestClientResponseBase(RTCRestClientResponseBase const &a_rThat)
     : m_rcStatus(a_rThat.m_rcStatus)
     , m_rcHttp(a_rThat.m_rcHttp)
-    , m_strContentType(a_rThat.m_strContentType)
     , m_pErrInfo(NULL)
+    , m_strContentType(a_rThat.m_strContentType)
 {
     if (m_pErrInfo)
         copyErrInfo(m_pErrInfo);
