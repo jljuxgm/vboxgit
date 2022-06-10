72ca85fb98b5f1b82688114009bef681afd515d7
qzuser <395143360@qq.com>
2022-06-10 12:02:25 +0800

    commit_73673 ------------------------------------------------------------------------ r73673 | vboxsync | 2018-08-15 02:43:35 +0800 (Wed, 15 Aug 2018) | 1 line
    FE/Qt: VirtualBox Manager UI: Update actions on startup and on current tool type change.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.h
@@ -1,4 +1,4 @@
-/* $Id: UIVirtualBoxManager.h 73492 2018-08-03 15:30:44Z vboxsync $ */
+/* $Id: UIVirtualBoxManager.h 73673 2018-08-14 18:43:35Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIVirtualBoxManager class declaration.
  */
@@ -111,6 +111,9 @@ private slots:
         void sltHandleChooserPaneIndexChange();
         /** Handles signal about group saving progress change. */
         void sltHandleGroupSavingProgressChange();
+
+        /** Handles singal about Tool type change.  */
+        void sltHandleToolTypeChange();
     /** @} */
 
     /** @name CVirtualBox event handling stuff.
