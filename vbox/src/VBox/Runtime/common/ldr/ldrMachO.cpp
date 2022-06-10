0b20d928978da180bd8be093414160406f9a3021
qzuser <395143360@qq.com>
2022-06-10 16:29:14 +0800

    commit_74641 ------------------------------------------------------------------------ r74641 | vboxsync | 2018-10-07 03:28:12 +0800 (Sun, 07 Oct 2018) | 1 line
    IPRT: More adjustments to the LX and Mach-O loader code from kStuff. bugref:9232
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/ldr/ldrMachO.cpp
+++ vbox/src/VBox/Runtime/common/ldr/ldrMachO.cpp
@@ -1,4 +1,4 @@
-/* $Id: ldrMachO.cpp 74640 2018-10-06 19:14:10Z vboxsync $ */
+/* $Id: ldrMachO.cpp 74641 2018-10-06 19:28:12Z vboxsync $ */
 /** @file
  * kLdr - The Module Interpreter for the MACH-O format.
  */
@@ -2291,7 +2291,6 @@ static int kldrModMachOQueryMainEntrypoint(PRTLDRMODINTERNAL pMod, const void *p
 
 #endif
 
-/** @copydoc kLdrModQueryImageUuid */
 static int kldrModMachOQueryImageUuid(PKLDRMODMACHO pThis, const void *pvBits, void *pvUuid, size_t cbUuid)
 {
     RT_NOREF(pvBits);
@@ -3684,7 +3683,7 @@ static DECLCALLBACK(int) rtldrMachO_LinkAddressToSegOffset(PRTLDRMODINTERNAL pMo
 
 
 /**
- * @interface_method_impl{RTLDROPS,pfnLinkAddressToRva}.
+ * @interface_method_impl{RTLDROPS,pfnLinkAddressToRva}
  */
 static DECLCALLBACK(int) rtldrMachO_LinkAddressToRva(PRTLDRMODINTERNAL pMod, RTLDRADDR LinkAddress, PRTLDRADDR pRva)
 {
