97f09d5175b9de1da5febf11cc0935629ac150d7
qzuser <395143360@qq.com>
2022-06-10 10:34:32 +0800

    commit_73293 ------------------------------------------------------------------------ r73293 | vboxsync | 2018-07-21 23:11:53 +0800 (Sat, 21 Jul 2018) | 3 lines
    VMM, SUPDrv: Nested VMX: bugref:9180 Read VMX true control MSRs, dump them. Remove pVM->hm.cpuid as we for a long time now
    have cpum.ro.HostFeatures available. Related cleanups and simplifications.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMAll/HMAll.cpp
+++ vbox/src/VBox/VMM/VMMAll/HMAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMAll.cpp 73266 2018-07-20 14:27:20Z vboxsync $ */
+/* $Id: HMAll.cpp 73293 2018-07-21 15:11:53Z vboxsync $ */
 /** @file
  * HM - All contexts.
  */
@@ -362,7 +362,7 @@ VMM_INT_DECL(bool) HMAreMsrBitmapsAvailable(PVM pVM)
             return true;
 
         if (   pVM->hm.s.vmx.fSupported
-            && (pVM->hm.s.vmx.Msrs.VmxProcCtls.n.allowed1 & VMX_VMCS_CTRL_PROC_EXEC_USE_MSR_BITMAPS))
+            && (pVM->hm.s.vmx.Msrs.ProcCtls.n.allowed1 & VMX_VMCS_CTRL_PROC_EXEC_USE_MSR_BITMAPS))
         {
             return true;
         }
