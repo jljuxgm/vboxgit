145a05407aff17e5516e7a6a4ce689655799f47b
qzuser <395143360@qq.com>
2022-06-11 00:43:19 +0800

    commit_76354 ------------------------------------------------------------------------ r76354 | vboxsync | 2018-12-22 09:12:35 +0800 (Sat, 22 Dec 2018) | 1 line
    *: string.h sans err.h fix.  bugref:9344
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/generic/memsafer-generic.cpp
+++ vbox/src/VBox/Runtime/generic/memsafer-generic.cpp
@@ -1,4 +1,4 @@
-/* $Id: memsafer-generic.cpp 69111 2017-10-17 14:26:02Z vboxsync $ */
+/* $Id: memsafer-generic.cpp 76354 2018-12-22 01:12:35Z vboxsync $ */
 /** @file
  * IPRT - Memory Allocate for Sensitive Data, generic heap-based implementation.
  */
@@ -32,6 +32,7 @@
 #include <iprt/memsafer.h>
 
 #include <iprt/assert.h>
+#include <iprt/err.h>
 #include <iprt/string.h>
 
 
