4b1ddc5c7a8232cee323c739f4618ef80b54f70f
qzuser <395143360@qq.com>
2022-06-10 10:48:52 +0800

    commit_73357 ------------------------------------------------------------------------ r73357 | vboxsync | 2018-07-26 00:05:24 +0800 (Thu, 26 Jul 2018) | 1 line
    DBGFR3BugCheck: file description fix (was renamed).
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR3/DBGFR3BugCheck.cpp
+++ vbox/src/VBox/VMM/VMMR3/DBGFR3BugCheck.cpp
@@ -1,6 +1,6 @@
-/* $Id: DBGFR3BugCheck.cpp 73353 2018-07-25 15:20:40Z vboxsync $ */
+/* $Id: DBGFR3BugCheck.cpp 73357 2018-07-25 16:05:24Z vboxsync $ */
 /** @file
- * DBGF - Debugger Facility, Misc.
+ * DBGF - Debugger Facility, NT Bug Checks.
  */
 
 /*
