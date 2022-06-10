1823b6a23bfa5677c325deae8fabb853c81d9b7e
qzuser <395143360@qq.com>
2022-06-10 10:32:39 +0800

    commit_73285 ------------------------------------------------------------------------ r73285 | vboxsync | 2018-07-21 09:49:30 +0800 (Sat, 21 Jul 2018) | 1 line
    APIC,PDM: Avoid a few more return-to-ring-3 statuses now that we've got LogRel.  Updated related (mostly) docs here and there. bugref:9044
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/include/PGMInternal.h
+++ vbox/src/VBox/VMM/include/PGMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: PGMInternal.h 73277 2018-07-20 18:30:34Z vboxsync $ */
+/* $Id: PGMInternal.h 73285 2018-07-21 01:49:30Z vboxsync $ */
 /** @file
  * PGM - Internal header file.
  */
@@ -3474,7 +3474,7 @@ typedef struct PGM
 
     /** Pointer to the 5 page CR3 content mapping.
      * The first page is always the CR3 (in some form) while the 4 other pages
-     * are used of the PDs in PAE mode. */
+     * are used for the PDs in PAE mode. */
     RTGCPTR                         GCPtrCR3Mapping;
 
     /** @name Intermediate Context
