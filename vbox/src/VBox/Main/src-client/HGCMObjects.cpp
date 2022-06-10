bc8e32b66a593d997aa88f535178fdc6f111bbf9
qzuser <395143360@qq.com>
2022-06-10 20:04:22 +0800

    commit_75541 ------------------------------------------------------------------------ r75541 | vboxsync | 2018-11-17 11:50:40 +0800 (Sat, 17 Nov 2018) | 1 line
    Main/HGCM: Skip the handle stuff for the HGCMMsgCore class, it just add overhead. bugref:9172
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/HGCMObjects.cpp
+++ vbox/src/VBox/Main/src-client/HGCMObjects.cpp
@@ -1,4 +1,4 @@
-/* $Id: HGCMObjects.cpp 75539 2018-11-17 02:35:23Z vboxsync $ */
+/* $Id: HGCMObjects.cpp 75541 2018-11-17 03:50:40Z vboxsync $ */
 /** @file
  * HGCMObjects - Host-Guest Communication Manager objects
  */
@@ -245,7 +245,7 @@ HGCMObject *hgcmObjReference (uint32_t handle, HGCMOBJ_TYPE enmObjType)
     return pObject;
 }
 
-void hgcmObjDereference(HGCMReferencedObject *pObject)
+void hgcmObjDereference(HGCMObject *pObject)
 {
     LogFlow(("MAIN::hgcmObjDereference: pObject %p\n", pObject));
 
