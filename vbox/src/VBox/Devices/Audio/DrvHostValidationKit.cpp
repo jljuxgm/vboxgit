12ebfcaba2f451dc4c9843af2b3ab2ef128c15ac
qzuser <395143360@qq.com>
2022-06-10 12:53:42 +0800

    commit_73877 ------------------------------------------------------------------------ r73877 | vboxsync | 2018-08-25 00:10:09 +0800 (Sat, 25 Aug 2018) | 1 line
    Audio/DrvHostValidationKit: No input stream supported (yet).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Audio/DrvHostValidationKit.cpp
+++ vbox/src/VBox/Devices/Audio/DrvHostValidationKit.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostValidationKit.cpp 73838 2018-08-22 16:15:08Z vboxsync $ */
+/* $Id: DrvHostValidationKit.cpp 73877 2018-08-24 16:10:09Z vboxsync $ */
 /** @file
  * ValidationKit audio driver - host backend for dumping and injecting audio data
  *                              from/to the device emulation.
@@ -88,7 +88,7 @@ static DECLCALLBACK(int) drvHostVaKitAudioGetConfig(PPDMIHOSTAUDIO pInterface, P
     pBackendCfg->cbStreamIn     = sizeof(VAKITAUDIOSTREAM);
 
     pBackendCfg->cMaxStreamsOut = 1; /* Output */
-    pBackendCfg->cMaxStreamsIn  = 2; /* Line input + microphone input. */
+    pBackendCfg->cMaxStreamsIn  = 0; /* No input supported yet. */
 
     return VINF_SUCCESS;
 }
