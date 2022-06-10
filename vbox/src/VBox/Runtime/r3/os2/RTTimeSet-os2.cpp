923c8d182647776203bde336a2fcd8d87efbfb24
qzuser <395143360@qq.com>
2022-06-10 19:47:08 +0800

    commit_75466 ------------------------------------------------------------------------ r75466 | vboxsync | 2018-11-15 04:19:03 +0800 (Thu, 15 Nov 2018) | 1 line
    IPRT/RTTimeSet-os2.cpp: debug build fix
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/os2/RTTimeSet-os2.cpp
+++ vbox/src/VBox/Runtime/r3/os2/RTTimeSet-os2.cpp
@@ -1,4 +1,4 @@
-/* $Id: RTTimeSet-os2.cpp 75328 2018-11-08 16:53:28Z vboxsync $ */
+/* $Id: RTTimeSet-os2.cpp 75466 2018-11-14 20:19:03Z vboxsync $ */
 /** @file
  * IPRT - RTTimeSet, OS/2.
  */
@@ -81,7 +81,7 @@ RTDECL(int) RTTimeSet(PCRTTIMESPEC pTime)
         APIRET rc = DosSetDateTime(&DateTime);
         if (rc == NO_ERROR)
             return VINF_SUCCESS;
-        AssertRC(rc);
+        AssertMsgFailed(("rc=%u\n", rc));
         return RTErrConvertFromOS2(rc);
     }
     return VERR_INVALID_PARAMETER;
