b4108ee06dc29fe04704f10f2a4bf0db3e72553c
qzuser <395143360@qq.com>
2022-06-11 04:26:58 +0800

    commit_77359 ------------------------------------------------------------------------ r77359 | vboxsync | 2019-02-19 19:16:35 +0800 (Tue, 19 Feb 2019) | 1 line
    FE/Qt: bugref:9340. Dont use/update recent medium list for hard disk when there is a vm content.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/medium/UIMediumManager.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMediumManager.cpp 77277 2019-02-12 14:45:05Z vboxsync $ */
+/* $Id: UIMediumManager.cpp 77359 2019-02-19 11:16:35Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIMediumManager class implementation.
  */
@@ -488,7 +488,8 @@ void UIMediumManagerWidget::sltHandleMediumEnumerationFinish()
 void UIMediumManagerWidget::sltAddMedium()
 {
     QString strDefaultMachineFolder = vboxGlobal().virtualBox().GetSystemProperties().GetDefaultMachineFolder();
-    vboxGlobal().openMediumWithFileOpenDialog(currentMediumType(), this, strDefaultMachineFolder);
+    vboxGlobal().openMediumWithFileOpenDialog(currentMediumType(), this,
+                                              strDefaultMachineFolder, true /* use most recent medium folder */);
 }
 
 void UIMediumManagerWidget::sltCreateMedium()
