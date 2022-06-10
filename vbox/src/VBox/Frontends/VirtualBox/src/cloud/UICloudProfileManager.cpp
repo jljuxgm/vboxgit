825132c2f60ac2ba0057a2e97c9704cd6d2e8c9f
qzuser <395143360@qq.com>
2022-06-10 17:37:37 +0800

    commit_74922 ------------------------------------------------------------------------ r74922 | vboxsync | 2018-10-18 22:36:50 +0800 (Thu, 18 Oct 2018) | 1 line
    FE/Qt: bugref:9230: Save profile changes on add/remove/edit.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/cloud/UICloudProfileManager.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/cloud/UICloudProfileManager.cpp
@@ -1,4 +1,4 @@
-/* $Id: UICloudProfileManager.cpp 74921 2018-10-18 14:15:19Z vboxsync $ */
+/* $Id: UICloudProfileManager.cpp 74922 2018-10-18 14:36:50Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UICloudProfileManager class implementation.
  */
@@ -252,6 +252,12 @@ void UICloudProfileManagerWidget::sltApplyCloudProfileDetailsChanges()
 
                     /* Make sure current item fetched: */
                     sltHandleCurrentItemChange();
+
+                    /* Save profile changes: */
+                    comCloudProvider.SaveProfiles();
+                    /* Show error message if necessary: */
+                    if (!comCloudProvider.isOk())
+                        msgCenter().cannotSaveCloudProfiles(comCloudProvider, this);
                 }
             }
         }
@@ -321,6 +327,12 @@ void UICloudProfileManagerWidget::sltAddCloudProfile()
                     UIDataCloudProfile data;
                     loadCloudProfile(comCloudProfile, data);
                     createItemForCloudProfile(pProviderItem, data, true);
+
+                    /* Save profile changes: */
+                    comCloudProvider.SaveProfiles();
+                    /* Show error message if necessary: */
+                    if (!comCloudProvider.isOk())
+                        msgCenter().cannotSaveCloudProfiles(comCloudProvider, this);
                 }
             }
         }
@@ -375,6 +387,12 @@ void UICloudProfileManagerWidget::sltRemoveCloudProfile()
 
                 /* Remove interface from the tree: */
                 delete pProfileItem;
+
+                /* Save profile changes: */
+                comCloudProvider.SaveProfiles();
+                /* Show error message if necessary: */
+                if (!comCloudProvider.isOk())
+                    msgCenter().cannotSaveCloudProfiles(comCloudProvider, this);
             }
         }
     }
