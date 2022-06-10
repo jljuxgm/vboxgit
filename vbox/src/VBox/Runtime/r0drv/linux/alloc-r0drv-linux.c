2a9d7b7c5cdf242eb5a9a1c741a41eabf84703a2
qzuser <395143360@qq.com>
2022-06-11 05:45:04 +0800

    commit_77727 ------------------------------------------------------------------------ r77727 | vboxsync | 2019-03-15 22:14:18 +0800 (Fri, 15 Mar 2019) | 1 line
    IPRT,HostDrivers: Fixed some warnings.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r0drv/linux/alloc-r0drv-linux.c
+++ vbox/src/VBox/Runtime/r0drv/linux/alloc-r0drv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: alloc-r0drv-linux.c 76553 2019-01-01 01:45:53Z vboxsync $ */
+/* $Id: alloc-r0drv-linux.c 77727 2019-03-15 14:14:18Z vboxsync $ */
 /** @file
  * IPRT - Memory Allocation, Ring-0 Driver, Linux.
  */
@@ -61,6 +61,8 @@
 # include <iprt/spinlock.h>
 # include <iprt/errcore.h>
 #endif
+
+#include "internal/initterm.h"
 
 
 /*********************************************************************************************************************************
