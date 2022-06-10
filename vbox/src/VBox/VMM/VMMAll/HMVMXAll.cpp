915478bf97c5941224c4eec4d6db96d010d20ad4
qzuser <395143360@qq.com>
2022-06-11 02:03:11 +0800

    commit_76638 ------------------------------------------------------------------------ r76638 | vboxsync | 2019-01-05 02:46:51 +0800 (Sat, 05 Jan 2019) | 2 lines
    VMM/HM: Nested VMX: bugref:9180 Add a diagnostic enum for failing to read the VMCS revision portion of the VMCS during VMPTRLD.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMAll/HMVMXAll.cpp
+++ vbox/src/VBox/VMM/VMMAll/HMVMXAll.cpp
@@ -1,4 +1,4 @@
-/* $Id: HMVMXAll.cpp 76553 2019-01-01 01:45:53Z vboxsync $ */
+/* $Id: HMVMXAll.cpp 76638 2019-01-04 18:46:51Z vboxsync $ */
 /** @file
  * HM VMX (VT-x) - All contexts.
  */
@@ -90,6 +90,7 @@ static const char * const g_apszVmxVDiagDesc[] =
     VMXV_DIAG_DESC(kVmxVDiag_Vmptrld_PtrVmxon                 , "PtrVmxon"                  ),
     VMXV_DIAG_DESC(kVmxVDiag_Vmptrld_PtrWidth                 , "PtrWidth"                  ),
     VMXV_DIAG_DESC(kVmxVDiag_Vmptrld_RealOrV86Mode            , "RealOrV86Mode"             ),
+    VMXV_DIAG_DESC(kVmxVDiag_Vmptrld_RevPtrReadPhys           , "RevPtrReadPhys"            ),
     VMXV_DIAG_DESC(kVmxVDiag_Vmptrld_ShadowVmcs               , "ShadowVmcs"                ),
     VMXV_DIAG_DESC(kVmxVDiag_Vmptrld_VmcsRevId                , "VmcsRevId"                 ),
     VMXV_DIAG_DESC(kVmxVDiag_Vmptrld_VmxRoot                  , "VmxRoot"                   ),
