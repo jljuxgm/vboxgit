e96d95050233d0f2b4975fb3049017be62eefadc
qzuser <395143360@qq.com>
2022-06-11 03:05:18 +0800

    commit_76945 ------------------------------------------------------------------------ r76945 | vboxsync | 2019-01-23 15:50:10 +0800 (Wed, 23 Jan 2019) | 1 line
    FE/Qt: bugref:9080: Remove some obsolete files
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoConfigurationPanel.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoConfigurationPanel.h
@@ -1,4 +1,4 @@
-/* $Id: UIVisoConfigurationPanel.h 76944 2019-01-23 07:39:42Z vboxsync $ */
+/* $Id: UIVisoConfigurationPanel.h 76945 2019-01-23 07:50:10Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIVisoConfigurationPanel class declaration.
  */
@@ -23,7 +23,6 @@
 
 /* GUI includes: */
 #include "UIVisoCreatorPanel.h"
-#include "UIVisoCreatorDefs.h"
 
 /* Forward declarations: */
 class QGridLayout;
