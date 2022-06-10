b6cf6150b67b79ac67f759ed9b21664ce78cce99
qzuser <395143360@qq.com>
2022-06-10 09:09:38 +0800

    commit_72997 ------------------------------------------------------------------------ r72997 | vboxsync | 2018-07-09 15:43:53 +0800 (Mon, 09 Jul 2018) | 1 line
    VDVfs.cpp: Fixed off by one issue in vdVfsFile_Write.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Storage/VDVfs.cpp
+++ vbox/src/VBox/Storage/VDVfs.cpp
@@ -1,4 +1,4 @@
-/* $Id: VDVfs.cpp 70307 2017-12-22 08:37:01Z vboxsync $ */
+/* $Id: VDVfs.cpp 72997 2018-07-09 07:43:53Z vboxsync $ */
 /** @file
  * Virtual Disk Container implementation. - VFS glue.
  */
@@ -361,7 +361,7 @@ static DECLCALLBACK(int) vdVfsFile_Write(void *pvThis, RTFOFF off, PCRTSGBUF pSg
     }
 
     size_t cbLeftToWrite;
-    if (offUnsigned + pSgBuf->paSegs[0].cbSeg < cbImage)
+    if (offUnsigned + pSgBuf->paSegs[0].cbSeg <= cbImage)
     {
         cbLeftToWrite = pSgBuf->paSegs[0].cbSeg;
         if (pcbWritten)
