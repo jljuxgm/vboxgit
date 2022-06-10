93df2710c6c7cd97321b96c3508c48d26d502603
qzuser <395143360@qq.com>
2022-06-10 17:52:36 +0800

    commit_74995 ------------------------------------------------------------------------ r74995 | vboxsync | 2018-10-23 20:15:27 +0800 (Tue, 23 Oct 2018) | 1 line
    VideoRec/Main: Build fix.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/VideoRecStream.cpp
+++ vbox/src/VBox/Main/src-client/VideoRecStream.cpp
@@ -1,4 +1,4 @@
-/* $Id: VideoRecStream.cpp 74994 2018-10-23 12:04:40Z vboxsync $ */
+/* $Id: VideoRecStream.cpp 74995 2018-10-23 12:15:27Z vboxsync $ */
 /** @file
  * Video recording stream code.
  */
@@ -37,6 +37,7 @@
 
 #include "VideoRec.h"
 #include "VideoRecStream.h"
+#include "WebMWriter.h"
 
 
 /**
