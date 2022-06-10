889de8b46311b4e6730b46c18ff4b7f505286162
qzuser <395143360@qq.com>
2022-06-11 00:26:10 +0800

    commit_76297 ------------------------------------------------------------------------ r76297 | vboxsync | 2018-12-20 00:32:48 +0800 (Thu, 20 Dec 2018) | 1 line
    FE/Qt: bugref:6699. Refactoring on file system model/item/view classes. episode 2
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerHostTable.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerHostTable.h
@@ -1,4 +1,4 @@
-/* $Id: UIFileManagerHostTable.h 76177 2018-12-12 13:38:57Z vboxsync $ */
+/* $Id: UIFileManagerHostTable.h 76297 2018-12-19 16:32:48Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIFileManagerHostTable class declaration.
  */
@@ -25,7 +25,7 @@
 class UIActionPool;
 
 /** This class scans the host file system by using the Qt API
-    and connects to the UIFileManagerModel*/
+    and connects to the UICustomFileSystemModel*/
 class UIFileManagerHostTable : public UIFileManagerTable
 {
     Q_OBJECT;
