05624c91e89f65b17a08b3f4e174965715f61df5
qzuser <395143360@qq.com>
2022-06-10 19:06:14 +0800

    commit_75366 ------------------------------------------------------------------------ r75366 | vboxsync | 2018-11-10 00:07:55 +0800 (Sat, 10 Nov 2018) | 1 line
    Recording: Added support for parsing the (legacy) option string values (audio).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/RecordingScreenSettingsImpl.h
+++ vbox/src/VBox/Main/include/RecordingScreenSettingsImpl.h
@@ -1,4 +1,4 @@
-/* $Id: RecordingScreenSettingsImpl.h 75361 2018-11-09 12:56:40Z vboxsync $ */
+/* $Id: RecordingScreenSettingsImpl.h 75366 2018-11-09 16:07:55Z vboxsync $ */
 
 /** @file
  *
@@ -104,7 +104,11 @@ private:
 private:
 
     // internal methods
-    int  i_initInternal();
+    int i_initInternal();
+
+private:
+
+    static int i_parseOptionsString(const com::Utf8Str &strOptions, settings::RecordingScreenSettings &screenSettings);
 
 private:
 
