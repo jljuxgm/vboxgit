d70c5aa6c7d36df3045e63b9593899ff7ce99e7e
qzuser <395143360@qq.com>
2022-06-10 18:27:16 +0800

    commit_75167 ------------------------------------------------------------------------ r75167 | vboxsync | 2018-10-30 05:03:40 +0800 (Tue, 30 Oct 2018) | 1 line
    Main/HGCM: Must deregister the 'guestprops' info item before shutting down HGCM, otherwise we risk calling into freed DLL memory should a guru happen later in the VM destruction/whatever process.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/HGCM.h
+++ vbox/src/VBox/Main/include/HGCM.h
@@ -1,4 +1,4 @@
-/* $Id: HGCM.h 69500 2017-10-28 15:14:05Z vboxsync $ */
+/* $Id: HGCM.h 75167 2018-10-29 21:03:40Z vboxsync $ */
 /** @file
  * HGCM - Host-Guest Communication Manager.
  */
@@ -60,4 +60,4 @@ int HGCMHostLoadState (PSSMHANDLE pSSM);
 
 RT_C_DECLS_END
 
-#endif /* __HGCM_h__ */
+#endif /* !__HGCM_h__ */
