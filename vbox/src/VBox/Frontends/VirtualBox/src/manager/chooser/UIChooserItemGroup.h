dc182f8c921a7fbbb74ee4527f175c7c9efa9849
qzuser <395143360@qq.com>
2022-06-11 05:19:58 +0800

    commit_77621 ------------------------------------------------------------------------ r77621 | vboxsync | 2019-03-08 23:21:26 +0800 (Fri, 08 Mar 2019) | 1 line
    FE/Qt: bugref:9241: Build fix for r129248.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.h
@@ -1,4 +1,4 @@
-/* $Id: UIChooserItemGroup.h 77620 2019-03-08 15:03:08Z vboxsync $ */
+/* $Id: UIChooserItemGroup.h 77621 2019-03-08 15:21:26Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIChooserItemGroup class declaration.
  */
@@ -26,6 +26,7 @@
 
 /* Forward declarations: */
 class QGraphicsLinearLayout;
+class QLineEdit;
 class UIEditorGroupRename;
 class UIGraphicsButton;
 class UIGraphicsRotatorButton;
