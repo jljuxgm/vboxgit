1932e3d241a625f19f8ab215b23b1f9df5d61592
qzuser <395143360@qq.com>
2022-06-11 01:40:39 +0800

    commit_76554 ------------------------------------------------------------------------ r76554 | vboxsync | 2019-01-01 09:51:10 +0800 (Tue, 01 Jan 2019) | 1 line
    scm: build fix
    ------------------------------------------------------------------------

--- vbox/src/bldprogs/scmrw.cpp
+++ vbox/src/bldprogs/scmrw.cpp
@@ -1,4 +1,4 @@
-/* $Id: scmrw.cpp 76553 2019-01-01 01:45:53Z vboxsync $ */
+/* $Id: scmrw.cpp 76554 2019-01-01 01:51:10Z vboxsync $ */
 /** @file
  * IPRT Testcase / Tool - Source Code Massager.
  */
@@ -2863,7 +2863,7 @@ bool rewrite_FixHeaderGuards(PSCMRWSTATE pState, PSCMSTREAM pIn, PSCMSTREAM pOut
 
     RTERRINFOSTATIC ErrInfo;
     char            szNormalized[168];
-    size_t          cchNormalized;
+    size_t          cchNormalized = 0;
     int             rc;
     bool            fRet = false;
 
