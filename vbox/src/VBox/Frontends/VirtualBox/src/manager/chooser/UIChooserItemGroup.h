40da7b4d01c2e84c8c33400161a7f509635c1f6a
qzuser <395143360@qq.com>
2022-06-10 18:30:21 +0800

    commit_75183 ------------------------------------------------------------------------ r75183 | vboxsync | 2018-10-30 22:59:35 +0800 (Tue, 30 Oct 2018) | 1 line
    FE/Qt: bugref:9241: VirtualBox Manager: Forgot something in r126263 (try two).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.h
@@ -1,4 +1,4 @@
-/* $Id: UIChooserItemGroup.h 75181 2018-10-30 14:55:01Z vboxsync $ */
+/* $Id: UIChooserItemGroup.h 75183 2018-10-30 14:59:35Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIChooserItemGroup class declaration.
  */
@@ -28,6 +28,7 @@
 
 /* Forward declarations: */
 class QGraphicsScene;
+class QLineEdit;
 class QMenu;
 class QMimeData;
 class QPainter;
