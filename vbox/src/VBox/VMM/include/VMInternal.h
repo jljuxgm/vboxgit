7fee6e6dfaec2ec5520931098cc3a8a862eaa752
qzuser <395143360@qq.com>
2022-06-10 10:47:30 +0800

    commit_73351 ------------------------------------------------------------------------ r73351 | vboxsync | 2018-07-25 21:02:11 +0800 (Wed, 25 Jul 2018) | 1 line
    VBoxGuest,VMMDev,DBGF,VM: Added bug check report to VBoxGuest/VMMDev and hooked it up to DBGF.  Made DBGF remember the last reported bug check, adding an info handler for displaying it.  Added VM reset counters w/ getters for use in bug check reporting.
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/include/VMInternal.h
+++ vbox/src/VBox/VMM/include/VMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VMInternal.h 69474 2017-10-28 13:12:06Z vboxsync $ */
+/* $Id: VMInternal.h 73351 2018-07-25 13:02:11Z vboxsync $ */
 /** @file
  * VM - Internal header file.
  */
@@ -165,6 +165,12 @@ typedef struct VMINT
     bool                            fTeleportedAndNotFullyResumedYet;
     /** The VM should power off instead of reset. */
     bool                            fPowerOffInsteadOfReset;
+    /** Reset counter (soft + hard). */
+    uint32_t                        cResets;
+    /** Hard reset counter. */
+    uint32_t                        cHardResets;
+    /** Soft reset counter. */
+    uint32_t                        cSoftResets;
 } VMINT;
 /** Pointer to the VM Internal Data (part of the VM structure). */
 typedef VMINT *PVMINT;
