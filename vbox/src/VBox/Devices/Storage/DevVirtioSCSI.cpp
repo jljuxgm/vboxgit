2404183282e4a07177dbc1f92063408695e24cdc
qzuser <395143360@qq.com>
2022-06-11 21:30:46 +0800

    commit_81816 ------------------------------------------------------------------------ r81816 | vboxsync | 2019-11-13 02:53:07 +0800 (Wed, 13 Nov 2019) | 1 line
    Storage/DevVirtioSCSI.cpp: More fixing  burns on build box for stuff that builds clean on my Debian machine
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Storage/DevVirtioSCSI.cpp
+++ vbox/src/VBox/Devices/Storage/DevVirtioSCSI.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVirtioSCSI.cpp 81815 2019-11-12 18:46:14Z vboxsync $ $Revision: 81815 $ $Date: 2019-11-13 02:46:14 +0800 (Wed, 13 Nov 2019) $ $Author: vboxsync $ */
+/* $Id: DevVirtioSCSI.cpp 81816 2019-11-12 18:53:07Z vboxsync $ $Revision: 81816 $ $Date: 2019-11-13 02:53:07 +0800 (Wed, 13 Nov 2019) $ $Author: vboxsync $ */
 /** @file
  * VBox storage devices - Virtio SCSI Driver
  *
@@ -1138,7 +1138,7 @@ static int virtioScsiR3ReqSubmit(PPDMDEVINS pDevIns, PVIRTIOSCSI pThis, PVIRTIOS
     if (uType == 0xc1 && uTarget == 0x01)
     {
         LogRel(("* * * REPORT LUNS LU ACCESSED * * * "));
-        uScsiLun == 0xff; /* Force rejection. todo: figure out right way to handle, r=paul */
+        uScsiLun = 0xff; /* Force rejection. todo: figure out right way to handle, r=paul */
     }
     else
     if (uType != 1)
