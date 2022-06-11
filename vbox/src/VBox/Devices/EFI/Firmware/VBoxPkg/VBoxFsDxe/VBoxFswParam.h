64dd6f7b53c64d15be5f7095c1c90306b9e668f6
qzuser <395143360@qq.com>
2022-06-12 01:43:28 +0800

    commit_82508 ------------------------------------------------------------------------ r82508 | vboxsync | 2019-12-09 20:10:54 +0800 (Mon, 09 Dec 2019) | 1 line
    EFI/Firmware/VBoxPkg: Add new GUID to retrieve the blessed file from HFS+ volumes through GetInfo()
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/EFI/Firmware/VBoxPkg/VBoxFsDxe/VBoxFswParam.h
+++ vbox/src/VBox/Devices/EFI/Firmware/VBoxPkg/VBoxFsDxe/VBoxFswParam.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxFswParam.h 76553 2019-01-01 01:45:53Z vboxsync $ */
+/* $Id: VBoxFswParam.h 82508 2019-12-09 12:10:54Z vboxsync $ */
 /** @file
  * VBoxFswParam.h
  */
@@ -52,6 +52,8 @@
 # include <Guid/FileInfo.h>
 # include <Guid/FileSystemVolumeLabelInfo.h>
 # include <Protocol/ComponentName.h>
+
+# include <Guid/VBoxFsBlessedFileInfo.h> /* For HFS blessed file support. */
 
 # define BS gBS
 # define PROTO_NAME(x) gEfi ## x ## Guid
