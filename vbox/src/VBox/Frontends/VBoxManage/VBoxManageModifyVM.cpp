f6971273af1adccd6c5e8d331a58d9c75af194d0
qzuser <395143360@qq.com>
2022-06-11 20:36:20 +0800

    commit_81603 ------------------------------------------------------------------------ r81603 | vboxsync | 2019-10-31 17:25:18 +0800 (Thu, 31 Oct 2019) | 1 line
    Main: Added PCnet-ISA/NE2100/Am79C960 to the API (the device side is long in place).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
+++ vbox/src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxManageModifyVM.cpp 81581 2019-10-30 09:45:55Z vboxsync $ */
+/* $Id: VBoxManageModifyVM.cpp 81603 2019-10-31 09:25:18Z vboxsync $ */
 /** @file
  * VBoxManage - Implementation of modifyvm command.
  */
@@ -1467,6 +1467,10 @@ RTEXITCODE handleModifyVM(HandlerArg *a)
                 else if (!RTStrICmp(ValueUnion.psz, "Am79C973"))
                 {
                     CHECK_ERROR(nic, COMSETTER(AdapterType)(NetworkAdapterType_Am79C973));
+                }
+                else if (!RTStrICmp(ValueUnion.psz, "Am79C960"))
+                {
+                    CHECK_ERROR(nic, COMSETTER(AdapterType)(NetworkAdapterType_Am79C960));
                 }
 #ifdef VBOX_WITH_E1000
                 else if (!RTStrICmp(ValueUnion.psz, "82540EM"))
