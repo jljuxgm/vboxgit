110dd900f24fff1fc0e8f288de25ba5037cb1054
qzuser <395143360@qq.com>
2022-06-11 11:37:21 +0800

    commit_79413 ------------------------------------------------------------------------ r79413 | vboxsync | 2019-06-28 21:00:16 +0800 (Fri, 28 Jun 2019) | 1 line
    IPRT/tcp: RTTcpClientCancelConnect must not attempt close an already cancelled socket as that'll lead to a crash. bugref:9151
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/tcp.cpp
+++ vbox/src/VBox/Runtime/r3/tcp.cpp
@@ -1,4 +1,4 @@
-/* $Id: tcp.cpp 78821 2019-05-28 14:16:34Z vboxsync $ */
+/* $Id: tcp.cpp 79413 2019-06-28 13:00:16Z vboxsync $ */
 /** @file
  * IPRT - TCP/IP.
  */
@@ -873,9 +873,11 @@ RTR3DECL(int) RTTcpClientCancelConnect(PRTTCPCLIENTCONNECTCANCEL volatile *ppCan
 {
     AssertPtrReturn(ppCancelCookie, VERR_INVALID_POINTER);
 
+    RTSOCKET const hSockCancelled = (RTSOCKET)(uintptr_t)0xdead9999;
+
     AssertCompile(NIL_RTSOCKET == NULL);
-    RTSOCKET hSock = (RTSOCKET)ASMAtomicXchgPtr((void * volatile *)ppCancelCookie, (void *)(uintptr_t)0xdead9999);
-    if (hSock != NIL_RTSOCKET)
+    RTSOCKET hSock = (RTSOCKET)ASMAtomicXchgPtr((void * volatile *)ppCancelCookie, hSockCancelled);
+    if (hSock != NIL_RTSOCKET && hSock != hSockCancelled)
     {
         int rc = rtTcpClose(hSock, "RTTcpClientCancelConnect", false /*fTryGracefulShutdown*/);
         AssertRCReturn(rc, rc);
