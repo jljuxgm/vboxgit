fb05ff92d63a98d144f4856f4f23baa569db8b9c
qzuser <395143360@qq.com>
2022-06-11 08:27:16 +0800

    commit_78520 ------------------------------------------------------------------------ r78520 | vboxsync | 2019-05-15 01:29:24 +0800 (Wed, 15 May 2019) | 1 line
    FE/Qt: bugref:4551: Few more icon enum values for SAS controller forgotten in r56277 (s.a. r130558).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsStorage.h
@@ -1,4 +1,4 @@
-/* $Id: UIMachineSettingsStorage.h 78518 2019-05-14 16:12:59Z vboxsync $ */
+/* $Id: UIMachineSettingsStorage.h 78520 2019-05-14 17:29:24Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIMachineSettingsStorage class declaration.
  */
@@ -111,6 +111,8 @@ enum PixmapType
     SATAControllerAddDis,
     SCSIControllerAddEn,
     SCSIControllerAddDis,
+    SASControllerAddEn,
+    SASControllerAddDis,
     USBControllerAddEn,
     USBControllerAddDis,
     NVMeControllerAddEn,
