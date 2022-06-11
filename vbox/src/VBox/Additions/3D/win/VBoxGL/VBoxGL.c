84a2c7746d8625a273b2b013db5a00e665d1e7ec
qzuser <395143360@qq.com>
2022-06-12 06:09:09 +0800

    commit_83832 ------------------------------------------------------------------------ r83832 | vboxsync | 2020-04-19 22:12:33 +0800 (Sun, 19 Apr 2020) | 1 line
    Additions/3D: VC++ 14.1 adjustments and warnings.  bugref:8489
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/3D/win/VBoxGL/VBoxGL.c
+++ vbox/src/VBox/Additions/3D/win/VBoxGL/VBoxGL.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxGL.c 82968 2020-02-04 10:35:17Z vboxsync $ */
+/* $Id: VBoxGL.c 83832 2020-04-19 14:12:33Z vboxsync $ */
 /** @file
  * VirtualBox Windows Guest Mesa3D - OpenGL driver.
  */
@@ -271,7 +271,7 @@ wddm_shared_surface_open(struct pipe_screen *screen,
         if (surface)
         {
             D3DKMT_HANDLE hDevice = GaDrvEnvKmtDeviceHandle(pEnv);
-            NTSTATUS Status = vboxKmtOpenSharedSurface(hDevice, (D3DKMT_HANDLE)hSharedSurface, surface);
+            NTSTATUS Status = vboxKmtOpenSharedSurface(hDevice, (D3DKMT_HANDLE)(uintptr_t)hSharedSurface, surface);
             if (Status != STATUS_SUCCESS)
             {
                 free(surface);
