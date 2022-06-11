bdea7dd37fbd92c75a22bf1c5a2ead8d67a41e47
qzuser <395143360@qq.com>
2022-06-11 08:18:52 +0800

    commit_78476 ------------------------------------------------------------------------ r78476 | vboxsync | 2019-05-13 16:06:10 +0800 (Mon, 13 May 2019) | 1 line
    Shared Clipboard/URI: SCM fixes.
    ------------------------------------------------------------------------

--- vbox/src/VBox/GuestHost/SharedClipboard/ClipboardDataObjectImpl-win.cpp
+++ vbox/src/VBox/GuestHost/SharedClipboard/ClipboardDataObjectImpl-win.cpp
@@ -1,4 +1,4 @@
-/* $Id: ClipboardDataObjectImpl-win.cpp 78474 2019-05-13 07:44:15Z vboxsync $ */
+/* $Id: ClipboardDataObjectImpl-win.cpp 78476 2019-05-13 08:06:10Z vboxsync $ */
 /** @file
  * ClipboardDataObjectImpl-win.cpp - Shared Clipboard IDataObject implementation.
  */
@@ -38,7 +38,7 @@
 #include <iprt/uri.h>
 #include <iprt/utf16.h>
 
-#include <VBox/err.h>
+#include <iprt/errcore.h>
 #include <VBox/log.h>
 
 VBoxClipboardWinDataObject::VBoxClipboardWinDataObject(LPFORMATETC pFormatEtc, LPSTGMEDIUM pStgMed, ULONG cFormats)
