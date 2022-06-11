98f78d1dfbf84f057b015c6a55d1d820ecd3eea0
qzuser <395143360@qq.com>
2022-06-11 10:25:15 +0800

    commit_79089 ------------------------------------------------------------------------ r79089 | vboxsync | 2019-06-11 21:03:19 +0800 (Tue, 11 Jun 2019) | 1 line
    FE/Qt: bugref:6143. Sme prep. work for a more able key editor
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/softkeyboard/UISoftKeyboard.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/softkeyboard/UISoftKeyboard.h
@@ -1,4 +1,4 @@
-/* $Id: UISoftKeyboard.h 79082 2019-06-11 08:37:29Z vboxsync $ */
+/* $Id: UISoftKeyboard.h 79089 2019-06-11 13:03:19Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UISoftKeyboard class declaration.
  */
@@ -63,6 +63,7 @@ private slots:
     void sltHandleLayoutChange(const QString &strLayoutName);
     void sltHandleKeyCapFileChange(const QString &strKeyCapFileName);
     void sltHandleStatusBarContextMenuRequest(const QPoint &point);
+
 private:
 
     void prepareObjects();
