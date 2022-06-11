03b5eddef9f07db0d73c8d47d38e49e73edbb9a7
qzuser <395143360@qq.com>
2022-06-12 02:58:47 +0800

    commit_82852 ------------------------------------------------------------------------ r82852 | vboxsync | 2020-01-24 23:12:23 +0800 (Fri, 24 Jan 2020) | 1 line
    Shared Clipboard: Added missing / disabled code for negotiating transfer chunk sizes. bugref:9437
    ------------------------------------------------------------------------

--- vbox/src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc-transfers.cpp
+++ vbox/src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc-transfers.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxSharedClipboardSvc-transfers.cpp 82848 2020-01-24 10:32:37Z vboxsync $ */
+/* $Id: VBoxSharedClipboardSvc-transfers.cpp 82852 2020-01-24 15:12:23Z vboxsync $ */
 /** @file
  * Shared Clipboard Service - Internal code for transfer (list) handling.
  */
@@ -736,6 +736,8 @@ bool shClSvcTransferMsgIsAllowed(uint32_t uMode, uint32_t uMsg)
             break;
 
         case VBOX_SHCL_GUEST_FN_CONNECT:
+            RT_FALL_THROUGH();
+        case VBOX_SHCL_GUEST_FN_NEGOTIATE_CHUNK_SIZE:
             RT_FALL_THROUGH();
         case VBOX_SHCL_GUEST_FN_MSG_PEEK_WAIT:
             RT_FALL_THROUGH();
