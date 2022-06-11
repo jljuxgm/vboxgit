aee262aecd352d4a9e2ca030c66b27dc56589395
qzuser <395143360@qq.com>
2022-06-11 21:09:21 +0800

    commit_81737 ------------------------------------------------------------------------ r81737 | vboxsync | 2019-11-08 00:10:43 +0800 (Fri, 08 Nov 2019) | 1 line
    FE/Qt: UIEmptyFilePathSelector: When chooser is opened we should try to propagate file-name information if available to QIFileDialog static stuff.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/widgets/UIEmptyFilePathSelector.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/widgets/UIEmptyFilePathSelector.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIEmptyFilePathSelector.cpp 80955 2019-09-23 17:27:06Z vboxsync $ */
+/* $Id: UIEmptyFilePathSelector.cpp 81737 2019-11-07 16:10:43Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIEmptyFilePathSelector class implementation.
  */
@@ -216,11 +216,18 @@ void UIEmptyFilePathSelector::choose()
 {
     QString path = mPath;
 
+    /* Check whether we have file-name information available: */
+    const QString strFileName = QFileInfo(path).fileName();
+
     /* Preparing initial directory. */
     QString initDir = path.isNull() ? mHomeDir :
         QIFileDialog::getFirstExistingDir (path);
     if (initDir.isNull())
         initDir = mHomeDir;
+
+    /* Append file-name information if any: */
+    if (!strFileName.isEmpty())
+        initDir = QDir(initDir).absoluteFilePath(strFileName);
 
     switch (mMode)
     {
