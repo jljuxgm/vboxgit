bfcafd66dbe9a79067d6276c2479f58985125900
qzuser <395143360@qq.com>
2022-06-12 06:39:50 +0800

    commit_83998 ------------------------------------------------------------------------ r83998 | vboxsync | 2020-04-27 19:25:33 +0800 (Mon, 27 Apr 2020) | 1 line
    IPRT,VBoxAddInstallNt3x.cpp: Split out the IPRT bits from VBoxAddInstallNt3x.cpp and move template to /Config.kmk. [scm]
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/r3/win/nocrt-startup-exe-win.cpp
+++ vbox/src/VBox/Runtime/r3/win/nocrt-startup-exe-win.cpp
@@ -1,6 +1,6 @@
-/* $Id: nocrt-startup-exe-win.cpp 83997 2020-04-27 11:25:19Z vboxsync $ */
+/* $Id: nocrt-startup-exe-win.cpp 83998 2020-04-27 11:25:33Z vboxsync $ */
 /** @file
- * IPRT - No-CRT windows EXE startup code.
+ * IPRT - No-CRT - Windows EXE startup code.
  *
  * @note Does not run static constructors and destructors!
  */
