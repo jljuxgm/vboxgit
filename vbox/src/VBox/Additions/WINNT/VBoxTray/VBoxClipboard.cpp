3c58fa2aedbca33be4dc33b996dfa3a20d1c5427
qzuser <395143360@qq.com>
2022-06-11 22:13:04 +0800

    commit_81960 ------------------------------------------------------------------------ r81960 | vboxsync | 2019-11-19 03:00:50 +0800 (Tue, 19 Nov 2019) | 1 line
    Shared Clipboard/Transfers: Update.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/WINNT/VBoxTray/VBoxClipboard.cpp
+++ vbox/src/VBox/Additions/WINNT/VBoxTray/VBoxClipboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxClipboard.cpp 81843 2019-11-14 16:30:44Z vboxsync $ */
+/* $Id: VBoxClipboard.cpp 81960 2019-11-18 19:00:50Z vboxsync $ */
 /** @file
  * VBoxClipboard - Shared clipboard, Windows Guest Implementation.
  */
@@ -1144,7 +1144,7 @@ DECLCALLBACK(int) VBoxShClWorker(void *pInstance, bool volatile *pfShutdown)
             break;
     }
 
-    LogRel(("Shared Clipboard: Worker loop ended\n"));
+    LogRel2(("Shared Clipboard: Worker loop ended\n"));
 
 #ifdef VBOX_WITH_SHARED_CLIPBOARD_TRANSFERS
     OleSetClipboard(NULL); /* Make sure to flush the clipboard on destruction. */
