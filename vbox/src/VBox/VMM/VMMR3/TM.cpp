57490ff170f3a087a6ce8a4679fde0eb88c9f9e3
qzuser <395143360@qq.com>
2022-06-11 18:53:25 +0800

    commit_81153 ------------------------------------------------------------------------ r81153 | vboxsync | 2019-10-08 21:59:03 +0800 (Tue, 08 Oct 2019) | 1 line
    VMM: Removed most VBOX_WITH_REM preprocessor stuff.  bugref:9576
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMR3/TM.cpp
+++ vbox/src/VBox/VMM/VMMR3/TM.cpp
@@ -1,4 +1,4 @@
-/* $Id: TM.cpp 81150 2019-10-08 12:53:47Z vboxsync $ */
+/* $Id: TM.cpp 81153 2019-10-08 13:59:03Z vboxsync $ */
 /** @file
  * TM - Time Manager.
  */
@@ -2105,9 +2105,6 @@ static DECLCALLBACK(void) tmR3TimerCallback(PRTTIMER pTimer, void *pvUser, uint6
     {
         Log5(("TM(%u): FF: 0 -> 1\n", __LINE__));
         VMCPU_FF_SET(pVCpuDst, VMCPU_FF_TIMER);
-#ifdef VBOX_WITH_REM
-        REMR3NotifyTimerPending(pVM, pVCpuDst);
-#endif
         VMR3NotifyCpuFFU(pVCpuDst->pUVCpu, VMNOTIFYFF_FLAGS_DONE_REM | VMNOTIFYFF_FLAGS_POKE);
         STAM_COUNTER_INC(&pVM->tm.s.StatTimerCallbackSetFF);
     }
