d32de9a996d2d4f6d26df9553bc3bbce82d6a8db
qzuser <395143360@qq.com>
2022-06-11 20:50:58 +0800

    commit_81667 ------------------------------------------------------------------------ r81667 | vboxsync | 2019-11-05 19:08:21 +0800 (Tue, 05 Nov 2019) | 1 line
    Main: Added a portPath attribute to IUSBDevice. This will allow uniquely identifying a specific USB port in a system (see bugref:9518).
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/include/HostUSBDeviceImpl.h
+++ vbox/src/VBox/Main/include/HostUSBDeviceImpl.h
@@ -1,4 +1,4 @@
-/* $Id: HostUSBDeviceImpl.h 76592 2019-01-01 20:13:07Z vboxsync $ */
+/* $Id: HostUSBDeviceImpl.h 81667 2019-11-05 11:08:21Z vboxsync $ */
 /** @file
  * VirtualBox IHostUSBDevice COM interface implementation.
  */
@@ -258,6 +258,7 @@ private:
     HRESULT getSerialNumber(com::Utf8Str &aSerialNumber);
     HRESULT getAddress(com::Utf8Str &aAddress);
     HRESULT getPort(USHORT *aPort);
+    HRESULT getPortPath(com::Utf8Str &aPortPath);
     HRESULT getVersion(USHORT *aVersion);
     HRESULT getPortVersion(USHORT *aPortVersion);
     HRESULT getSpeed(USBConnectionSpeed_T *aSpeed);
