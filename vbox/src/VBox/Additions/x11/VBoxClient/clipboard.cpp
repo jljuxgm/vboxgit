1f53927ebc6a572509ef26a30b926d2aa83274ec
qzuser <395143360@qq.com>
2022-06-12 00:27:03 +0800

    commit_82287 ------------------------------------------------------------------------ r82287 | vboxsync | 2019-11-30 00:18:10 +0800 (Sat, 30 Nov 2019) | 1 line
    Shared Clipboard/VBoxClient: SCM fix.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/x11/VBoxClient/clipboard.cpp
+++ vbox/src/VBox/Additions/x11/VBoxClient/clipboard.cpp
@@ -1,4 +1,4 @@
-/** $Id: clipboard.cpp 82286 2019-11-29 16:08:08Z vboxsync $ */
+/** $Id: clipboard.cpp 82287 2019-11-29 16:18:10Z vboxsync $ */
 /** @file
  * Guest Additions - X11 Shared Clipboard.
  */
@@ -42,6 +42,7 @@
 #ifdef VBOX_WITH_SHARED_CLIPBOARD_FUSE
 # include "clipboard-fuse.h"
 #endif
+
 
 /*********************************************************************************************************************************
 *   Global Variables                                                                                                             *
