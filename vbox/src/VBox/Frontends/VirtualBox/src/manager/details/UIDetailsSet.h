0c9b5f04e7f422b816476c53bd7a147761364dd3
qzuser <395143360@qq.com>
2022-06-12 04:36:06 +0800

    commit_83352 ------------------------------------------------------------------------ r83352 | vboxsync | 2020-03-20 23:42:09 +0800 (Fri, 20 Mar 2020) | 1 line
    FE/Qt: bugref:9623: !VirtualBox Manager: Details pane: A set of details should handle enumeration signals of media which are directly related to it only; This reduces amount of spam messages unrelated to current set.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/manager/details/UIDetailsSet.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/manager/details/UIDetailsSet.h
@@ -1,4 +1,4 @@
-/* $Id: UIDetailsSet.h 83179 2020-02-28 15:29:09Z vboxsync $ */
+/* $Id: UIDetailsSet.h 83352 2020-03-20 15:42:09Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIDetailsSet class declaration.
  */
@@ -140,8 +140,8 @@ private slots:
 
     /** @name Item stuff.
       * @{ */
-        /** Updates item appearance. */
-        void sltUpdateAppearance();
+        /** Handles enumerated signal for medium with @a uId. */
+        void sltMediumEnumerated(const QUuid &uId);
     /** @} */
 
 private:
