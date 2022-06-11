c5b066f1dba2da475bbafb3f8380be513b3761a6
qzuser <395143360@qq.com>
2022-06-11 19:32:12 +0800

    commit_81344 ------------------------------------------------------------------------ r81344 | vboxsync | 2019-10-18 17:02:18 +0800 (Fri, 18 Oct 2019) | 2 lines
    EFI/Firmware: switch to "usual" variable handling, using NVRAM
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/EFI/Firmware/VBoxPkg/VBoxSysTables/VBoxSysTables.c
+++ vbox/src/VBox/Devices/EFI/Firmware/VBoxPkg/VBoxSysTables/VBoxSysTables.c
@@ -1,4 +1,4 @@
-/* $Id: VBoxSysTables.c 80934 2019-09-22 15:37:49Z vboxsync $ */
+/* $Id: VBoxSysTables.c 81344 2019-10-18 09:02:18Z vboxsync $ */
 /** @file
  * VBoxSysTables.c - VirtualBox system tables
  */
@@ -118,7 +118,7 @@ ConvertAndInstallTable(EFI_GUID* Guid, VOID* Ptr)
     EFI_STATUS  rc = EFI_SUCCESS;
 
     rc = ConvertSystemTable(Guid, &Ptr);
-    ASSERT_EFI_ERROR (rc);
+    //ASSERT_EFI_ERROR (rc);
 
     rc = gBS->InstallConfigurationTable(Guid, Ptr);
     ASSERT_EFI_ERROR (rc);
