c6311b16c8637c1774fb29c12a2fcc5c4a41cd71
qzuser <395143360@qq.com>
2022-06-11 12:54:03 +0800

    commit_79702 ------------------------------------------------------------------------ r79702 | vboxsync | 2019-07-12 03:34:05 +0800 (Fri, 12 Jul 2019) | 1 line
    Shared Clipboard/URI: Update.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/WINNT/VBoxTray/VBoxClipboard.cpp
+++ vbox/src/VBox/Additions/WINNT/VBoxTray/VBoxClipboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxClipboard.cpp 79672 2019-07-10 13:02:50Z vboxsync $ */
+/* $Id: VBoxClipboard.cpp 79702 2019-07-11 19:34:05Z vboxsync $ */
 /** @file
  * VBoxClipboard - Shared clipboard, Windows Guest Implementation.
  */
@@ -800,7 +800,8 @@ static LRESULT vboxClipboardWinProcessMsg(PVBOXCLIPBOARDCONTEXT pCtx, HWND hwnd,
 
                                if (RT_SUCCESS(rc))
                                {
-                                   rc = SharedClipboardURILTransferSetRoots(pTransfer, papszList, cbList);
+                                   rc = SharedClipboardURILTransferSetRoots(pTransfer,
+                                                                            papszList, cbList + 1 /* Include termination */);
                                    if (RT_SUCCESS(rc))
                                    {
                                        PVBOXCLIPBOARDURIWRITETHREADCTX pThreadCtx
