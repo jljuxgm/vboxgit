c3e1c59c4ee4704ad8422f7ddd5dc2c7f9cf0ebf
qzuser <395143360@qq.com>
2022-06-13 05:02:14 +0800

    commit_88888 ------------------------------------------------------------------------ r88888 | vboxsync | 2021-05-06 07:40:45 +0800 (Thu, 06 May 2021) | 1 line
    DrvAudioVRDE: Fixed bug that snuk into drvAudioVrdeHA_StreamDestroy in r143712.  bugref:9890
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/DrvAudioVRDE.cpp
+++ vbox/src/VBox/Main/src-client/DrvAudioVRDE.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvAudioVRDE.cpp 88887 2021-05-05 23:38:58Z vboxsync $ */
+/* $Id: DrvAudioVRDE.cpp 88888 2021-05-05 23:40:45Z vboxsync $ */
 /** @file
  * VRDE audio backend for Main.
  */
@@ -414,7 +414,6 @@ static DECLCALLBACK(int) drvAudioVrdeHA_StreamDestroy(PPDMIHOSTAUDIO pInterface,
             pStreamVRDE->In.pCircBuf = NULL;
         }
     }
-    pDrv->pConsoleVRDPServer = NULL;   /** @todo r=bird: WTF? */
 
     return VINF_SUCCESS;
 }
