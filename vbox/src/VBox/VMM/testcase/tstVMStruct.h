0ae2d9edc0b3305ad0300e1a61c2204ba377a69f
qzuser <395143360@qq.com>
2022-06-12 22:47:16 +0800

    commit_87349 ------------------------------------------------------------------------ r87349 | vboxsync | 2021-01-21 20:11:34 +0800 (Thu, 21 Jan 2021) | 1 line
    VMM/CPUM: Kicked out CPUMR3RemEnter & CPUMR3RemLeave. bugref:9576
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/testcase/tstVMStruct.h
+++ vbox/src/VBox/VMM/testcase/tstVMStruct.h
@@ -1,4 +1,4 @@
-/* $Id: tstVMStruct.h 86098 2020-09-13 07:17:19Z vboxsync $ */
+/* $Id: tstVMStruct.h 87349 2021-01-21 12:11:34Z vboxsync $ */
 /** @file
  * tstVMMStruct - Statements for generating VM and VMCPU offset and size tests.
  *
@@ -53,7 +53,7 @@
     GEN_CHECK_OFF(CPUMCPU, fApicDisVectors);
     GEN_CHECK_OFF(CPUMCPU, fX2Apic);
 #endif
-    GEN_CHECK_OFF(CPUMCPU, fRemEntered);
+    GEN_CHECK_OFF(CPUMCPU, fCpuIdApicFeatureVisible);
 
     GEN_CHECK_SIZE(CPUMHOSTCTX);
     GEN_CHECK_OFF(CPUMHOSTCTX, pXStateR3);
