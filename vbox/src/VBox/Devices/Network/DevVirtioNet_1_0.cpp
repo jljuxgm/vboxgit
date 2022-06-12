9beca7e6d916f67b6eed64b390653857f19a897d
qzuser <395143360@qq.com>
2022-06-12 09:32:38 +0800

    commit_84804 ------------------------------------------------------------------------ r84804 | vboxsync | 2020-06-12 14:27:21 +0800 (Fri, 12 Jun 2020) | 1 line
    Fix burns
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Network/DevVirtioNet_1_0.cpp
+++ vbox/src/VBox/Devices/Network/DevVirtioNet_1_0.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVirtioNet_1_0.cpp 84803 2020-06-12 06:17:35Z vboxsync $ $Revision: 84803 $ $Date: 2020-06-12 14:17:35 +0800 (Fri, 12 Jun 2020) $ $Author: vboxsync $ */
+/* $Id: DevVirtioNet_1_0.cpp 84804 2020-06-12 06:27:21Z vboxsync $ $Revision: 84804 $ $Date: 2020-06-12 14:27:21 +0800 (Fri, 12 Jun 2020) $ $Author: vboxsync $ */
 
 /** @file
  * VBox storage devices - Virtio NET Driver
@@ -662,7 +662,7 @@ static DECLCALLBACK(void) virtioNetVirtqNotified(PPDMDEVINS pDevIns, PVIRTIOCORE
         }
     }
     else
-        LogRelFunc(("%s unrecognized queue %s (idx=%d) notified\n", pVirtq->szName, idxVirtq));
+        LogRelFunc(("%s unrecognized queue %s (idx=%d) notified\n", pThis->szInst, pVirtq->szName, idxVirtq));
 }
 
 #ifdef IN_RING3 /* spans most of the file, at the moment. */
