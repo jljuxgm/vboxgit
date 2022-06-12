0dfaf1940357d1d2f8029f1e698f9e3d722cbe60
qzuser <395143360@qq.com>
2022-06-12 18:43:32 +0800

    commit_86767 ------------------------------------------------------------------------ r86767 | vboxsync | 2020-10-30 19:34:06 +0800 (Fri, 30 Oct 2020) | 1 line
    FE/Qt: bugref:9653: !VirtualBox Manager: Chooser pane: Small cleanup for unused stuff.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserItemGroup.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIChooserItemGroup.cpp 86744 2020-10-28 17:35:28Z vboxsync $ */
+/* $Id: UIChooserItemGroup.cpp 86767 2020-10-30 11:34:06Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIChooserItemGroup class implementation.
  */
@@ -1034,18 +1034,6 @@ void UIChooserItemGroup::sltGroupToggleFinish(bool fToggled)
 
     /* Toggle finished: */
     emit sigToggleFinished();
-}
-
-void UIChooserItemGroup::sltIndentRoot()
-{
-    /* Unhover before indenting: */
-    setHovered(false);
-}
-
-void UIChooserItemGroup::sltUnindentRoot()
-{
-    /* Unhover before unindenting: */
-    setHovered(false);
 }
 
 void UIChooserItemGroup::prepare()
