a48eb53bbe1e74c29a66058f96ebbe75e7a514b1
qzuser <395143360@qq.com>
2022-06-12 16:40:07 +0800

    commit_86329 ------------------------------------------------------------------------ r86329 | vboxsync | 2020-09-29 00:40:45 +0800 (Tue, 29 Sep 2020) | 1 line
    Debugger: More doxygen fixes for r140613
    ------------------------------------------------------------------------

--- vbox/src/VBox/Debugger/VBoxDbgConsole.h
+++ vbox/src/VBox/Debugger/VBoxDbgConsole.h
@@ -1,4 +1,4 @@
-/* $Id: VBoxDbgConsole.h 86327 2020-09-28 16:20:50Z vboxsync $ */
+/* $Id: VBoxDbgConsole.h 86329 2020-09-28 16:40:45Z vboxsync $ */
 /** @file
  * VBox Debugger GUI - Console.
  */
@@ -316,7 +316,7 @@ protected:
     static DECLCALLBACK(int) backWrite(PCDBGCIO pIo, const void *pvBuf, size_t cbBuf, size_t *pcbWritten);
 
     /**
-     * @copydoc DBGCIO::PfnSetReady
+     * @copydoc DBGCIO::pfnSetReady
      */
     static DECLCALLBACK(void) backSetReady(PCDBGCIO pIo, bool fReady);
 
