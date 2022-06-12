4555f7ac31eb0e95310a4a7cdaf13e8df76b36ba
qzuser <395143360@qq.com>
2022-06-12 19:36:21 +0800

    commit_86979 ------------------------------------------------------------------------ r86979 | vboxsync | 2020-11-26 15:04:40 +0800 (Thu, 26 Nov 2020) | 1 line
    FE/Qt: bugref:9831. Enabling F1 button for help in wizards
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/globals/UIMessageCenter.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/globals/UIMessageCenter.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIMessageCenter.cpp 86963 2020-11-24 09:43:55Z vboxsync $ */
+/* $Id: UIMessageCenter.cpp 86979 2020-11-26 07:04:40Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIMessageCenter class implementation.
  */
@@ -3462,7 +3462,7 @@ void UIMessageCenter::sltHelpBrowserClosed()
 void UIMessageCenter::sltHandleDialogHelpButtonPress()
 {
 # if defined(VBOX_WITH_DOCS_QHELP) && (QT_VERSION >= QT_VERSION_CHECK(5, 9, 0))
-    QAbstractButton *pSender = qobject_cast<QAbstractButton*>(sender());
+    QWidget *pSender = qobject_cast<QWidget*>(sender());
     if (!pSender)
         return;
     QVariant keyWordProp = pSender->property("helptag");
