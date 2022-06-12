b232faa31b2551f25cf4ac3a86ed807f62f1e5eb
qzuser <395143360@qq.com>
2022-06-13 01:04:31 +0800

    commit_87731 ------------------------------------------------------------------------ r87731 | vboxsync | 2021-02-12 19:00:29 +0800 (Fri, 12 Feb 2021) | 1 line
    FE/Qt: Working on the error indicators of UIUserNamePasswordEditor
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageExpert.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageExpert.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMPageExpert.cpp 87724 2021-02-11 15:21:12Z vboxsync $ */
+/* $Id: UIWizardNewVMPageExpert.cpp 87731 2021-02-12 11:00:29Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMPageExpert class implementation.
  */
@@ -22,6 +22,7 @@
 #include <QGroupBox>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QLineEdit>
 #include <QRadioButton>
 #include <QSpinBox>
 #include <QVBoxLayout>
