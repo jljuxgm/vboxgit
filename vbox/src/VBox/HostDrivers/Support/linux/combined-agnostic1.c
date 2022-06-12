b3e2e4bf52f76f1dc6afb5f953a9aa8f730c2507
qzuser <395143360@qq.com>
2022-06-12 12:23:25 +0800

    commit_85523 ------------------------------------------------------------------------ r85523 | vboxsync | 2020-07-29 19:54:52 +0800 (Wed, 29 Jul 2020) | 1 line
    SUPDrv/linux: Use two combined-agnostic.c files to undo the build time regression on systems with lots of cores. bugref:9798
    ------------------------------------------------------------------------

--- vbox/src/VBox/HostDrivers/Support/linux/combined-agnostic1.c
+++ vbox/src/VBox/HostDrivers/Support/linux/combined-agnostic1.c
@@ -1,4 +1,4 @@
-/* $Id: combined-agnostic.c 85519 2020-07-29 11:24:55Z vboxsync $ */
+/* $Id: combined-agnostic1.c 85523 2020-07-29 11:54:52Z vboxsync $ */
 /** @file
  * SUPDrv - Combine a bunch of OS agnostic sources into one compile unit.
  */
@@ -84,80 +84,4 @@
 #include "common/misc/handletablectx.c"
 #undef LOG_GROUP
 #include "common/misc/thread.c"
-#undef LOG_GROUP
-#include "common/string/RTStrCat.c"
-#undef LOG_GROUP
-#include "common/string/RTStrCopy.c"
-#undef LOG_GROUP
-#include "common/string/RTStrCopyEx.c"
-#undef LOG_GROUP
-#include "common/string/RTStrCopyP.c"
-#undef LOG_GROUP
-#include "common/string/RTStrEnd.c"
-#undef LOG_GROUP
-#include "common/string/RTStrNCmp.c"
-#undef LOG_GROUP
-#include "common/string/RTStrNLen.c"
-#undef LOG_GROUP
-#include "common/string/stringalloc.c"
-#undef LOG_GROUP
-#include "common/string/strformat.c"
-#undef LOG_GROUP
-#include "common/string/strformatnum.c"
-#undef LOG_GROUP
-#include "common/string/strformattype.c"
-#undef LOG_GROUP
-#include "common/string/strprintf.c"
-#undef LOG_GROUP
-#include "common/string/strtonum.c"
-#undef LOG_GROUP
-#include "common/table/avlpv.c"
-#undef LOG_GROUP
-#include "common/time/time.c"
-#undef LOG_GROUP
-#include "generic/RTAssertShouldPanic-generic.c"
-#undef LOG_GROUP
-#include "generic/RTLogWriteStdErr-stub-generic.c"
-#undef LOG_GROUP
-#include "generic/RTLogWriteStdOut-stub-generic.c"
-#undef LOG_GROUP
-#include "generic/RTLogWriteUser-generic.c"
-#undef LOG_GROUP
-#include "generic/RTMpGetArraySize-generic.c"
-#undef LOG_GROUP
-#include "generic/RTMpGetCoreCount-generic.c"
-#undef LOG_GROUP
-#include "generic/RTSemEventWait-2-ex-generic.c"
-#undef LOG_GROUP
-#include "generic/RTSemEventWaitNoResume-2-ex-generic.c"
-#undef LOG_GROUP
-#include "generic/RTSemEventMultiWait-2-ex-generic.c"
-#undef LOG_GROUP
-#include "generic/RTSemEventMultiWaitNoResume-2-ex-generic.c"
-#undef LOG_GROUP
-#include "generic/RTTimerCreate-generic.c"
-#undef LOG_GROUP
-#include "generic/errvars-generic.c"
-#undef LOG_GROUP
-#include "generic/mppresent-generic.c"
-#undef LOG_GROUP
-#include "generic/uuid-generic.c"
-#undef LOG_GROUP
-#include "VBox/log-vbox.c"
-
-#ifdef RT_ARCH_AMD64
-# undef LOG_GROUP
-# include "common/alloc/heapsimple.c"
-#endif
-
-#if 0 /*def RT_ARCH_X86 - iprt/nocrt/limit.h clashes.*/
-# include "common/math/gcc/divdi3.c"
-# include "common/math/gcc/moddi3.c"
-# include "common/math/gcc/udivdi3.c"
-# include "common/math/gcc/udivmoddi4.c"
-# include "common/math/gcc/umoddi3.c"
-# include "common/math/gcc/qdivrem.c"
-#endif
-
-
 
