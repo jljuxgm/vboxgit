204ff332feb5efeedb21a345611679709d03cb55
qzuser <395143360@qq.com>
2022-06-12 17:08:09 +0800

    commit_86441 ------------------------------------------------------------------------ r86441 | vboxsync | 2020-10-04 20:05:24 +0800 (Sun, 04 Oct 2020) | 1 line
    tstCollector: file header
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/testcase/tstCollector.cpp
+++ vbox/src/VBox/Main/testcase/tstCollector.cpp
@@ -1,8 +1,6 @@
-/* $Id: tstCollector.cpp 82968 2020-02-04 10:35:17Z vboxsync $ */
-
+/* $Id: tstCollector.cpp 86441 2020-10-04 12:05:24Z vboxsync $ */
 /** @file
- *
- * Collector classes test cases.
+ * VirtualBox Main - Performance collector classes test cases.
  */
 
 /*
