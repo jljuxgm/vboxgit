6a9f688ee8b48c9078309c23649f391b7b7eda1f
qzuser <395143360@qq.com>
2022-06-13 02:59:48 +0800

    commit_88229 ------------------------------------------------------------------------ r88229 | vboxsync | 2021-03-22 17:33:04 +0800 (Mon, 22 Mar 2021) | 1 line
    Audio: Renamed DevHDA.cpp to DevHda.cpp. bugref:9890
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Audio/DevHda.cpp
+++ vbox/src/VBox/Devices/Audio/DevHda.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevHDA.cpp 88228 2021-03-22 09:28:22Z vboxsync $ */
+/* $Id: DevHda.cpp 88229 2021-03-22 09:33:04Z vboxsync $ */
 /** @file
  * DevHDA.cpp - VBox Intel HD Audio Controller.
  *
