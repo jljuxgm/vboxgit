bc7af029dec5115e31d6e4678ef86a582146b768
qzuser <395143360@qq.com>
2022-06-13 03:19:11 +0800

    commit_88337 ------------------------------------------------------------------------ r88337 | vboxsync | 2021-04-01 02:17:20 +0800 (Thu, 01 Apr 2021) | 1 line
    kBuild 3488 -> 3496: DTrace fix and some macOS updates.
    ------------------------------------------------------------------------

--- vbox/kBuild/env.sh
+++ vbox/kBuild/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: env.sh 3295 2019-01-10 16:35:56Z bird $
+# $Id: env.sh 3492 2020-12-23 13:59:02Z bird $
 ## @file
 # Environment setup script.
 #
@@ -365,7 +365,7 @@ if test -z "$KBUILD_HOST_ARCH"; then
         aarch32|arm|arm1|arm2|arm3|arm6|armv1|armv2|armv3*|armv4*|armv5*|armv6*|armv7*|armv8*)
             KBUILD_HOST_ARCH='arm32'
             ;;
-        aarch64*)
+        aarch64*|arm64) # (Apple M1 is arm64.)
             KBUILD_HOST_ARCH='arm64'
             ;;
         hppa32|parisc32|parisc)
