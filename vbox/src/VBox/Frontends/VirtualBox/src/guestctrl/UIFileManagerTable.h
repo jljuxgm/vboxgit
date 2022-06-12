083e7a53ccc4716246d4a4f3af7ab932d50f8f73
qzuser <395143360@qq.com>
2022-06-13 01:01:11 +0800

    commit_87718 ------------------------------------------------------------------------ r87718 | vboxsync | 2021-02-11 16:50:48 +0800 (Thu, 11 Feb 2021) | 1 line
    FE/Qt: bugref:9532: Qt 5.15.2 migration: Fix obsolete Qt::WindowFlags init.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerTable.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerTable.h
@@ -1,4 +1,4 @@
-/* $Id: UIFileManagerTable.h 86233 2020-09-23 12:10:51Z vboxsync $ */
+/* $Id: UIFileManagerTable.h 87718 2021-02-11 08:50:48Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIFileManagerTable class declaration.
  */
@@ -113,7 +113,7 @@ class UIPropertiesDialog : public QIDialog
 
 public:
 
-    UIPropertiesDialog(QWidget *pParent = 0, Qt::WindowFlags flags = 0);
+    UIPropertiesDialog(QWidget *pParent = 0, Qt::WindowFlags enmFlags = Qt::WindowFlags());
     void setPropertyText(const QString &strProperty);
     void addDirectoryStatistics(UIDirectoryStatistics statictics);
 
