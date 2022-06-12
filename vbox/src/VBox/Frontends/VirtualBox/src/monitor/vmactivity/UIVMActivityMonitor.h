0b8707725f5439a5a7a9465e4740a30df5ab3006
qzuser <395143360@qq.com>
2022-06-13 02:27:35 +0800

    commit_88073 ------------------------------------------------------------------------ r88073 | vboxsync | 2021-03-11 01:29:56 +0800 (Thu, 11 Mar 2021) | 1 line
    FE/Qt: scm fix
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/monitor/vmactivity/UIVMActivityMonitor.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/monitor/vmactivity/UIVMActivityMonitor.h
@@ -1,4 +1,4 @@
-/* $Id: UIVMActivityMonitor.h 88071 2021-03-10 15:10:39Z vboxsync $ */
+/* $Id: UIVMActivityMonitor.h 88073 2021-03-10 17:29:56Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIVMActivityMonitor class declaration.
  */
@@ -15,8 +15,8 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
-#ifndef FEQT_INCLUDED_SRC_monitor_performance_UIVMActivityMonitor_h
-#define FEQT_INCLUDED_SRC_monitor_performance_UIVMActivityMonitor_h
+#ifndef FEQT_INCLUDED_SRC_monitor_vmactivity_UIVMActivityMonitor_h
+#define FEQT_INCLUDED_SRC_monitor_vmactivity_UIVMActivityMonitor_h
 #ifndef RT_WITHOUT_PRAGMA_ONCE
 # pragma once
 #endif
@@ -252,4 +252,4 @@ private:
     UIActionPool *m_pActionPool;
 };
 
-#endif /* !FEQT_INCLUDED_SRC_monitor_performance_UIVMActivityMonitor_h */
+#endif /* !FEQT_INCLUDED_SRC_monitor_vmactivity_UIVMActivityMonitor_h */
