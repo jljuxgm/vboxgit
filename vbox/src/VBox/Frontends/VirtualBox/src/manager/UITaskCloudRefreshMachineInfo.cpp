980d018cd24cb834b7379863af4c6c1e48cd65a2
qzuser <395143360@qq.com>
2022-06-12 16:05:52 +0800

    commit_86200 ------------------------------------------------------------------------ r86200 | vboxsync | 2020-09-21 22:34:41 +0800 (Mon, 21 Sep 2020) | 1 line
    FE/Qt: bugref:9653: UITaskCloudRefreshMachineInfo: Allow to cancel cloud machine refresh task.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/manager/UITaskCloudRefreshMachineInfo.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/manager/UITaskCloudRefreshMachineInfo.cpp
@@ -1,4 +1,4 @@
-/* $Id: UITaskCloudRefreshMachineInfo.cpp 86199 2020-09-21 14:33:29Z vboxsync $ */
+/* $Id: UITaskCloudRefreshMachineInfo.cpp 86200 2020-09-21 14:34:41Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UITaskCloudRefreshMachineInfo class implementation.
  */
@@ -26,6 +26,11 @@ UITaskCloudRefreshMachineInfo::UITaskCloudRefreshMachineInfo(const CCloudMachine
     : UITask(Type_CloudRefreshMachineInfo)
     , m_comCloudMachine(comCloudMachine)
 {
+}
+
+void UITaskCloudRefreshMachineInfo::cancel()
+{
+    m_comCloudMachineRefreshProgress.Cancel();
 }
 
 QString UITaskCloudRefreshMachineInfo::errorInfo() const
