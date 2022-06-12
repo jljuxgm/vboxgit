dc1619374643365f25a1cb3191094e91b0383638
qzuser <395143360@qq.com>
2022-06-13 04:23:48 +0800

    commit_88678 ------------------------------------------------------------------------ r88678 | vboxsync | 2021-04-23 21:33:28 +0800 (Fri, 23 Apr 2021) | 1 line
    FE/Qt: bugref:8161: Fixing regression in r143970; We should detach UISession COM stuff before COM itself is cleaned up; Otherwise any VM appears in aborted state on XPCOM hosts.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UISession.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UISession.h
@@ -1,4 +1,4 @@
-/* $Id: UISession.h 88633 2021-04-21 12:43:05Z vboxsync $ */
+/* $Id: UISession.h 88678 2021-04-23 13:33:28Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UISession class declaration.
  */
@@ -364,6 +364,8 @@ private slots:
     /** Marks machine started. */
     void sltMarkInitialized() { m_fInitialized = true; }
 
+    /** Detaches COM. */
+    void sltDetachCOM();
     /** Close Runtime UI. */
     void sltCloseRuntimeUI();
 
