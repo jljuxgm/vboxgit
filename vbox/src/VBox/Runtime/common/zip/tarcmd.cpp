0b3f46afa50ad1cf44e121b90d871cca3f8ee641
qzuser <395143360@qq.com>
2022-06-12 15:18:17 +0800

    commit_86036 ------------------------------------------------------------------------ r86036 | vboxsync | 2020-09-07 04:33:03 +0800 (Mon, 07 Sep 2020) | 1 line
    Runtime/cpiovfs.cpp: A simple CPIO archive reader (WIP, writer comes next and then some cleanup) [build fix]
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/zip/tarcmd.cpp
+++ vbox/src/VBox/Runtime/common/zip/tarcmd.cpp
@@ -1,4 +1,4 @@
-/* $Id: tarcmd.cpp 86035 2020-09-06 20:31:16Z vboxsync $ */
+/* $Id: tarcmd.cpp 86036 2020-09-06 20:33:03Z vboxsync $ */
 /** @file
  * IPRT - A mini TAR Command.
  */
@@ -76,7 +76,7 @@ typedef enum RTZIPTARCMDFORMAT
     /** XAR.  */
     RTZIPTARCMDFORMAT_XAR,
     /** CPIO. */
-    RTZIPTARCMDFORMAT_CPIO,
+    RTZIPTARCMDFORMAT_CPIO
 } RTZIPTARCMDFORMAT;
 
 
