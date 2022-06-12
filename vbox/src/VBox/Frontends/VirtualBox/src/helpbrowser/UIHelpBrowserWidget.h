1b12654b233e578ed5b7795713786bf1dc051839
qzuser <395143360@qq.com>
2022-06-13 06:33:06 +0800

    commit_89318 ------------------------------------------------------------------------ r89318 | vboxsync | 2021-05-27 21:02:27 +0800 (Thu, 27 May 2021) | 1 line
    FE/Qt: bugref:9831. Showing zoom percentage in the status bar
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/helpbrowser/UIHelpBrowserWidget.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/helpbrowser/UIHelpBrowserWidget.h
@@ -1,4 +1,4 @@
-/* $Id: UIHelpBrowserWidget.h 88550 2021-04-15 16:45:15Z vboxsync $ */
+/* $Id: UIHelpBrowserWidget.h 89318 2021-05-27 13:02:27Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIHelpBrowserWidget class declaration.
  */
@@ -57,6 +57,7 @@ signals:
     void sigCloseDialog();
     void sigLinkHighlighted(const QString &strLink);
     void sigStatusBarVisible(bool fToggled);
+    void sigZoomPercentageChanged(int iPercentage);
 
 public:
 
@@ -67,6 +68,7 @@ public:
 #ifdef VBOX_WS_MAC
     QIToolBar *toolbar() const { return m_pToolBar; }
 #endif
+    int zoomPercentage() const;
 
 protected:
 
