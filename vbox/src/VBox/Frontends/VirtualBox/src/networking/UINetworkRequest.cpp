d1482575062d2664d7a8228caaeca215978a7042
qzuser <395143360@qq.com>
2022-06-13 11:20:33 +0800

    commit_90526 ------------------------------------------------------------------------ r90526 | vboxsync | 2021-08-05 19:21:32 +0800 (Thu, 05 Aug 2021) | 1 line
    FE/Qt: bugref:10067: Move request registration stuff to appropriate place from UINetworkRequest to UINetworkRequestManager.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/networking/UINetworkRequest.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/networking/UINetworkRequest.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINetworkRequest.cpp 90525 2021-08-05 10:14:37Z vboxsync $ */
+/* $Id: UINetworkRequest.cpp 90526 2021-08-05 11:21:32Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UINetworkRequest class implementation.
  */
@@ -186,9 +186,6 @@ void UINetworkRequest::prepare()
     m_iUrlIndex = 0;
     m_url = m_urls.at(m_iUrlIndex);
 
-    /* Register network-request in network-manager: */
-    manager()->registerNetworkRequest(this);
-
     /* Prepare network-reply: */
     prepareNetworkReply();
 }
@@ -228,7 +225,4 @@ void UINetworkRequest::cleanup()
 {
     /* Cleanup network-reply: */
     cleanupNetworkReply();
-
-    /* Unregister network-request from network-manager: */
-    manager()->unregisterNetworkRequest(m_uuid);
 }
