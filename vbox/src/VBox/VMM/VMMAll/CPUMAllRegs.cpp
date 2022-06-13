379fa75bfed3ca4175430f705c6817227c4c73bb
qzuser <395143360@qq.com>
2022-06-13 17:30:33 +0800

    commit_91951 ------------------------------------------------------------------------ r91951 | vboxsync | 2021-10-21 21:55:49 +0800 (Thu, 21 Oct 2021) | 2 lines
    VMM: Nested VMX: bugref:10092 Added CPUMIsGuestVmxEptPagingEnabledEx for inline'd access from IEM.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
+++ vbox/src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
@@ -1,4 +1,4 @@
-/* $Id: CPUMAllRegs.cpp 91710 2021-10-13 11:05:26Z vboxsync $ */
+/* $Id: CPUMAllRegs.cpp 91951 2021-10-21 13:55:49Z vboxsync $ */
 /** @file
  * CPUM - CPU Monitor(/Manager) - Getters and Setters.
  */
@@ -3041,7 +3041,6 @@ VMM_INT_DECL(int) CPUMGetSvmMsrpmOffsetAndBit(uint32_t idMsr, uint16_t *pbOffMsr
  */
 VMM_INT_DECL(bool) CPUMIsGuestVmxEptPagingEnabled(PCVMCPUCC pVCpu)
 {
-    return    CPUMIsGuestInVmxNonRootMode(&pVCpu->cpum.s.Guest)
-           && CPUMIsGuestVmxProcCtls2Set(&pVCpu->cpum.s.Guest, VMX_PROC_CTLS2_EPT);
+    return CPUMIsGuestVmxEptPagingEnabledEx(&pVCpu->cpum.s.Guest);
 }
 
