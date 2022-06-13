01774e0a01eb6705f2f13a228887b57720849413
qzuser <395143360@qq.com>
2022-06-13 17:11:15 +0800

    commit_91858 ------------------------------------------------------------------------ r91858 | vboxsync | 2021-10-20 15:25:21 +0800 (Wed, 20 Oct 2021) | 1 line
    Audio/ALSA: Temporary instrumentation build for investigating munmap_chunk() / double free or corruption issues. ​bugref:10008
    ------------------------------------------------------------------------

--- vbox/src/VBox/ValidationKit/utils/audio/vkatCommon.cpp
+++ vbox/src/VBox/ValidationKit/utils/audio/vkatCommon.cpp
@@ -1,4 +1,4 @@
-/* $Id: vkatCommon.cpp 91834 2021-10-19 08:04:08Z vboxsync $ */
+/* $Id: vkatCommon.cpp 91858 2021-10-20 07:25:21Z vboxsync $ */
 /** @file
  * Validation Kit Audio Test (VKAT) - Self test code.
  */
@@ -693,8 +693,8 @@ int audioTestPlayTone(PAUDIOTESTIOOPTS pIoOpts, PAUDIOTESTENV pTstEnv, PAUDIOTES
 
         if (RT_SUCCESS(rc))
         {
-            RTTestPrintf(g_hTest, RTTESTLVL_ALWAYS, "Draining stream ...\n");
-            rc = AudioTestMixStreamDrain(&pStream->Mix, true /*fSync*/);
+            RTTestPrintf(g_hTest, RTTESTLVL_ALWAYS, "Skipping draining stream!\n");
+            //rc = AudioTestMixStreamDrain(&pStream->Mix, true /*fSync*/);
         }
     }
     else
