72f47d57300fd685524dedb8e4c0a065be44e2b8
qzuser <395143360@qq.com>
2022-06-13 12:17:12 +0800

    commit_90791 ------------------------------------------------------------------------ r90791 | vboxsync | 2021-08-23 18:28:24 +0800 (Mon, 23 Aug 2021) | 1 line
    Devices: More VALID_PTR -> RT_VALID_PTR/AssertPtr.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/USB/VUSBInternal.h
+++ vbox/src/VBox/Devices/USB/VUSBInternal.h
@@ -1,4 +1,4 @@
-/* $Id: VUSBInternal.h 90049 2021-07-06 10:23:26Z vboxsync $ */
+/* $Id: VUSBInternal.h 90791 2021-08-23 10:28:24Z vboxsync $ */
 /** @file
  * Virtual USB - Internal header.
  *
@@ -616,7 +616,7 @@ DECLINLINE(void) vusbUrbUnlink(PVUSBURB pUrb)
  */
 #ifdef VBOX_STRICT
 # define vusbUrbAssert(pUrb) do { \
-    AssertMsg(VALID_PTR((pUrb)),  ("%p\n", (pUrb))); \
+    AssertPtr((pUrb)); \
     AssertMsg((pUrb)->u32Magic == VUSBURB_MAGIC, ("%#x", (pUrb)->u32Magic)); \
     AssertMsg((pUrb)->enmState > VUSBURBSTATE_INVALID && (pUrb)->enmState < VUSBURBSTATE_END, \
               ("%d\n", (pUrb)->enmState)); \
