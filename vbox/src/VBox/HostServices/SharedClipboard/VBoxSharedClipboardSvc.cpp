ed8fdbbcbc275f7ad2a20301aad0ff4f805c0607
qzuser <395143360@qq.com>
2022-06-13 20:34:49 +0800

    commit_92785 ------------------------------------------------------------------------ r92785 | vboxsync | 2021-12-07 20:13:49 +0800 (Tue, 07 Dec 2021) | 5 lines
    Host Services: Shared Clipboard: reference counting extra fix for events produced by ShClEventSourceGenerateAndRegisterEvent(), bugref:10160.
    
    Do not release event reference when signalling that data has arrived. Event
    reference should be released by function which waits for it.
    
    ------------------------------------------------------------------------

--- vbox/src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc.cpp
+++ vbox/src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxSharedClipboardSvc.cpp 92772 2021-12-06 19:09:30Z vboxsync $ */
+/* $Id: VBoxSharedClipboardSvc.cpp 92785 2021-12-07 12:13:49Z vboxsync $ */
 /** @file
  * Shared Clipboard Service - Host service entry points.
  */
@@ -1383,11 +1383,6 @@ int ShClSvcGuestDataSignal(PSHCLCLIENT pClient, PSHCLCLIENTCMDCTX pCmdCtx, SHCLF
         ShClPayloadFree(pPayload);
         LogRel(("Shared Clipboard: Signalling of guest clipboard data to the host failed: %Rrc\n", rc));
     }
-
-    /*
-     * Release reference (and free it if not used anymore).
-     */
-    ShClEventRelease(pEvent);
 
     LogFlowFuncLeaveRC(rc);
     return rc;
