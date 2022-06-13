88bd473d424e35c87391af89740b7c512bd28ba8
qzuser <395143360@qq.com>
2022-06-13 10:45:55 +0800

    commit_90370 ------------------------------------------------------------------------ r90370 | vboxsync | 2021-07-28 22:18:54 +0800 (Wed, 28 Jul 2021) | 1 line
    FE/Qt: bugref:9996. Fixing medium format and variant connections
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvd/UIWizardNewVDPageFileType.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvd/UIWizardNewVDPageFileType.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVDPageFileType.h 90356 2021-07-27 14:06:35Z vboxsync $ */
+/* $Id: UIWizardNewVDPageFileType.h 90370 2021-07-28 14:18:54Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVDPageFileType class declaration.
  */
@@ -37,9 +37,6 @@ class UIDiskFormatsGroupBox;
 
 namespace UIWizardNewVDFileType
 {
-//     void addFormatButton(QWidget *pParent, QVBoxLayout *pFormatsLayout, CMediumFormat medFormat, bool fPreferred = false);
-
-   // QWidget *createFormatButtonGroup(QButtonGroup *&pFormatButtonGroup, bool fExperMode);
 
 //     CMediumFormat mediumFormat() const;
 //     void setMediumFormat(const CMediumFormat &mediumFormat);
@@ -61,6 +58,10 @@ public:
 
     /* Constructor: */
     UIWizardNewVDPageFileType();
+
+private slots:
+
+    void sltMediumFormatChanged();
 
 private:
 
