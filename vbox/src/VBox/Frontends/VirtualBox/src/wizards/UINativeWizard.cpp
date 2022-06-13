bdb9c670d1b405fffcf414fb1f9274eda90c1291
qzuser <395143360@qq.com>
2022-06-13 17:47:18 +0800

    commit_92035 ------------------------------------------------------------------------ r92035 | vboxsync | 2021-10-25 21:38:29 +0800 (Mon, 25 Oct 2021) | 1 line
    FE/Qt: bugref:10067: UINativeWizard: Extend UINativeWizard with public API to access his local notification-center.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/UINativeWizard.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/UINativeWizard.cpp
@@ -1,4 +1,4 @@
-/* $Id: UINativeWizard.cpp 91877 2021-10-20 11:26:48Z vboxsync $ */
+/* $Id: UINativeWizard.cpp 92035 2021-10-25 13:38:29Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UINativeWizard class implementation.
  */
@@ -78,6 +78,11 @@ void UIFrame::paintEvent(QPaintEvent *pEvent)
 }
 #endif /* VBOX_WS_MAC */
 
+
+UINotificationCenter *UINativeWizard::notificationCenter() const
+{
+    return m_pNotificationCenter;
+}
 
 bool UINativeWizard::handleNotificationProgressNow(UINotificationProgress *pProgress)
 {
