3e3e5832e8f52e2cfe78ee154f2bdaea459aa423
qzuser <395143360@qq.com>
2022-06-14 02:48:06 +0800

    commit_93985 ------------------------------------------------------------------------ r93985 | vboxsync | 2022-02-28 22:41:44 +0800 (Mon, 28 Feb 2022) | 1 line
    FE/Qt: scm fix. bugref:9898
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/softkeyboard/UISoftKeyboard.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/softkeyboard/UISoftKeyboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: UISoftKeyboard.cpp 93983 2022-02-28 14:37:43Z vboxsync $ */
+/* $Id: UISoftKeyboard.cpp 93985 2022-02-28 14:41:44Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UISoftKeyboard class implementation.
  */
@@ -3509,6 +3509,7 @@ QVector<QPointF> UIPhysicalLayoutReader::computeKeyVertices(const UISoftKeyboard
     }
     return vertices;
 }
+
 
 /*********************************************************************************************************************************
 *   UIKeyboardLayoutReader implementation.                                                                                       *
