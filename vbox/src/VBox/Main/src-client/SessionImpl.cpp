60ba144d6dbc459605c7c2f0de6817aa28ce899d
qzuser <395143360@qq.com>
2022-06-13 18:36:52 +0800

    commit_92274 ------------------------------------------------------------------------ r92274 | vboxsync | 2021-11-09 00:29:09 +0800 (Tue, 09 Nov 2021) | 1 line
    Main/Session::getRemoteConsole: Don't was space on a read-locking in the VBOX_COM_INPROC_API_CLIENT stub.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-client/SessionImpl.cpp
+++ vbox/src/VBox/Main/src-client/SessionImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: SessionImpl.cpp 92271 2021-11-08 14:47:09Z vboxsync $ */
+/* $Id: SessionImpl.cpp 92274 2021-11-08 16:29:09Z vboxsync $ */
 /** @file
  * VBox Client Session COM Class implementation in VBoxC.
  */
@@ -260,10 +260,9 @@ HRESULT Session::getPID(ULONG *aPid)
 HRESULT Session::getRemoteConsole(ComPtr<IConsole> &aConsole)
 {
     LogFlowThisFuncEnter();
-
+#ifndef VBOX_COM_INPROC_API_CLIENT
     AutoReadLock alock(this COMMA_LOCKVAL_SRC_POS);
 
-#ifndef VBOX_COM_INPROC_API_CLIENT
     if (mType == SessionType_WriteLock && !!mConsole)
     {
         /* return a failure if the session already transitioned to Closing
