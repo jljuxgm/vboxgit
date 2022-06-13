35d294f5215f46f0317a2e96060faee5d8503527
qzuser <395143360@qq.com>
2022-06-13 19:00:12 +0800

    commit_92385 ------------------------------------------------------------------------ r92385 | vboxsync | 2021-11-12 07:54:29 +0800 (Fri, 12 Nov 2021) | 1 line
    VMM/PGM: Some more large page stats. bugref:10093
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/include/PGMInternal.h
+++ vbox/src/VBox/VMM/include/PGMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: PGMInternal.h 92381 2021-11-11 20:47:44Z vboxsync $ */
+/* $Id: PGMInternal.h 92385 2021-11-11 23:54:29Z vboxsync $ */
 /** @file
  * PGM - Internal header file.
  */
@@ -3014,7 +3014,8 @@ typedef struct PGMSTATS
     STAMCOUNTER StatTrackOverflows;                 /**< The number of times the extent list grows to long. */
     STAMPROFILE StatTrackDeref;                     /**< Profiling of SyncPageWorkerTrackDeref (expensive). */
 
-    STAMPROFILE StatLargePageSetup;                 /**< Time spent setting up newly allocated large pages. */
+    STAMPROFILEADV StatLargePageAlloc2;            /**< Time spent setting up newly allocated large pages. */
+    STAMPROFILEADV StatLargePageSetup;             /**< Time spent setting up newly allocated large pages. */
     /** pgmPhysIsValidLargePage profiling - R3 */
     STAMPROFILE StatR3IsValidLargePage;
     /** pgmPhysIsValidLargePage profiling - RZ*/
