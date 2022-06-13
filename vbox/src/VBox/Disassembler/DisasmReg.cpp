770a1a0a8ace4edcf7c37f734838b36b3434a2ce
qzuser <395143360@qq.com>
2022-06-14 02:01:50 +0800

    commit_93746 ------------------------------------------------------------------------ r93746 | vboxsync | 2022-02-15 05:04:50 +0800 (Tue, 15 Feb 2022) | 1 line
    Disasm,iprt/cdefs.h: clang and arm64 adjustments. [scm] bugref:9898
    ------------------------------------------------------------------------

--- vbox/src/VBox/Disassembler/DisasmReg.cpp
+++ vbox/src/VBox/Disassembler/DisasmReg.cpp
@@ -1,4 +1,4 @@
-/* $Id: DisasmReg.cpp 93742 2022-02-14 20:59:08Z vboxsync $ */
+/* $Id: DisasmReg.cpp 93746 2022-02-14 21:04:50Z vboxsync $ */
 /** @file
  * VBox disassembler- Register Info Helpers.
  */
@@ -828,5 +828,4 @@ DISDECL(int) DISQueryParamRegPtr(PCPUMCTXCORE pCtx, PCDISSTATE pDis, PCDISOPPARA
     }
     return VERR_INVALID_PARAMETER;
 }
-
 
