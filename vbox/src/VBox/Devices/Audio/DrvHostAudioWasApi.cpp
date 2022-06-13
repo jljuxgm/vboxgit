41fe94c3870f917704450631638ea69b6dbceacd
qzuser <395143360@qq.com>
2022-06-13 08:09:16 +0800

    commit_89765 ------------------------------------------------------------------------ r89765 | vboxsync | 2021-06-17 21:38:32 +0800 (Thu, 17 Jun 2021) | 1 line
    DrvHostAudioWasApi: Must disable the stream when draining completes. bugref:9890
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Audio/DrvHostAudioWasApi.cpp
+++ vbox/src/VBox/Devices/Audio/DrvHostAudioWasApi.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostAudioWasApi.cpp 89596 2021-06-10 11:37:53Z vboxsync $ */
+/* $Id: DrvHostAudioWasApi.cpp 89765 2021-06-17 13:38:32Z vboxsync $ */
 /** @file
  * Host audio driver - Windows Audio Session API.
  */
@@ -2715,6 +2715,7 @@ static DECLCALLBACK(int) drvHostAudioWasHA_StreamPlay(PPDMIHOSTAUDIO pInterface,
             LogRelMax(64, ("WasAPI: Failed to stop draining stream '%s': %Rhrc\n", pStreamWas->Cfg.szName, hrc));
         pStreamWas->fDraining = false;
         pStreamWas->fStarted  = false;
+        pStreamWas->fEnabled  = false;
     }
 
     /*
