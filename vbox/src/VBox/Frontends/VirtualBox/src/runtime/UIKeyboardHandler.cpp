23730ee0432903f2884345c2707c04161d95757f
qzuser <395143360@qq.com>
2022-06-13 11:08:50 +0800

    commit_90475 ------------------------------------------------------------------------ r90475 | vboxsync | 2021-08-02 21:12:02 +0800 (Mon, 02 Aug 2021) | 1 line
    FE/Qt: Pre-clear the copy of the pressed keys, too. We may get into a situation where the host key is pressed before the VM ever started running.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIKeyboardHandler.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/runtime/UIKeyboardHandler.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIKeyboardHandler.cpp 90242 2021-07-19 14:31:08Z vboxsync $ */
+/* $Id: UIKeyboardHandler.cpp 90475 2021-08-02 13:12:02Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIKeyboardHandler class implementation.
  */
@@ -1011,6 +1011,7 @@ void UIKeyboardHandler::prepareCommon()
 
     /* Pressed keys: */
     ::memset(m_pressedKeys, 0, sizeof(m_pressedKeys));
+    ::memset(m_pressedKeysCopy, 0, sizeof(m_pressedKeysCopy));
 }
 
 void UIKeyboardHandler::loadSettings()
