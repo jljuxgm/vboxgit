8d4c606929d0a3edac89abe795af3932fa741c1b
qzuser <395143360@qq.com>
2022-06-13 08:40:38 +0800

    commit_89916 ------------------------------------------------------------------------ r89916 | vboxsync | 2021-06-26 00:12:46 +0800 (Sat, 26 Jun 2021) | 1 line
    FE/Qt: Adding a forgotten connection to the a button in the new vm wizard's expert mode
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageExpert.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageExpert.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMPageExpert.h 88837 2021-05-03 13:50:13Z vboxsync $ */
+/* $Id: UIWizardNewVMPageExpert.h 89916 2021-06-25 16:12:46Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMPageExpert class declaration.
  */
@@ -101,6 +101,7 @@ private slots:
     void sltMediumFormatChanged();
     void sltMediumSizeChanged();
     void sltSelectedDiskSourceChanged();
+    void sltSelectLocationButtonClicked();
 
 private:
 
