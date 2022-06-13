a680a10766f992c7f0e8c6820106f91612027fa6
qzuser <395143360@qq.com>
2022-06-13 10:02:28 +0800

    commit_90236 ------------------------------------------------------------------------ r90236 | vboxsync | 2021-07-19 19:13:47 +0800 (Mon, 19 Jul 2021) | 1 line
    FE/Qt: bugref:9996. Working on the isComplete stuff of the expert page.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/editors/UIHostnameDomainNameEditor.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/editors/UIHostnameDomainNameEditor.h
@@ -1,4 +1,4 @@
-/* $Id: UIHostnameDomainNameEditor.h 90157 2021-07-12 11:14:38Z vboxsync $ */
+/* $Id: UIHostnameDomainNameEditor.h 90236 2021-07-19 11:13:47Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIHostnameDomainNameEditor class declaration.
  */
@@ -56,6 +56,7 @@ public:
     QString hostnameDomainName() const;
 
     bool isComplete() const;
+    void mark();
 
 protected:
 
--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMUnattendedPageBasic.cpp
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMUnattendedPageBasic.cpp
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMUnattendedPageBasic.cpp 90232 2021-07-16 14:27:18Z vboxsync $ */
+/* $Id: UIWizardNewVMUnattendedPageBasic.cpp 90236 2021-07-19 11:13:47Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMUnattendedPageBasic class implementation.
  */
@@ -254,5 +254,5 @@ void UIWizardNewVMUnattendedPageBasic::markWidgets() const
 {
     UIWizardNewVM *pWizard = qobject_cast<UIWizardNewVM*>(wizard());
     if (pWizard && pWizard->installGuestAdditions() && m_pGAInstallationISOContainer)
-        m_pGAInstallationISOContainer->mark(!UIWizardNewVMUnattendedPage::checkGAISOFile(m_pGAInstallationISOContainer->path()));
+        m_pGAInstallationISOContainer->mark();
 }
