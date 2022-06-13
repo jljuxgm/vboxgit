370208757cb7103b6906c114c83cb722b51516ce
qzuser <395143360@qq.com>
2022-06-14 00:07:25 +0800

    commit_93300 ------------------------------------------------------------------------ r93300 | vboxsync | 2022-01-18 19:24:12 +0800 (Tue, 18 Jan 2022) | 1 line
    Add/common: VC++ 19.2 update 11 build adjustments. bugref:8489
    ------------------------------------------------------------------------

--- vbox/src/VBox/Additions/common/VBoxControl/VBoxControl.cpp
+++ vbox/src/VBox/Additions/common/VBoxControl/VBoxControl.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxControl.cpp 93115 2022-01-01 11:31:46Z vboxsync $ */
+/* $Id: VBoxControl.cpp 93300 2022-01-18 11:24:12Z vboxsync $ */
 /** @file
  * VBoxControl - Guest Additions Command Line Management Interface.
  */
@@ -945,7 +945,7 @@ struct
     DWORD xres;
     DWORD yres;
     DWORD bpp;
-} customModes[MAX_CUSTOM_MODES] = {0};
+} customModes[MAX_CUSTOM_MODES] = {{0}};
 
 void getCustomModes(HKEY hkeyVideo)
 {
