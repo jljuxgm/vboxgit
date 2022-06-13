75b4bc31649388f686d2b926ec4c2a14596c419a
qzuser <395143360@qq.com>
2022-06-13 12:08:22 +0800

    commit_90749 ------------------------------------------------------------------------ r90749 | vboxsync | 2021-08-19 22:47:55 +0800 (Thu, 19 Aug 2021) | 1 line
    FE/Qt: bugref:9996. Making wizard() member virtual.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/UINativeWizardPage.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/UINativeWizardPage.h
@@ -1,4 +1,4 @@
-/* $Id: UINativeWizardPage.h 90727 2021-08-18 16:09:34Z vboxsync $ */
+/* $Id: UINativeWizardPage.h 90749 2021-08-19 14:47:55Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UINativeWizardPage class declaration.
  */
@@ -71,7 +71,7 @@ public:
 protected:
 
     /** Returns wizard this page belongs to. */
-    UINativeWizard *wizard() const;
+    virtual UINativeWizard *wizard() const;
 
     /** Holds the page title. */
     QString  m_strTitle;
