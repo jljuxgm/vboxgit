02e13accca4698e5921f9f1b5ce4c0ddb89a625b
qzuser <395143360@qq.com>
2022-06-13 09:24:52 +0800

    commit_90064 ------------------------------------------------------------------------ r90064 | vboxsync | 2021-07-06 20:51:48 +0800 (Tue, 06 Jul 2021) | 1 line
    Runtime/RTEfi*: Start of a VFS module to access a EFI variable store stored in the NVRAM file to allow changing the firmware config without having the VM running, bugref:9580 [build fix]
    ------------------------------------------------------------------------

--- vbox/src/VBox/Runtime/common/efi/efivarstorevfs.cpp
+++ vbox/src/VBox/Runtime/common/efi/efivarstorevfs.cpp
@@ -1,4 +1,4 @@
-/* $Id: efivarstorevfs.cpp 90063 2021-07-06 12:50:55Z vboxsync $ */
+/* $Id: efivarstorevfs.cpp 90064 2021-07-06 12:51:48Z vboxsync $ */
 /** @file
  * IPRT - Expose a EFI variable store as a Virtual Filesystem.
  */
@@ -1051,7 +1051,7 @@ static DECLCALLBACK(int) rtEfiVarStoreDir_ReadDir(void *pvThis, PRTDIRENTRYEX pD
         {
             memcpy(&pDirEntry->szName[0], pszName, cbName);
             pDirEntry->szName[cbName] = '\0';
-            pDirEntry->cbName         = cbName;
+            pDirEntry->cbName         = (uint16_t)cbName;
             rc = rtEfiVarStore_QueryInfo(cbObject, fIsDir, &Time, &pDirEntry->Info, enmAddAttr);
             if (RT_SUCCESS(rc))
             {
