4a2b2d633cbc6e3995aec23761e9f3d9ab03f3c0
qzuser <395143360@qq.com>
2022-06-13 18:16:10 +0800

    commit_92170 ------------------------------------------------------------------------ r92170 | vboxsync | 2021-11-02 06:06:25 +0800 (Tue, 02 Nov 2021) | 1 line
    VMM/PGM,NEM: Let NEM handle dirty VRAM (MMIO2) page tracking.  Saves lots of exits when using the VBoxVGA device or VMSVGA in non-VMSVGA mode.  Added a 32-bit NEM field to the PGMRAMRANGE structure called uNemRange. bugref:10122
    ------------------------------------------------------------------------

--- vbox/src/VBox/VMM/include/NEMInternal.h
+++ vbox/src/VBox/VMM/include/NEMInternal.h
@@ -1,4 +1,4 @@
-/* $Id: NEMInternal.h 92120 2021-10-28 00:31:35Z vboxsync $ */
+/* $Id: NEMInternal.h 92170 2021-11-01 22:06:25Z vboxsync $ */
 /** @file
  * NEM - Internal header file.
  */
@@ -216,6 +216,15 @@ typedef struct NEM
 #  endif
     STAMCOUNTER                 StatMapPageFailed;
     STAMCOUNTER                 StatUnmapPageFailed;
+#  ifdef VBOX_WITH_PGM_NEM_MODE
+    STAMPROFILE                 StatProfMapGpaRange;
+    STAMPROFILE                 StatProfUnmapGpaRange;
+    STAMPROFILE                 StatProfQueryGpaRangeDirtyBitmap;
+#  endif
+#  ifndef NEM_WIN_USE_HYPERCALLS_FOR_PAGES
+    STAMPROFILE                 StatProfMapGpaRangePage;
+    STAMPROFILE                 StatProfUnmapGpaRangePage;
+#  endif
 
 #  ifdef NEM_WIN_USE_HYPERCALLS_FOR_PAGES
     /** Info about the VidGetHvPartitionId I/O control interface. */
