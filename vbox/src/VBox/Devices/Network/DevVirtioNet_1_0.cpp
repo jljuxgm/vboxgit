82c63f3ebb53342f1a8b5dbbd6eba4f46d448e22
qzuser <395143360@qq.com>
2022-06-13 21:53:10 +0800

    commit_93013 ------------------------------------------------------------------------ r93013 | vboxsync | 2021-12-17 18:11:56 +0800 (Fri, 17 Dec 2021) | 1 line
    Fix burn
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Network/DevVirtioNet_1_0.cpp
+++ vbox/src/VBox/Devices/Network/DevVirtioNet_1_0.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVirtioNet_1_0.cpp 93012 2021-12-17 10:07:36Z vboxsync $ $Revision: 93012 $ $Date: 2021-12-17 18:07:36 +0800 (Fri, 17 Dec 2021) $ $Author: vboxsync $ */
+/* $Id: DevVirtioNet_1_0.cpp 93013 2021-12-17 10:11:56Z vboxsync $ $Revision: 93013 $ $Date: 2021-12-17 18:11:56 +0800 (Fri, 17 Dec 2021) $ $Author: vboxsync $ */
 
 /** @file
  * VBox storage devices - Virtio NET Driver
@@ -99,7 +99,7 @@
 #define VIRTIONET_F_CTRL_MAC_ADDR        RT_BIT_64(23)         /**< Set MAC address through control channel         */
 /** @} */
 
-#if LOG_ENABLED
+#ifdef IN_RING3
 static const VIRTIO_FEATURES_LIST s_aDevSpecificFeatures[] =
 {
     { VIRTIONET_F_STATUS,              "   STATUS               Configuration status field is available.\n" },
@@ -2540,7 +2540,7 @@ static int virtioNetR3ReadVirtioTxPktHdr(PVIRTIOCORE pVirtio, PVIRTIONET pThis, 
                     ("Header plus message exceeds packet size"), VERR_BUFFER_OVERFLOW);
     }
 
-    AssertMsgReturn(  !pPktHdr->uFlags & VIRTIONET_HDR_F_NEEDS_CSUM
+    AssertMsgReturn( !(pPktHdr->uFlags & VIRTIONET_HDR_F_NEEDS_CSUM)
                     || sizeof(uint16_t) + pPktHdr->uChksumStart + pPktHdr->uChksumOffset <= cbFrame,
                  ("Checksum (%d bytes) doesn't fit into pkt header (%d bytes)\n",
                  sizeof(uint16_t) + pPktHdr->uChksumStart + pPktHdr->uChksumOffset, cbFrame),
