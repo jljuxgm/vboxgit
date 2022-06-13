82a92b47108e82ffaa31faa16748da47b99b731e
qzuser <395143360@qq.com>
2022-06-13 09:46:55 +0800

    commit_90165 ------------------------------------------------------------------------ r90165 | vboxsync | 2021-07-13 16:13:50 +0800 (Tue, 13 Jul 2021) | 1 line
    FE/Qt: bugref:9996. Using UIUserNamePasswordGroupBox
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/editors/UIWizardNewVMEditors.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/editors/UIWizardNewVMEditors.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMEditors.h 90164 2021-07-13 07:26:44Z vboxsync $ */
+/* $Id: UIWizardNewVMEditors.h 90165 2021-07-13 08:13:50Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMEditors class declaration.
  */
@@ -56,6 +56,7 @@ public:
        QString password() const;
        void setPassword(const QString &strPassword);
        bool isComplete();
+       void setLabelsVisible(bool fVisible);
     /** @} */
 
 private:
