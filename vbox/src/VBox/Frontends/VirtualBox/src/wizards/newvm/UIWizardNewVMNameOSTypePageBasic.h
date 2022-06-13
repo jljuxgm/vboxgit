ea688da838f42d44c69a70b0c4e8976255c8d35a
qzuser <395143360@qq.com>
2022-06-13 10:12:11 +0800

    commit_90280 ------------------------------------------------------------------------ r90280 | vboxsync | 2021-07-22 15:50:38 +0800 (Thu, 22 Jul 2021) | 1 line
    FE/Qt: bugref:9996. Preserving user's virtual medium source selection
    ------------------------------------------------------------------------

--- vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMNameOSTypePageBasic.h
+++ vbox/src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMNameOSTypePageBasic.h
@@ -1,4 +1,4 @@
-/* $Id: UIWizardNewVMNameOSTypePageBasic.h 90223 2021-07-16 08:49:52Z vboxsync $ */
+/* $Id: UIWizardNewVMNameOSTypePageBasic.h 90280 2021-07-22 07:50:38Z vboxsync $ */
 /** @file
  * VBox Qt GUI - UIWizardNewVMNameOSTypePageBasic class declaration.
  */
@@ -39,7 +39,7 @@ class UIWizardNewVM;
 
 namespace UIWizardNewVMNameOSTypePage
 {
-    void guessOSTypeFromName(UINameAndSystemEditor *pNameAndSystemEditor, QString strNewName);
+    bool guessOSTypeFromName(UINameAndSystemEditor *pNameAndSystemEditor, QString strNewName);
     bool createMachineFolder(UINameAndSystemEditor *pNameAndSystemEditor,
                              UINativeWizardPage *pCaller,
                              UIWizardNewVM *pWizard);
