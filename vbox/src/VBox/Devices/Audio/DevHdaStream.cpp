b030d3f93d942c219555b6128e8ba93c05a3b2fc
qzuser <395143360@qq.com>
2022-06-13 09:32:03 +0800

    commit_90097 ------------------------------------------------------------------------ r90097 | vboxsync | 2021-07-08 21:14:12 +0800 (Thu, 08 Jul 2021) | 1 line
    DevHda: Fixed bug in hdaR3StreamAddScheduleItem when splitting up too long period. EMT would get stuck in a buggy loop. bugref:9890
    ------------------------------------------------------------------------

--- vbox/src/VBox/Devices/Audio/DevHdaStream.cpp
+++ vbox/src/VBox/Devices/Audio/DevHdaStream.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevHdaStream.cpp 90016 2021-07-04 21:19:22Z vboxsync $ */
+/* $Id: DevHdaStream.cpp 90097 2021-07-08 13:14:12Z vboxsync $ */
 /** @file
  * Intel HD Audio Controller Emulation - Streams.
  */
@@ -343,7 +343,7 @@ static int hdaR3StreamAddScheduleItem(PHDASTREAM pStreamShared, uint32_t cbCur, 
         /* Reduce till we've below the threshold. */
         uint32_t cbLoop = cbCurAligned;
         do
-            cbLoop = cbCurAligned / 2;
+            cbLoop = cbLoop / 2;
         while (cbLoop > cbMaxPeriod);
         cbLoop = PDMAudioPropsRoundUpBytesToFrame(pProps, cbLoop);
 
