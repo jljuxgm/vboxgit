43e8dfc1f10edad4e0acf4f433f54c9a0caadf5d
qzuser <395143360@qq.com>
2022-06-14 01:17:35 +0800

    commit_93535 ------------------------------------------------------------------------ r93535 | vboxsync | 2022-02-02 00:56:55 +0800 (Wed, 02 Feb 2022) | 1 line
    Main/Unattended: bugref:9781. Build fix.
    ------------------------------------------------------------------------

--- vbox/src/VBox/Main/src-server/UnattendedImpl.cpp
+++ vbox/src/VBox/Main/src-server/UnattendedImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: UnattendedImpl.cpp 93534 2022-02-01 16:46:52Z vboxsync $ */
+/* $Id: UnattendedImpl.cpp 93535 2022-02-01 16:56:55Z vboxsync $ */
 /** @file
  * Unattended class implementation
  */
@@ -3240,7 +3240,7 @@ HRESULT Unattended::getDetectedImageNames(std::vector<com::Utf8Str> &aDetectedIm
     return S_OK;
 }
 
-HRESULT Unattended::getDetectedImageIndices(std::vector<uint32_t> &aDetectedImageIndices)
+HRESULT Unattended::getDetectedImageIndices(std::vector<ULONG> &aDetectedImageIndices)
 {
     AutoReadLock alock(this COMMA_LOCKVAL_SRC_POS);
     aDetectedImageIndices.clear();
